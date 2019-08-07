931d17a49fa3290afb98ac64b052545301075d83
Nazar Stasyuk <nazar.stasyuk@gmail.com>
2019-08-07 13:13:11 +0300

    Created base Spring Boot project with base dependencies.

---
+++ src/main/java/greencity/GreenCityApplication.java
@@ -0,0 +1,13 @@
+package greencity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenCityApplication.class, args);
+    }
+
+}
