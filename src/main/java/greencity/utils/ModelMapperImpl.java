402ff808e4ada3e01fc6c7c18eb8a0f28d5e0c42
studroma <studroma@gmail.com>
2019-08-13 11:23:40 +0300

    Add javaDocs into ModelMapperImpl.class

--- src/main/java/greencity/utils/ModelMapperImpl.java
+++ src/main/java/greencity/utils/ModelMapperImpl.java
@@ -8,21 +8,34 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+/**
+ * {@inheritDoc}
+ */
 @Component
 @AllArgsConstructor
 public class ModelMapperImpl implements ModelMapperUtil {
 
     private ModelMapper modelMapper;
 
+
+    /**
+     * {@inheritDoc}
+     */
     public <S, D> D map(final S srcObj, Class<D> destClass) {
         return modelMapper.map(srcObj, destClass);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public <S, D> D map(final S srcObj, D destObj) {
         modelMapper.map(srcObj, destObj);
         return destObj;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public <S, D> List<D> mapAll(final Collection<S> srcList, Class<D> destClass) {
         return srcList.stream().map(src -> map(src, destClass)).collect(Collectors.toList());
     }
