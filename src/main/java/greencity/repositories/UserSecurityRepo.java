24801ee82f34fd600f9c53c54b09be6dd657e73e
Nazar Stasyuk <nazar.stasyuk@gmail.com>
2019-08-14 18:21:16 +0300

    [issue - 34] Create UserSecurityRepo

---
+++ src/main/java/greencity/repositories/UserSecurityRepo.java
@@ -0,0 +1,6 @@
+package greencity.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSecurityRepo extends JpaRepository<UserSecurityRepo, Long> {
+}
