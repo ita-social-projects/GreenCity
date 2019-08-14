095de263bc322d5f2ede9156bc880097a7341ef8
Nazar Stasyuk <nazar.stasyuk@gmail.com>
2019-08-14 22:32:52 +0300

    [issue - 35] Add firstName, lastName to User

--- src/main/java/greencity/entities/User.java
+++ src/main/java/greencity/entities/User.java
@@ -21,8 +21,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
-    private String name;
+    @Column(nullable = false, length = 20)
+    private String firstName;
+
+    @Column(nullable = false, length = 20)
+    private String lastName;
 
     @Column(unique = true, nullable = false, length = 50)
     private String email;
