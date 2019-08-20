134559d494e54d23ecc49418a17969d550ed4bbd
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-20 16:19:48 +0300

    [Issue #38] edit by checkstyle

--- src/main/java/greencity/controller/UserController.java
+++ src/main/java/greencity/controller/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private UserService userService;
