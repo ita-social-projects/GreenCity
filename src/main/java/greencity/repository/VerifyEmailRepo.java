1656fe3609445e4be102e4300a7348ce08697e38
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-21 14:29:01 +0300

    [issue - 38] merge to dev

--- src/main/java/greencity/repository/VerifyEmailRepo.java
+++ src/main/java/greencity/repository/VerifyEmailRepo.java
@@ -1,13 +1,10 @@
 package greencity.repository;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 import greencity.entity.User;
 import greencity.entity.VerifyEmail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface VerifyEmailRepo extends JpaRepository<VerifyEmail, Long> {
 
