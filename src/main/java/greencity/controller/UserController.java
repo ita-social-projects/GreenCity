51a3be826f1989c6e63cb696e006dcaf7619691f
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-22 23:53:11 +0300

    [issue - 65] edit by checkstyle

--- src/main/java/greencity/controller/UserController.java
+++ src/main/java/greencity/controller/UserController.java
@@ -1,26 +1,18 @@
 package greencity.controller;
 
-import greencity.dto.PageableDto;
-import greencity.dto.user.UserForListDto;
 import greencity.entity.enums.ROLE;
 import greencity.entity.enums.UserStatus;
 import greencity.service.UserService;
-import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@CrossOrigin
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor
@@ -44,7 +36,6 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<?> getAllUsers(Pageable pageable) {
-        return new ResponseEntity<PageableDto<UserForListDto>>(userService.findByPage(pageable)
-                , HttpStatus.OK);
+        return new ResponseEntity<>(userService.findByPage(pageable), HttpStatus.OK);
     }
 }
