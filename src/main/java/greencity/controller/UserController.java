a7785a21b0298c32c4ceaa850e19706791fb95e9
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-23 19:48:53 +0300

    [issue - 65] edit configuration

--- src/main/java/greencity/controller/UserController.java
+++ src/main/java/greencity/controller/UserController.java
@@ -7,12 +7,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor
