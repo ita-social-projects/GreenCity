78f789c5f1e39611fef4da5c4cdbf7e2ed95253b
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-25 15:30:04 +0300

    [issue - 65] add javadoc

--- src/main/java/greencity/repository/UserRepo.java
+++ src/main/java/greencity/repository/UserRepo.java
@@ -5,8 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/** Provides an interface to manage {@link User} entity. */
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
+    /**
+     * Find {@code User} by page.
+     *
+     * @param pageable pageable configuration.
+     * @return {@code Page<User>}
+     */
     Page<User> findAllByOrderByEmail(Pageable pageable);
 }
