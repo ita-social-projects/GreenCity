696c4ce8523b7046302c64543482a9100f75b699
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-28 11:24:28 +0300

    [issue - 65] fix conflicts

--- src/main/java/greencity/controller/UserController.java
+++ src/main/java/greencity/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,14 +23,14 @@ public class UserController {
 
     private UserService userService;
 
-    @PostMapping("update/role")
+    @PutMapping("update/role")
     public ResponseEntity<?> updateRole(
             @RequestParam("id") Long id, @RequestParam("role") ROLE role) {
         userService.updateRole(id, role);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("update/status")
+    @PutMapping("update/status")
     public ResponseEntity<?> updateUserStatus(
             @RequestParam("id") Long id, @RequestParam UserStatus status) {
         userService.updateUserStatus(id, status);
