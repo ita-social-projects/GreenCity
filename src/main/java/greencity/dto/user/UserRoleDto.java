0af1537940bceffe6ccd2a57357955db98e7b5aa
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-09-02 15:53:26 +0300

    [issue - 65] add dtos for update role and userStatus

--- src/main/java/greencity/dto/user/UserRoleDto.java
+++ src/main/java/greencity/dto/user/UserRoleDto.java
@@ -2,11 +2,12 @@ package greencity.dto.user;
 
 import greencity.entity.enums.ROLE;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserRoleDto {
-
     @NotNull
     private Long id;
 
--- src/main/java/greencity/dto/user/UserStatusDto.java
+++ src/main/java/greencity/dto/user/UserStatusDto.java
@@ -2,15 +2,15 @@ package greencity.dto.user;
 
 import greencity.entity.enums.UserStatus;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserStatusDto {
-
     @NotNull
     private Long id;
 
     @NotNull
     private UserStatus userStatus;
-
 }
