adaff130282a6aa86f9fa424aaf2854d6635fd22
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-09-03 12:31:13 +0300

    [issue - 65] delete unused method

--- src/test/java/greencity/service/impl/UserServiceImplTest.java
+++ src/test/java/greencity/service/impl/UserServiceImplTest.java
@@ -124,14 +124,6 @@ public class UserServiceImplTest {
         userService.save(new User());
     }
 
-    @Test
-    public void getUserRoleTest() {
-        User user = User.builder().email("nazarvladykaaa@gmail.com").role(ROLE.ROLE_ADMIN).build();
-        when(userService.findByEmail("nazarvladykaaa@gmail.com")).thenReturn(user);
-
-        assertEquals(ROLE.ROLE_ADMIN, userService.getRole("nazarvladykaaa@gmail.com"));
-    }
-
     @Test(expected = BadIdException.class)
     public void deleteByIdExceptionBadIdTest() {
         userService.deleteById(1l);
