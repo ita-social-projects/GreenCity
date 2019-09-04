da3f5922304f1e31ddff4bbd0a50397fafd5a4e9
DmytroDovgal <dovgal.dmytr@gmail.com>
2019-09-04 17:23:50 +0300

    added new rows to sql script

--- insert.sql
+++ insert.sql
@@ -49,11 +49,11 @@ values ('Malevych', '0322 849 348', 'malevych_lviv@gmail.com', '2011-08-23T14:25
 INSERT INTO place (name, phone, email, modified_date, status, author_id, category_id, description)
 values ('Kryivka', '067 310 3145', 'kryivka_lviv@gmail.com', '2009-07-23T14:25:10', 2, 1, 1, 'Restaurant');
 
-INSERT INTO specification (name) VALUES ('cafe');
-INSERT INTO specification (name) VALUES ('own cup');
-INSERT INTO specification (name) VALUES ('vegetarian food');
-INSERT INTO specification (name) VALUES ('ith pets');
-INSERT INTO specification (name) VALUES ('shop');
+INSERT INTO specification (name) VALUES ('Animal');
+INSERT INTO specification (name) VALUES ('Own cup');
+INSERT INTO specification (name) VALUES ('Karaoke');
+INSERT INTO specification (name) VALUES ('Shopping');
+INSERT INTO specification (name) VALUES ('Shopping');
 
 INSERT INTO specification_value (value, place_id, specification_id) VALUES ('disc', 1, 1);
 INSERT INTO specification_value (value, place_id, specification_id) VALUES ('disc', 2, 2);
@@ -63,7 +63,7 @@ INSERT INTO specification_value (value, place_id, specification_id) VALUES ('dis
 
 INSERT INTO favorite_place (place_id, user_id, name) values (1, 1, 'My best place');
 INSERT INTO favorite_place (place_id, user_id, name) values (2, 2, 'My best place');
-INSERT INTO favorite_place (place_id, user_id, name) values (2, 2, 'My best place');
+INSERT INTO favorite_place (place_id, user_id, name) values (3, 3, 'My best place');
 
 INSERT INTO rate (rate, place_id, user_id) values (5, 1, 1);
 INSERT INTO rate (rate, place_id, user_id) values (4, 2, 2);
@@ -75,28 +75,50 @@ INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('2
 INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 1, 1);
 INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 2, 1);
 INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 3, 1);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 4, 1);
 
-INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '07:00:00', 0, 2);
-INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '07:00:00', 1, 2);
-INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '07:00:00', 2, 2);
-INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '07:00:00', 3, 2);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 0, 2);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 1, 2);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 2, 2);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 3, 2);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 4, 2);
+
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 0, 3);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 1, 3);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 2, 3);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 3, 3);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 4, 3);
+
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 0, 4);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 1, 4);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 2, 4);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 3, 4);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 4, 4);
+
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 0, 5);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 1, 5);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 2, 5);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 3, 5);
+INSERT INTO opening_hours (close_time, open_time, week_day, place_id) VALUES ('20:00:00', '06:00:00', 4, 5);
+
+
 
 
 INSERT INTO comment (date, text, place_id, user_id) values ('2019-08-11', 'cool place', 1, 1);
-INSERT INTO comment (date, text, place_id, user_id) values ('2019-02-11', 'great', 1, 1);
-INSERT INTO comment (date, text, place_id, user_id) values ('2019-03-11', 'nice', 1, 1);
-INSERT INTO comment (date, text, place_id, user_id) values ('2019-05-11', 'good', 1, 1);
-INSERT INTO comment (date, text, place_id, user_id) values ('2019-08-11', 'well', 1, 1);
+INSERT INTO comment (date, text, place_id, user_id) values ('2019-02-11', 'great', 2, 1);
+INSERT INTO comment (date, text, place_id, user_id) values ('2019-03-11', 'nice', 3, 1);
+INSERT INTO comment (date, text, place_id, user_id) values ('2019-05-11', 'good', 4, 1);
+INSERT INTO comment (date, text, place_id, user_id) values ('2019-08-11', 'well', 5, 1);
 
 INSERT INTO location (address, lat, lng, place_id) VALUES ('вулиця Під Дубом', 49.84988, 24.022533, 1);
 INSERT INTO location (address, lat, lng, place_id)
-VALUES ('вулиця Кульпарківська, 226а, Львів, Львівська область, 79000', 49.807129, 23.977985, 2);
+VALUES ('Вулиця Кульпарківська, 226а, Львів, Львівська область, 79000', 49.807129, 23.977985, 2);
 INSERT INTO location (address, lat, lng, place_id)
-VALUES ('Torhovyy Dim Tsipperiv, площа Ринок, Львів, Львівська область, 79000', 49.842042, 24.030359, 3);
+VALUES ('Площа Ринок, Львів, Львівська область, 79000', 49.842042, 24.030359, 3);
 INSERT INTO location (address, lat, lng, place_id)
-VALUES ('проспект В''ячеслава Чорновола, 2, Львів, Львівська область, 79000', 49.847489, 24.025975, 4);
+VALUES ('Проспект В''ячеслава Чорновола, 2, Львів, Львівська область, 79000', 49.847489, 24.025975, 4);
 INSERT INTO location (address, lat, lng, place_id)
-VALUES ('пл. Ринок, 14 (підвал), Львів, Львівська область, 79000', 49.841311, 24.03229, 5);
+VALUES ('Площа Ринок, 14 (підвал), Львів, Львівська область, 79000', 49.841311, 24.03229, 5);
 
 
 
