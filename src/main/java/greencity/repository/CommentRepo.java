99500ca371acb60d9da6ce3b64086b44a99f7f27
MarianMilian <milanmarian@mail.ru>
2019-09-16 14:22:49 +0300

    [issue-158 ] add photo and comment repositories

---
+++ src/main/java/greencity/repository/CommentRepo.java
@@ -0,0 +1,9 @@
+package greencity.repository;
+
+import greencity.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepo extends JpaRepository<Comment, Long> {
+}
