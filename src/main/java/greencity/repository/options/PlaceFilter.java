5b936725b64a3f7716e9c000f90f337ef0aa4c8d
studroma <studroma@gmail.com>
2019-09-17 20:06:39 +0300

    Removed log

--- src/main/java/greencity/repository/options/PlaceFilter.java
+++ src/main/java/greencity/repository/options/PlaceFilter.java
@@ -11,7 +11,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.domain.Specification;
 
 /**
@@ -21,7 +20,6 @@ import org.springframework.data.jpa.domain.Specification;
  *
  * @author Roman Zahouri, Nazar Stasyuk
  */
-@Slf4j
 public class PlaceFilter implements Specification<Place> {
     private FilterPlaceDto filterPlaceDto;
     private MapBoundsDto mapBoundsDto;
@@ -54,7 +52,6 @@ public class PlaceFilter implements Specification<Place> {
         List<Predicate> predicates = new ArrayList<>();
 
         if (null != filterPlaceDto) {
-            log.info(filterPlaceDto.toString());
             predicates.add(hasStatus(root, criteriaBuilder, filterPlaceDto.getStatus()));
             predicates.add(hasPositionInBounds(root, criteriaBuilder, filterPlaceDto.getMapBoundsDto()));
             predicates.add(hasDiscount(root, criteriaBuilder, filterPlaceDto.getDiscountDto()));
