40b2dd6f18b8148f7d074c81044a4e2580c93efa
MarianMilian <milanmarian@mail.ru>
2019-09-20 23:22:02 +0300

    [issue-158 ] add PhotoDto

--- src/main/java/greencity/repository/PlaceCommentRepo.java
+++ src/main/java/greencity/repository/PlaceCommentRepo.java
@@ -1,16 +1,18 @@
 package greencity.repository;
 
 import greencity.entity.Comment;
-
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PlaceCommentRepo extends JpaRepository<Comment, Long> {
-    Page<Comment> findAllByPlaceId(Long id, Pageable pageable);
+    Page<Comment> findAllByPlaceId(Long placeId, Pageable pageable);
+
+    Optional<Comment> findByPlaceIdAndUserId(Long placeId, Long userId);
 
     Optional<Comment> findById(Long id);
 }
