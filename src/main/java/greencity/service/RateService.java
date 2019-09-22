daa2eef721e5d65b632f015c4b4a112e79b417e5
MarianMilian <milanmarian@mail.ru>
2019-09-22 23:09:14 +0300

    [issue-158 ] add Rate service and Rate  service implementation

---
+++ src/main/java/greencity/service/RateService.java
@@ -0,0 +1,4 @@
+package greencity.service;
+
+public interface RateService {
+}
---
+++ src/main/java/greencity/service/impl/RateServiceImpl.java
@@ -0,0 +1,15 @@
+package greencity.service.impl;
+
+import greencity.service.RateService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * The class provides implementation of the {@code RateService}.
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class RateServiceImpl implements RateService {
+}
