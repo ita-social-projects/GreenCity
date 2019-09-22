de11d1b5d09fc44313f6ae489bd29a99c217bae9
MarianMilian <milanmarian@mail.ru>
2019-09-22 23:02:48 +0300

    [issue-158 ] add Rate repository

--- src/main/java/greencity/repository/RateRepo.java
+++ src/main/java/greencity/repository/RateRepo.java
@@ -1,12 +1,12 @@
 package greencity.repository;
 
-import greencity.entity.Photo;
+import greencity.entity.Rate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Provides an interface to manage {@link Photo} entity.
+ * Provides an interface to manage {@link Rate} entity.
  */
 @Repository
-public interface PhotoRepo extends JpaRepository<Photo, Long> {
+public interface RateRepo extends JpaRepository<Rate,Long> {
 }
