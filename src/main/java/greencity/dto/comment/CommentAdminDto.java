50dfaffbfa199dba6b810037331d666f5da4d4f6
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-10-08 13:03:59 +0300

    [issue - 245] dto for admin tab

---
+++ src/main/java/greencity/dto/comment/CommentAdminDto.java
@@ -0,0 +1,24 @@
+package greencity.dto.comment;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import greencity.dto.photo.PhotoReturnDto;
+import greencity.dto.place.AdminPlaceDto;
+import greencity.dto.rate.RateAddDto;
+import greencity.entity.Place;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentAdminDto {
+    private Long id;
+    private String text;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdDate;
+    private List<PhotoReturnDto> photos;
+    private AdminPlaceDto place;
+}
