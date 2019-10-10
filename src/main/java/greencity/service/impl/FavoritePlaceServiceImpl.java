a5e737793bfc2163cd2aea7fb9cb473b9320f23e
ZakharSkaletskyi <sklzakhar@gmail.com>
2019-10-10 11:34:06 +0300

    fix save in FavoritePlaceImpl and add test

--- src/main/java/greencity/service/impl/FavoritePlaceServiceImpl.java
+++ src/main/java/greencity/service/impl/FavoritePlaceServiceImpl.java
@@ -45,6 +45,11 @@ public class FavoritePlaceServiceImpl implements FavoritePlaceService {
         if (!placeService.existsById(favoritePlace.getPlace().getId())) {
             throw new BadIdException(ErrorMessage.PLACE_NOT_FOUND_BY_ID);
         }
+        if (repo.findByPlaceIdAndUserEmail(favoritePlaceDto.getPlaceId(), userEmail) == null) {
+            throw new BadIdException(ErrorMessage.FAVORITE_PLACE_ALREADY_EXISTS
+                + String.format("for this placeId:  %d and user with email: %s", favoritePlaceDto.getPlaceId(),
+                favoritePlaceDto.getName()));
+        }
         favoritePlace.setUser(User.builder().email(userEmail).id(userService.findIdByEmail(userEmail)).build());
         return favoritePlaceDtoMapper.convertToDto(repo.save(favoritePlace));
     }
@@ -55,14 +60,14 @@ public class FavoritePlaceServiceImpl implements FavoritePlaceService {
      * @author Zakhar Skaletskyi
      */
     @Override
-    public FavoritePlaceDto update(FavoritePlaceDto favoritePlaceShowDto, String userEmail) {
-        log.info(LogMessage.IN_UPDATE, favoritePlaceShowDto);
+    public FavoritePlaceDto update(FavoritePlaceDto favoritePlaceDto, String userEmail) {
+        log.info(LogMessage.IN_UPDATE, favoritePlaceDto);
 
-        FavoritePlace favoritePlace = repo.findByPlaceIdAndUserEmail(favoritePlaceShowDto.getPlaceId(), userEmail);
+        FavoritePlace favoritePlace = repo.findByPlaceIdAndUserEmail(favoritePlaceDto.getPlaceId(), userEmail);
         if (favoritePlace == null) {
-            throw new BadIdException(ErrorMessage.FAVORITE_PLACE_NOT_FOUND + favoritePlaceShowDto.getPlaceId());
+            throw new BadIdException(ErrorMessage.FAVORITE_PLACE_NOT_FOUND + favoritePlaceDto.getPlaceId());
         }
-        favoritePlace.setName(favoritePlaceShowDto.getName());
+        favoritePlace.setName(favoritePlaceDto.getName());
         return favoritePlaceDtoMapper.convertToDto(repo.save(favoritePlace));
     }
 
