59c55d7d3b6ba7880ec231c8a77248bea20e202b
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2019-11-13 15:32:49 +0200

    Create Advice entity

---
+++ src/main/java/greencity/entity/Advice.java
@@ -0,0 +1,27 @@
+package greencity.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "advice")
+public class Advice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 300)
+    private String name;
+
+    @OneToMany(mappedBy = "advice", cascade = {CascadeType.ALL})
+    private List<HabitDictionary> habitDictionaries = new ArrayList<>();
+}
