e875dba4b07f9bd988af5bd737fb1758814b42f7
nomadness <vitalii.skolozdra@gmail.com>
2019-11-15 14:18:53 +0200

    Add repository

--- src/main/java/greencity/repository/UserGoalRepo.java
+++ src/main/java/greencity/repository/UserGoalRepo.java
@@ -1,7 +1,18 @@
 package greencity.repository;
 
+import greencity.entity.Goal;
+import greencity.entity.User;
 import greencity.entity.UserGoal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserGoalRepo extends JpaRepository<UserGoal, Long> {
+    /**
+     * Method returns list of {@link UserGoal} for specific user.
+     *
+     * @param userId - id of user.
+     * @return list of {@link UserGoal}
+     */
+    List<UserGoal> findAllByUserId(Long userId);
 }
