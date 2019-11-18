935493a327e39a464731b5fdf9090461bd89e14d
Yuriy <yuriy.olkhovskyi@gmail.com>
2019-11-19 00:48:53 +0200

    added documentation and refactored code in some places

--- src/main/java/greencity/service/HabitStatisticService.java
+++ src/main/java/greencity/service/HabitStatisticService.java
@@ -1,13 +1,14 @@
 package greencity.service;
 
-import greencity.dto.habitstatistic.AddHabitStatisticDto;
-import greencity.dto.habitstatistic.UpdateHabitStatisticDto;
+import greencity.dto.habitstatistic.*;
 import greencity.entity.Habit;
 import greencity.entity.HabitStatistic;
+import greencity.entity.User;
+import java.util.List;
 
 public interface HabitStatisticService {
     /**
-     * Method for creating new Habit statistic to database.
+     * Method for creating {@link HabitStatistic} to database.
      *
      * @param addHabitStatisticDto - dto with {@link HabitStatistic} rate, amount of items, date and {@link Habit} id.
      * @return {@link AddHabitStatisticDto} instance.
@@ -15,18 +16,51 @@ public interface HabitStatisticService {
     AddHabitStatisticDto save(AddHabitStatisticDto addHabitStatisticDto);
 
     /**
-     * Method for updating Habit statistic in database.
+     * Method for updating {@link HabitStatistic} in database.
      *
      * @param dto - dto with {@link HabitStatistic} id, rate and amount of items.
-     * @return {@link AddHabitStatisticDto} instance.
+     * @return {@link UpdateHabitStatisticDto} instance.
      */
     UpdateHabitStatisticDto update(Long habitStatisticId, UpdateHabitStatisticDto dto);
 
     /**
-     * Method for finding a Habit Statistic by its id.
+     * Method for finding {@link HabitStatistic} by its id.
      *
-     * @param id Habit Statistic's id
-     * @return a Habit Statistic's instance
+     * @param id {@link HabitStatistic} id
+     * @return a {@link HabitStatistic} instance
      */
     HabitStatistic findById(Long id);
+
+    /**
+     * Method for finding all {@link Habit} by {@link User} email.
+     *
+     * @param email - {@link User} email.
+     * @return list of user's {@link Habit}.
+     */
+    List<Habit> findAllHabitsByUserEmail(String email);
+
+    /**
+     * Method for finding all {@link HabitDto} by {@link User} email and {@link Habit}status.
+     *
+     * @param email  {@link User} email.
+     * @param status {@link Habit} status.
+     * @return list of {@link HabitDto}
+     */
+    List<HabitDto> findAllHabitsByStatus(String email, Boolean status);
+
+    /**
+     * Method for finding some statistics by {@link User} email.
+     *
+     * @param email {@link User} email.
+     * @return {@link CalendarUsefulHabitsDto} instance.
+     */
+    CalendarUsefulHabitsDto getInfoAboutUserHabits(String email);
+
+    /**
+     * Method for finding all {@link HabitStatisticDto} by {@link Habit id}.
+     *
+     * @param habitId {@link Habit} id.
+     * @return list of {@link HabitStatisticDto} instances.
+     */
+    List<HabitStatisticDto> findAllByHabitId(Long habitId);
 }
