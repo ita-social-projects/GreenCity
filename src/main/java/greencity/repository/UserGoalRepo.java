52f8d7fd1f595c1690a1ced14b0ca208542dfe14
nomadness <vitalii.skolozdra@gmail.com>
2019-11-26 10:04:44 +0200

    Remove unused imports

--- src/main/java/greencity/repository/UserGoalRepo.java
+++ src/main/java/greencity/repository/UserGoalRepo.java
@@ -1,11 +1,8 @@
 package greencity.repository;
 
-import greencity.entity.Goal;
-import greencity.entity.User;
 import greencity.entity.UserGoal;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserGoalRepo extends JpaRepository<UserGoal, Long> {
     /**
