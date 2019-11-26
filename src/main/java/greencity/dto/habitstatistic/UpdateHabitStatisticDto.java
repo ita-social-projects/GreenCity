e3300ac842f5e384618e1402d249d0f8a8f69840
Yuriy <yuriy.olkhovskyi@gmail.com>
2019-11-26 15:23:56 +0200

    removed unused imports, rewrote native queries to jpql, changed calculating statistic, now it returns general statistic for current month and difference of untaken items statistic between current day and previous

--- src/main/java/greencity/dto/habitstatistic/UpdateHabitStatisticDto.java
+++ src/main/java/greencity/dto/habitstatistic/UpdateHabitStatisticDto.java
@@ -2,15 +2,12 @@ package greencity.dto.habitstatistic;
 
 import greencity.constant.ValidationConstants;
 import greencity.entity.enums.HabitRate;
-import java.time.LocalDate;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @NoArgsConstructor
