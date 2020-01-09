b90044aa6c366befb2c14e313192288a8a1de39d
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2020-01-09 15:53:14 +0200

    Update tests for Advice and Fact

--- src/test/java/greencity/service/impl/AdviceServiceImplTest.java
+++ src/test/java/greencity/service/impl/AdviceServiceImplTest.java
@@ -1,26 +1,41 @@
 package greencity.service.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 import greencity.dto.advice.AdviceDTO;
+import greencity.dto.advice.AdvicePostDTO;
+import greencity.dto.language.LanguageTranslationDTO;
+import greencity.dto.user.HabitDictionaryIdDto;
+import greencity.entity.Advice;
+import greencity.entity.AdviceTranslation;
+import greencity.entity.HabitDictionary;
+import greencity.exception.exceptions.NotDeletedException;
 import greencity.exception.exceptions.NotFoundException;
+import greencity.exception.exceptions.NotUpdatedException;
 import greencity.repository.AdviceRepo;
 import greencity.repository.AdviceTranslationRepo;
 import greencity.repository.HabitDictionaryRepo;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AdviceServiceImplTest {
-    @Mock
-    private ModelMapper modelMapper;
 
     @InjectMocks
     private AdviceServiceImpl adviceService;
+
+    @Mock
+    private ModelMapper modelMapper;
 
     @Mock
     private AdviceRepo adviceRepo;
@@ -32,96 +47,88 @@ public class AdviceServiceImplTest {
     private HabitDictionaryRepo habitDictionaryRepo;
 
     private AdviceDTO adviceDTO = new AdviceDTO(1L, "content", null);
-    //    private AdvicePostDTO advicePostDTO = new AdvicePostDTO("content", new HabitDictionaryIdDto(1L));
-//    private HabitDictionary habitDictionary = new HabitDictionary(1L, "test", "test", "test", null, null);
-
-//    private Advice advice = new Advice(1L, Collections.emptyList(), habitDictionary);
-    //  private Language language = new Language(1L, "en", anyList());
-
-    //  private AdviceTranslation adviceTranslation = new AdviceTranslation(1L, language, content, "djdj");
+    private LanguageTranslationDTO languageTranslationDTO = new LanguageTranslationDTO(null, "test");
+    private AdvicePostDTO advicePostDTO = new AdvicePostDTO(null, new HabitDictionaryIdDto(1L));
+    private HabitDictionary habitDictionary = new HabitDictionary(1L, "test", null, null);
+    private Advice advice = new Advice(1L, Collections.emptyList(), habitDictionary);
+    private AdviceTranslation adviceTranslation = new AdviceTranslation(1L, null, advice, "test");
 
 
-//    @Test
-//    public void getAllAdvicesFailed() {
-//        List<AdviceDTO> expected = Collections.emptyList();
-//        when(modelMapper.map(adviceRepo.findAll(), new TypeToken<List<AdviceDTO>>() {
-//        }.getType())).thenReturn(expected);
-//        assertEquals(expected, adviceService.getAllAdvices());
-//    }
+    @Test
+    public void getAllAdvicesFailed() {
+        List<LanguageTranslationDTO> expected = Collections.emptyList();
+        when(modelMapper.map(adviceTranslationRepo.findAll(), new TypeToken<List<LanguageTranslationDTO>>() {
+        }.getType())).thenReturn(expected);
+        assertEquals(expected, adviceService.getAllAdvices());
+    }
 
-//    @Test
-//    public void getRandomAdviceByHabitIdAndLanguage() {
-//        when(adviceTranslationRepo.getRandomAdviceTranslationByHabitIdAndLanguage(anyString(), anyLong()))
-//                .thenReturn(Optional.of(adviceTranslation));
-//        when(modelMapper.map(content, AdviceDTO.class)).thenReturn(adviceDTO);
-//        assertEquals(adviceDTO, adviceService.getRandomAdviceByHabitIdAndLanguage(anyLong(), "en"));
-//    }
+    @Test
+    public void getRandomAdviceByHabitIdAndLanguage() {
+        when(adviceTranslationRepo.getRandomAdviceTranslationByHabitIdAndLanguage("en", 1L))
+            .thenReturn(Optional.of(adviceTranslation));
+        when(modelMapper.map(adviceTranslation, LanguageTranslationDTO.class)).thenReturn(languageTranslationDTO);
+        assertEquals(languageTranslationDTO, adviceService.getRandomAdviceByHabitIdAndLanguage(1L, "en"));
+    }
 
-//    @Test(expected = NotFoundException.class)
-//    public void getRandomAdviceByHabitIdFailed() {
-//        adviceService.getRandomAdviceByHabitIdAndLanguage(anyLong());
-//    }
+    @Test(expected = NotFoundException.class)
+    public void getRandomAdviceByHabitIdFailed() {
+        adviceService.getRandomAdviceByHabitIdAndLanguage(1L, "en");
+    }
 
-//    @Test
-//    public void getAdviceById() {
-//        when(adviceRepo.findById(anyLong())).thenReturn(Optional.of(advice));
-//        when(modelMapper.map(advice, AdviceDTO.class)).thenReturn(adviceDTO);
-//        assertEquals(adviceDTO, adviceService.getAdviceById(anyLong()));
-//    }
+    @Test
+    public void getAdviceById() {
+        when(adviceRepo.findById(anyLong())).thenReturn(Optional.of(advice));
+        when(modelMapper.map(advice, AdviceDTO.class)).thenReturn(adviceDTO);
+        assertEquals(adviceDTO, adviceService.getAdviceById(anyLong()));
+    }
 
     @Test(expected = NotFoundException.class)
     public void getAdviceByIdFailed() {
         adviceService.getAdviceById(anyLong());
     }
 
-//    @Test
-//    public void getAdviceByName() {
-//        when(adviceRepo.findAdviceByAdvice(anyString())).thenReturn(Optional.of(content));
-//        when(modelMapper.map(content, AdviceDTO.class)).thenReturn(adviceDTO);
-//        assertEquals(adviceDTO, adviceService.getAdviceByName(anyString()));
-//    }
+    @Test
+    public void getAdviceByName() {
+        when(adviceTranslationRepo.findAdviceTranslationByLanguage_CodeAndAdvice("en", "test")).thenReturn(Optional.of(adviceTranslation));
+        when(modelMapper.map(adviceTranslation, AdviceDTO.class)).thenReturn(adviceDTO);
+        assertEquals(adviceDTO, adviceService.getAdviceByName("en", "test"));
+    }
 
-//    @Test(expected = NotFoundException.class)
-//    public void getAdviceByNameFailed() {
-//        adviceService.getAdviceByName(anyString());
-//    }
+    @Test(expected = NotFoundException.class)
+    public void getAdviceByNameFailed() {
+        adviceService.getAdviceByName("en", "test");
+    }
 
-//    @Test
-//    public void saveAdviceAndAdviceTranslation() {
-//        when(adviceService.saveAdviceAndAdviceTranslation(advicePostDTO)).thenReturn(content);
-//        assertEquals(content, adviceService.saveAdviceAndAdviceTranslation(advicePostDTO));
-//    }
-//
-//    @Test(expected = NotSavedException.class)
-//    public void saveFailed() {
-//        when(adviceRepo.findAdviceByAdvice(anyString())).thenReturn(Optional.of(content));
-//        adviceService.saveAdviceAndAdviceTranslation(advicePostDTO);
-//    }
-//
-//    @Test
-//    public void update() {
-//        when(habitDictionaryRepo.findById(anyLong())).thenReturn(Optional.of(habitDictionary));
-//        when(adviceRepo.findById(anyLong())).thenReturn(Optional.of(content));
-//        when(adviceRepo.saveAdviceAndAdviceTranslation(content)).thenReturn(content);
-//        when(adviceService.update(advicePostDTO, 1L)).thenReturn(content);
-//        assertEquals(content, adviceService.update(advicePostDTO, 1L));
-//    }
-//
-//    @Test(expected = NotUpdatedException.class)
-//    public void updateFailed() {
-//        adviceService.update(advicePostDTO, 1L);
-//
-//    }
+    @Test
+    public void save() {
+        when(adviceService.save(advicePostDTO)).thenReturn(advice);
+        assertEquals(advice, adviceService.save(advicePostDTO));
+    }
 
-//    @Test
-//    public void delete() {
-//        when(adviceRepo.findById(advice.getId())).thenReturn(Optional.of(advice));
-//        assertEquals(advice.getId(), adviceService.delete(advice.getId()));
-//        verify(adviceRepo, times(1)).deleteById(anyLong());
-//    }
-//
-//    @Test(expected = NotDeletedException.class)
-//    public void deleteFailed() {
-//        adviceService.delete(advice.getId());
-//    }
+    @Test
+    public void update() {
+        when(habitDictionaryRepo.findById(anyLong())).thenReturn(Optional.of(habitDictionary));
+        when(adviceRepo.findById(anyLong())).thenReturn(Optional.of(advice));
+        when(adviceRepo.save(advice)).thenReturn(advice);
+        when(adviceService.update(advicePostDTO, 1L)).thenReturn(advice);
+        assertEquals(advice, adviceService.update(advicePostDTO, 1L));
+    }
+
+    @Test(expected = NotUpdatedException.class)
+    public void updateFailed() {
+        adviceService.update(advicePostDTO, 1L);
+
+    }
+
+    @Test
+    public void delete() {
+        when(adviceRepo.findById(advice.getId())).thenReturn(Optional.of(advice));
+        assertEquals(advice.getId(), adviceService.delete(advice.getId()));
+        verify(adviceRepo, times(1)).deleteById(anyLong());
+    }
+
+    @Test(expected = NotDeletedException.class)
+    public void deleteFailed() {
+        adviceService.delete(advice.getId());
+    }
 }
--- src/test/java/greencity/service/impl/HabitFactServiceImplTest.java
+++ src/test/java/greencity/service/impl/HabitFactServiceImplTest.java
@@ -2,15 +2,25 @@ package greencity.service.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import greencity.dto.advice.AdviceDTO;
 import greencity.dto.fact.HabitFactDTO;
+import greencity.dto.fact.HabitFactPostDTO;
+import greencity.dto.language.LanguageTranslationDTO;
+import greencity.dto.user.HabitDictionaryIdDto;
+import greencity.entity.FactTranslation;
+import greencity.entity.HabitDictionary;
+import greencity.entity.HabitFact;
+import greencity.exception.exceptions.NotDeletedException;
 import greencity.exception.exceptions.NotFoundException;
+import greencity.exception.exceptions.NotUpdatedException;
+import greencity.repository.FactTranslationRepo;
 import greencity.repository.HabitDictionaryRepo;
 import greencity.repository.HabitFactRepo;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -29,101 +39,95 @@ public class HabitFactServiceImplTest {
     private ModelMapper modelMapper;
 
     @Mock
+    private FactTranslationRepo factTranslationRepo;
+
+    @Mock
     private HabitFactRepo habitFactRepo;
 
     @Mock
     private HabitDictionaryRepo habitDictionaryRepo;
 
-    //    private HabitDictionary habitDictionary = new HabitDictionary(1L, "test", "test", "test", null, null);
-//    private HabitFact habitFact = new HabitFact(1L, null, habitDictionary);
+    private HabitDictionary habitDictionary = new HabitDictionary(1L, "test", null, null);
+    private HabitFact habitFact = new HabitFact(1L, null, habitDictionary);
+    private FactTranslation factTranslation = new FactTranslation(1L, null, null, "test");
     private HabitFactDTO habitFactDTO = new HabitFactDTO(1L, "fact", null);
-//    private HabitFactPostDTO habitFactPostDTO = new HabitFactPostDTO("fact", new HabitDictionaryIdDto(1L));
+    private LanguageTranslationDTO languageTranslationDTO = new LanguageTranslationDTO(null, "test");
+    private HabitFactPostDTO habitFactPostDTO = new HabitFactPostDTO(null, new HabitDictionaryIdDto(1L));
 
     @Test
     public void getAllHabitFactsFailed() {
         List<AdviceDTO> expected = Collections.emptyList();
-        when(modelMapper.map(habitFactRepo.findAll(), new TypeToken<List<HabitFactDTO>>() {
+        when(modelMapper.map(factTranslationRepo.findAll(), new TypeToken<List<LanguageTranslationDTO>>() {
         }.getType())).thenReturn(expected);
         assertEquals(expected, habitFactService.getAllHabitFacts());
     }
 
-//    @Test
-//    public void getRandomHabitFactByHabitId() {
-//        when(habitFactRepo.getRandomHabitFactByHabitId(anyLong())).thenReturn(Optional.of(habitFact));
-//        when(modelMapper.map(habitFact, HabitFactDTO.class)).thenReturn(habitFactDTO);
-//        assertEquals(habitFactDTO, habitFactService.getRandomHabitFactByHabitId(anyLong()));
-//    }
-//
-//    @Test(expected = NotFoundException.class)
-//    public void getRandomHabitFactByHabitIdFailed() {
-//        habitFactService.getRandomHabitFactByHabitId(anyLong());
-//    }
+    @Test
+    public void getRandomHabitFactByHabitId() {
+        when(factTranslationRepo.getRandomFactTranslationByHabitIdAndLanguage("en", 1L)).thenReturn(Optional.of(factTranslation));
+        when(modelMapper.map(factTranslation, LanguageTranslationDTO.class)).thenReturn(languageTranslationDTO);
+        assertEquals(languageTranslationDTO, habitFactService.getRandomHabitFactByHabitIdAndLanguage(1L, "en"));
+    }
 
-//    @Test(expected = NotFoundException.class)
-//    public void getRandomHabitFactByHabitIdFailed() {
-//        habitFactService.getRandomHabitFactByHabitId(anyLong());
-//    }
+    @Test(expected = NotFoundException.class)
+    public void getRandomHabitFactByHabitIdFailed() {
+        habitFactService.getRandomHabitFactByHabitIdAndLanguage(1L, "en");
+    }
 
-//    @Test
-//    public void getHabitFactById() {
-//        when(habitFactRepo.findById(anyLong())).thenReturn(Optional.of(habitFact));
-//        when(modelMapper.map(habitFact, HabitFactDTO.class)).thenReturn(habitFactDTO);
-//        assertEquals(habitFactDTO, habitFactService.getHabitFactById(anyLong()));
-//    }
+    @Test
+    public void getHabitFactById() {
+        when(habitFactRepo.findById(anyLong())).thenReturn(Optional.of(habitFact));
+        when(modelMapper.map(habitFact, HabitFactDTO.class)).thenReturn(habitFactDTO);
+        assertEquals(habitFactDTO, habitFactService.getHabitFactById(anyLong()));
+    }
 
     @Test(expected = NotFoundException.class)
     public void getHabitFactByIdFailed() {
         habitFactService.getHabitFactById(anyLong());
     }
 
-//    @Test
-//    public void getHabitFactByName() {
-//        when(habitFactRepo.findHabitFactByFact(anyString())).thenReturn(Optional.of(habitFact));
-//        when(modelMapper.map(habitFact, HabitFactDTO.class)).thenReturn(habitFactDTO);
-//        assertEquals(habitFactDTO, habitFactService.getHabitFactByName(anyString()));
-//    }
+    @Test
+    public void getHabitFactByName() {
+        when(factTranslationRepo.findFactTranslationByLanguage_CodeAndHabitFact("en", "test")).thenReturn(Optional.of(factTranslation));
+        when(modelMapper.map(factTranslation, HabitFactDTO.class)).thenReturn(habitFactDTO);
+        assertEquals(habitFactDTO, habitFactService.getHabitFactByName("en", "test"));
+    }
 
-//    @Test(expected = NotFoundException.class)
-//    public void getHabitFactByNameFailed() {
-//        habitFactService.getHabitFactByName(anyString());
-//    }
+    @Test(expected = NotFoundException.class)
+    public void getHabitFactByNameFailed() {
+        habitFactService.getHabitFactByName("en", "test");
+    }
 
-//    @Test
-//    public void save() {
-//        when(habitFactService.save(habitFactPostDTO)).thenReturn(habitFact);
-//        assertEquals(habitFact, habitFactService.save(habitFactPostDTO));
-//    }
+    @Test
+    public void save() {
+        when(habitFactService.save(habitFactPostDTO)).thenReturn(habitFact);
+        assertEquals(habitFact, habitFactService.save(habitFactPostDTO));
+    }
 
-//    @Test(expected = NotSavedException.class)
-//    public void saveFailed() {
-//        when(habitFactRepo.findHabitFactByFact(anyString())).thenReturn(Optional.of(habitFact));
-//        habitFactService.save(habitFactPostDTO);
-//    }
+    @Test
+    public void update() {
+        when(habitDictionaryRepo.findById(anyLong())).thenReturn(Optional.of(habitDictionary));
+        when(habitFactRepo.findById(anyLong())).thenReturn(Optional.of(habitFact));
+        when(habitFactRepo.save(habitFact)).thenReturn(habitFact);
+        when(habitFactService.update(habitFactPostDTO, 1L)).thenReturn(habitFact);
+        assertEquals(habitFact, habitFactService.update(habitFactPostDTO, 1L));
+    }
 
-//    @Test
-//    public void update() {
-//        when(habitDictionaryRepo.findById(anyLong())).thenReturn(Optional.of(habitDictionary));
-//        when(habitFactRepo.findById(anyLong())).thenReturn(Optional.of(habitFact));
-//        when(habitFactRepo.save(habitFact)).thenReturn(habitFact);
-//        when(habitFactService.update(habitFactPostDTO, 1L)).thenReturn(habitFact);
-//        assertEquals(habitFact, habitFactService.update(habitFactPostDTO, 1L));
-//    }
+    @Test(expected = NotUpdatedException.class)
+    public void updateFailed() {
+        habitFactService.update(habitFactPostDTO, 1L);
+    }
 
-//    @Test(expected = NotUpdatedException.class)
-//    public void updateFailed() {
-//        habitFactService.update(habitFactPostDTO, 1L);
-//    }
+    @Test
+    public void delete() {
+        when(habitFactRepo.findById(habitFact.getId())).thenReturn(Optional.of(habitFact));
+        assertEquals(habitFact.getId(), habitFactService.delete(habitFact.getId()));
+        verify(habitFactRepo, times(1)).deleteById(anyLong());
+    }
 
-//    @Test
-//    public void delete() {
-//        when(habitFactRepo.findById(habitFact.getId())).thenReturn(Optional.of(habitFact));
-//        assertEquals(habitFact.getId(), habitFactService.delete(habitFact.getId()));
-//        verify(habitFactRepo, times(1)).deleteById(anyLong());
-//    }
-//
-//    @Test(expected = NotDeletedException.class)
-//    public void deleteFailed() {
-//        habitFactService.delete(habitFact.getId());
-//    }
+    @Test(expected = NotDeletedException.class)
+    public void deleteFailed() {
+        habitFactService.delete(habitFact.getId());
+    }
 
 }
