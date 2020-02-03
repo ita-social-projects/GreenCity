01a016b7646818cbfd9cf74abdf5288715c5d167
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2020-02-03 13:57:01 +0200

    Create cache for habit item statistic

--- src/main/java/greencity/constant/CacheConstants.java
+++ src/main/java/greencity/constant/CacheConstants.java
@@ -1,6 +1,8 @@
 package greencity.constant;
 
 public final class CacheConstants {
+    public static final String HABIT_ITEM_STATISTIC_CACHE = "habit_item_statistic_cache";
+
     private CacheConstants() {
     }
 }
