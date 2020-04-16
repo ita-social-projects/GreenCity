2f27df7e1c0b7ae10551027182d0040590eef4e2
greatpanda1603 <olesya.ostriychuk@gmail.com>
2020-04-17 02:11:16 +0300

    add HabitMapperTest, refactor HabitMapper

---
+++ core/src/test/java/greencity/mapping/HabitMapperTest.java
@@ -0,0 +1,32 @@
+package greencity.mapping;
+
+import greencity.ModelUtils;
+import greencity.dto.habitstatistic.AddHabitStatisticDto;
+import greencity.entity.Habit;
+import greencity.entity.HabitStatistic;
+import greencity.entity.User;
+import java.time.ZonedDateTime;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class HabitMapperTest {
+    @InjectMocks
+    private HabitMapper habitMapper;
+
+    @Test
+    public void convertTest() {
+        User user = ModelUtils.getUser();
+
+        Habit expected = Habit.builder()
+            .user(user)
+            .createDate(ZonedDateTime.now())
+            .statusHabit(true)
+            .build();
+
+        assertEquals(expected, habitMapper.convert(user));
+    }
+}
