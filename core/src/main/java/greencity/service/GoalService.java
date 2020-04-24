910105f12c08b39eb1c9fea0faa5a09f06fcf9ae
greatpanda1603 <olesya.ostriychuk@gmail.com>
2020-04-24 21:58:18 +0300

    add missing javadoc

--- core/src/main/java/greencity/service/GoalService.java
+++ core/src/main/java/greencity/service/GoalService.java
@@ -3,6 +3,7 @@ package greencity.service;
 import greencity.dto.goal.GoalDto;
 import greencity.dto.user.UserGoalResponseDto;
 import greencity.entity.Habit;
+import greencity.entity.Language;
 import greencity.entity.UserGoal;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,11 @@ public interface GoalService {
      */
     List<GoalDto> findAll(String language);
 
+    /**
+     * Method for getting {@link UserGoalResponseDto} from {@link UserGoal}.
+     *
+     * @param userGoal needed text either from CustomGoal or GoalTranslation
+     * @return userGoalResponseDto.
+     */
     UserGoalResponseDto getUserGoalResponseDto(UserGoal userGoal);
 }
