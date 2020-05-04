6d3733af702b9f6dc416f7affebf99c8daf9fbcb
Taras <taras19041@gmail.com>
2020-05-04 16:18:35 +0300

    add countOfResult to SearchResponseDto

--- core/src/main/java/greencity/dto/search/SearchResponseDto.java
+++ core/src/main/java/greencity/dto/search/SearchResponseDto.java
@@ -2,10 +2,16 @@ package greencity.dto.search;
 
 import java.util.List;
 import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Builder
+@EqualsAndHashCode
 public class SearchResponseDto {
     List<SearchNewsDto> ecoNews;
+
+    Long countOfResults;
 }
