9892e0d410bfedbb88f911eaa456aaadd3f7ca03
MarDatsko <datskomar@gmail.com>
2020-05-09 15:42:06 +0300

    rewritten code logic in EcoNewsServiceImpl, SearchServiceImpl after -> merge

--- core/src/test/java/greencity/service/impl/SearchServiceImplTest.java
+++ core/src/test/java/greencity/service/impl/SearchServiceImplTest.java
@@ -1,17 +1,15 @@
 package greencity.service.impl;
 
-import greencity.ModelUtils;
+import greencity.dto.PageableDto;
 import greencity.dto.search.SearchNewsDto;
 import greencity.service.EcoNewsService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -23,18 +21,17 @@ class SearchServiceImplTest {
     @InjectMocks
     private SearchServiceImpl searchService;
 
-    @InjectMocks
-    private ModelMapper modelMapper;
-
     @Mock
     private EcoNewsService ecoNewsService;
 
     @Test
     void searchTest() {
-        List<SearchNewsDto> listSearchNewsDto = new ArrayList<>();
-        listSearchNewsDto.add(modelMapper.map(ModelUtils.getEcoNews(), SearchNewsDto.class));
+        SearchNewsDto searchNewsDto = new SearchNewsDto(1L, "title", null, null, Collections.singletonList("tag"));
+        PageableDto<SearchNewsDto> pageableDto = new PageableDto<>(Collections.singletonList(searchNewsDto), 4, 1);
 
-        when(ecoNewsService.search(anyString())).thenReturn(listSearchNewsDto);
-        assertEquals(listSearchNewsDto, searchService.search("test").getEcoNews());
+        when(ecoNewsService.search(anyString())).thenReturn(pageableDto);
+
+        assertEquals(pageableDto.getPage(), searchService.search("test").getEcoNews());
+        assertEquals(Long.valueOf(pageableDto.getTotalElements()), searchService.search("test").getCountOfResults());
     }
 }
