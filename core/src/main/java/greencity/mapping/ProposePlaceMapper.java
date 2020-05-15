3a49c6558daa50ccf676f935fd7a43c1694366a4
MarDatsko <datskomar@gmail.com>
2020-05-15 17:46:12 +0300

    rewritten ProposePlaceMapper using AbstractConverter, added new mapper to config class

--- core/src/main/java/greencity/mapping/ProposePlaceMapper.java
+++ core/src/main/java/greencity/mapping/ProposePlaceMapper.java
@@ -1,84 +1,70 @@
 package greencity.mapping;
 
-import greencity.constant.ErrorMessage;
-import greencity.dto.location.LocationAddressAndGeoDto;
-import greencity.dto.openhours.OpeningHoursDto;
 import greencity.dto.place.PlaceAddDto;
 import greencity.entity.*;
-import greencity.exception.exceptions.BadRequestException;
-import greencity.exception.exceptions.NotFoundException;
-import greencity.service.CategoryService;
-import greencity.service.LocationService;
-import greencity.service.PhotoService;
-import greencity.service.SpecificationService;
-import java.util.List;
-import java.util.Set;
-import lombok.AllArgsConstructor;
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 /**
- * The class uses other {@code Autowired} mappers to convert {@link Place} entity objects to {@link
- * PlaceAddDto} dto objects and vise versa.
+ * Class that used by {@link ModelMapper} to map {@link PlaceAddDto} into
+ * {@link Place}.
  *
- * @author Kateryna Horokh
+ * @author Marian Datsko
  */
-@AllArgsConstructor
 @Component
-public class ProposePlaceMapper implements MapperToEntity<PlaceAddDto, Place> {
-    private ModelMapper mapper;
-    private CategoryService categoryService;
-    private SpecificationService specService;
-    private PhotoService photoService;
-    private LocationService locationService;
+public class ProposePlaceMapper extends AbstractConverter<PlaceAddDto, Place> {
+
+    /**
+     * Method convert {@link PlaceAddDto} to {@link Place}.
+     *
+     * @return {@link Place}
+     */
 
     @Override
-    public Place convertToEntity(PlaceAddDto dto) {
-        Category category = categoryService.findByName(dto.getCategory().getName());
-        checkLocationValues(dto.getLocation());
-        checkInputTime(dto.getOpeningHoursList());
-        Place place = mapper.map(dto, Place.class);
-        place.setCategory(category);
+    protected Place convert(PlaceAddDto placeAddDto) {
+        Place place = new Place();
+        place.setName(placeAddDto.getName());
+        place.setLocation(Location.builder()
+                .address(placeAddDto.getLocation().getAddress())
+                .lat(placeAddDto.getLocation().getLat())
+                .lng(placeAddDto.getLocation().getLng())
+                .build());
+        place.setCategory(Category.builder()
+                .name(placeAddDto.getCategory().getName())
+                .build());
+        place.setOpeningHoursList(placeAddDto.getOpeningHoursList().stream()
+                .map(openingHoursDto -> {
+                    OpeningHours openingHours = new OpeningHours();
+                    openingHours.setOpenTime(openingHoursDto.getOpenTime());
+                    openingHours.setCloseTime(openingHoursDto.getCloseTime());
+                    openingHours.setBreakTime(BreakTime.builder()
+                            .startTime(openingHoursDto.getBreakTime().getStartTime())
+                            .endTime(openingHoursDto.getBreakTime().getEndTime())
+                            .build());
+                    openingHours.setWeekDay(openingHoursDto.getWeekDay());
+                    return openingHours;
+                }).collect(Collectors.toSet()));
+        place.setDiscountValues(placeAddDto.getDiscountValues().stream()
+                .map(discountValueDto -> {
+                    DiscountValue discountValue = new DiscountValue();
+                    discountValue.setValue(discountValueDto.getValue());
+                    discountValue.setSpecification(Specification.builder()
+                            .name(discountValueDto.getSpecification().getName())
+                            .build());
+                    return discountValue;
+                }).collect(Collectors.toSet()));
+        place.setPhotos(placeAddDto.getPhotos().stream()
+                .map(photoAddDto -> {
+                    Photo photo = new Photo();
+                    photo.setName(photoAddDto.getName());
+                    return photo;
+                }).collect(Collectors.toList()));
         place.getOpeningHoursList().forEach(h -> h.setPlace(place));
-        saveDiscountValuesWithPlace(place.getDiscountValues(), place);
-        savePhotosWithPlace(place.getPhotos(), place);
+        place.getPhotos().forEach(photo -> photo.setUser(place.getAuthor()));
 
         return place;
-    }
-
-    private void checkLocationValues(LocationAddressAndGeoDto dto) {
-        if (locationService.findByLatAndLng(dto.getLat(), dto.getLng()).isPresent()) {
-            throw new BadRequestException(ErrorMessage.LOCATION_IS_PRESENT);
-        }
-    }
-
-    private void savePhotosWithPlace(List<Photo> photos, Place place) {
-        photos.forEach(photo -> {
-            if (photoService.findByName(photo.getName()).isPresent()) {
-                throw new BadRequestException(ErrorMessage.PHOTO_IS_PRESENT);
-            }
-            photo.setPlace(place);
-        });
-    }
-
-    private void saveDiscountValuesWithPlace(Set<DiscountValue> discountValues, Place place) {
-        discountValues.forEach(disc -> {
-            disc.setSpecification(specService.findByName(disc.getSpecification().getName()));
-            disc.setPlace(place);
-        });
-    }
-
-    private void checkInputTime(Set<OpeningHoursDto> dto) {
-        dto.forEach(hours -> {
-            if (hours.getOpenTime().getHour() > hours.getCloseTime().getHour()) {
-                throw new BadRequestException(ErrorMessage.CLOSE_TIME_LATE_THAN_OPEN_TIME);
-            }
-            if (hours.getBreakTime() != null) {
-                if (hours.getBreakTime().getStartTime().getHour() < hours.getOpenTime().getHour()
-                    || hours.getBreakTime().getEndTime().getHour() > hours.getCloseTime().getHour()) {
-                    throw new BadRequestException(ErrorMessage.WRONG_BREAK_TIME);
-                }
-            }
-        });
     }
 }
