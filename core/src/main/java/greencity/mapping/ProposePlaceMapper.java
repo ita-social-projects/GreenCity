81a78a1f14cf49fb8dbe5bf37c870b57ec8fcd70
MarDatsko <datskomar@gmail.com>
2020-05-19 19:56:21 +0300

    refactored code, added new class UtilsMapper

--- core/src/main/java/greencity/mapping/ProposePlaceMapper.java
+++ core/src/main/java/greencity/mapping/ProposePlaceMapper.java
@@ -6,7 +6,6 @@ import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
 
 /**
  * Class that used by {@link ModelMapper} to map {@link PlaceAddDto} into
@@ -25,41 +24,12 @@ public class ProposePlaceMapper extends AbstractConverter<PlaceAddDto, Place> {
     protected Place convert(PlaceAddDto placeAddDto) {
         Place place = new Place();
         place.setName(placeAddDto.getName());
-        place.setLocation(Location.builder()
-                .address(placeAddDto.getLocation().getAddress())
-                .lat(placeAddDto.getLocation().getLat())
-                .lng(placeAddDto.getLocation().getLng())
-                .build());
-        place.setCategory(Category.builder()
-                .name(placeAddDto.getCategory().getName())
-                .build());
-        place.setOpeningHoursList(placeAddDto.getOpeningHoursList().stream()
-                .map(openingHoursDto -> {
-                    OpeningHours openingHours = new OpeningHours();
-                    openingHours.setOpenTime(openingHoursDto.getOpenTime());
-                    openingHours.setCloseTime(openingHoursDto.getCloseTime());
-                    openingHours.setBreakTime(BreakTime.builder()
-                            .startTime(openingHoursDto.getBreakTime().getStartTime())
-                            .endTime(openingHoursDto.getBreakTime().getEndTime())
-                            .build());
-                    openingHours.setWeekDay(openingHoursDto.getWeekDay());
-                    return openingHours;
-                }).collect(Collectors.toSet()));
-        place.setDiscountValues(placeAddDto.getDiscountValues().stream()
-                .map(discountValueDto -> {
-                    DiscountValue discountValue = new DiscountValue();
-                    discountValue.setValue(discountValueDto.getValue());
-                    discountValue.setSpecification(Specification.builder()
-                            .name(discountValueDto.getSpecification().getName())
-                            .build());
-                    return discountValue;
-                }).collect(Collectors.toSet()));
-        place.setPhotos(placeAddDto.getPhotos().stream()
-                .map(photoAddDto -> {
-                    Photo photo = new Photo();
-                    photo.setName(photoAddDto.getName());
-                    return photo;
-                }).collect(Collectors.toList()));
+        place.setLocation(UtilsMapper.map(placeAddDto.getLocation(), Location.class));
+        place.setCategory(UtilsMapper.map(placeAddDto.getCategory(), Category.class));
+        place.setOpeningHoursList(UtilsMapper.mapAllToSet(placeAddDto.getOpeningHoursList(), OpeningHours.class));
+        place.setDiscountValues(UtilsMapper.mapAllToSet(placeAddDto.getDiscountValues(), DiscountValue.class));
+        place.setPhotos(UtilsMapper.mapAllToList(placeAddDto.getPhotos(), Photo.class));
+
         place.getOpeningHoursList().forEach(h -> h.setPlace(place));
         place.getPhotos().forEach(photo -> photo.setUser(place.getAuthor()));
 
