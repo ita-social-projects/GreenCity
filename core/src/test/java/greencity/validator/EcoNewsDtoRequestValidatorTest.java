5a68a8d1c3735fa9afe0234a7385dce281484d41
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-06-21 18:14:13 +0300

    unit tests for EcoNewsDtoRequestValidator

---
+++ core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
@@ -0,0 +1,44 @@
+package greencity.validator;
+
+import greencity.ModelUtils;
+import greencity.dto.econews.AddEcoNewsDtoRequest;
+import greencity.exception.exceptions.InvalidURLException;
+import greencity.exception.exceptions.WrongCountOfTagsException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+
+import static greencity.ModelUtils.getAddEcoNewsDtoRequest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+class EcoNewsDtoRequestValidatorTest {
+    @InjectMocks
+    private EcoNewsDtoRequestValidator validator;
+
+    @Test
+    void isValidTrueTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setSource("https://eco-lavca.ua/");
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidWrongCountOfTagsExceptionTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setTags(Collections.emptyList());
+        request.setSource("https://eco-lavca.ua/");
+        assertThrows(WrongCountOfTagsException.class, () -> validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidInvalidURLExceptionTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setSource("/eco-lavca.ua/https:/");
+        assertThrows(InvalidURLException.class, () -> validator.isValid(request, null));
+    }
+}
