861601d2064f539b7690774c2ebd9c9eee6f3b24
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-06-30 22:54:34 +0300

    Removed 'public' modifier

--- core/src/test/java/greencity/service/impl/HabitDictionaryServiceImplTest.java
+++ core/src/test/java/greencity/service/impl/HabitDictionaryServiceImplTest.java
@@ -22,14 +22,14 @@ class HabitDictionaryServiceImplTest {
     private HabitDictionaryRepo habitDictionaryRepo;
 
     @Test
-    public void findByIdTest() {
+    void findByIdTest() {
         HabitDictionary expected = ModelUtils.getHabitDictionary();
         when(habitDictionaryRepo.findById(expected.getId())).thenReturn(Optional.of(expected));
         assertEquals(Optional.of(ModelUtils.getHabitDictionary()), habitDictionaryRepo.findById(1L));
     }
 
     @Test
-    public void findByIdNotFoundExceptionTest() {
+    void findByIdNotFoundExceptionTest() {
         when(habitDictionaryRepo.findById(1L))
                 .thenThrow(new NotFoundException(ErrorMessage.HABIT_NOT_FOUND_BY_ID + 1L));
         assertThrows(NotFoundException.class, () -> {
