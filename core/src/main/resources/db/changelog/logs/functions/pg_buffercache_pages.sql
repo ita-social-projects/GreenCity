193d07ec2f902cbbfa2e20132da77ea7c6f23a0a
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-08-08 15:43:06 +0300

    Integrated functions into liquibase

---
+++ core/src/main/resources/db/changelog/logs/functions/pg_buffercache_pages.sql
@@ -0,0 +1,11 @@
+
+CREATE OR REPLACE FUNCTION public.pg_buffercache_pages(
+	)
+    RETURNS SETOF record
+    LANGUAGE 'c'
+    COST 1
+    VOLATILE
+    ROWS 1000
+AS '$libdir/pg_buffercache', 'pg_buffercache_pages';
+
+REVOKE ALL ON FUNCTION public.pg_buffercache_pages() FROM PUBLIC;
