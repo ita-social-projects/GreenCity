769ffd58c59e0dab4f7554a5f96f9b06f908127f
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-08-12 14:11:01 +0300

    added ManagementUserController, management_users_list.html and updated UserForListDto

---
+++ core/src/main/java/greencity/controller/ManagementUserController.java
@@ -0,0 +1,42 @@
+package greencity.controller;
+
+import greencity.annotations.ApiPageable;
+import greencity.dto.PageableDto;
+import greencity.dto.user.UserForListDto;
+import greencity.entity.User;
+import greencity.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/management/users")
+public class ManagementUserController {
+    private UserService userService;
+
+    /**
+     * Returns management page with all {@link User}.
+     *
+     * @param model Model that will be configured and returned to user.
+     * @param page  Page index you want to retrieve.
+     * @param size  Number of records per page.
+     * @return View template path {@link String}.
+     * @author Vasyl Zhovnir
+     */
+    @ApiPageable
+    @GetMapping("")
+    public String getAllUsers(Model model,
+                              @RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "5") int size) {
+        Pageable paging = PageRequest.of(page, size, Sort.by("id").descending());
+        model.addAttribute("users", userService.findByPage(paging).getPage());
+        return "core/management_users_list";
+    }
+}
