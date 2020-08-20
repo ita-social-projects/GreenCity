fdc4200e0acb5efda65b4ab610e315a144a95297
Vasyl <vazhovnir@gmail.com>
2020-08-20 12:32:40 +0300

    update functionality for user management (#1183)

---
+++ core/src/main/java/greencity/dto/user/UserManagementDto.java
@@ -0,0 +1,36 @@
+package greencity.dto.user;
+
+import greencity.constant.ValidationConstants;
+import greencity.entity.enums.UserStatus;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class UserManagementDto {
+    @NotNull
+    private Long id;
+
+    @NotBlank(message = ValidationConstants.EMPTY_USERNAME)
+    @Size(
+        min = ValidationConstants.USERNAME_MIN_LENGTH,
+        max = ValidationConstants.USERNAME_MAX_LENGTH,
+        message = ValidationConstants.INVALID_USERNAME_LENGTH)
+    private String name;
+
+    @Email(message = ValidationConstants.INVALID_EMAIL)
+    @NotBlank(message = ValidationConstants.EMPTY_EMAIL)
+    private String email;
+
+    private String userCredo;
+
+    @NotNull
+    private UserStatus userStatus;
+}
--- core/src/main/java/greencity/service/impl/UserServiceImpl.java
+++ core/src/main/java/greencity/service/impl/UserServiceImpl.java
@@ -105,6 +105,41 @@ public class UserServiceImpl implements UserService {
             users.getTotalPages());
     }
 
+    @Override
+    public PageableDto<UserManagementDto> findUserForManagementByPage(Pageable pageable) {
+        Page<User> users = userRepo.findAll(pageable);
+        List<UserManagementDto> userManagementDtos =
+            users.getContent().stream()
+                .map(user -> modelMapper.map(user, UserManagementDto.class))
+                .collect(Collectors.toList());
+        return new PageableDto<>(
+            userManagementDtos,
+            users.getTotalElements(),
+            users.getPageable().getPageNumber(),
+            users.getTotalPages());
+    }
+
+    @Override
+    public void updateUser(UserManagementDto dto) {
+        User user = findById(dto.getId());
+        updateUserFromDto(dto, user);
+        userRepo.save(user);
+    }
+
+    /**
+     * Method for setting data from {@link UserForListDto} to {@link User}.
+     *
+     * @param dto  - dto {@link UserForListDto} with updated fields.
+     * @param user {@link User} to be updated.
+     * @author Vasyl Zhovnir
+     */
+    private void updateUserFromDto(UserManagementDto dto, User user) {
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setUserCredo(dto.getUserCredo());
+        user.setUserStatus(dto.getUserStatus());
+    }
+
     /**
      * {@inheritDoc}
      */
