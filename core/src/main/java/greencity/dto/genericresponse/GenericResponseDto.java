8dc817a9a8ecff666e782c2ee0b677cf6dbb0948
Mykola Lehkyi <mykola.lehkyi.dev@gmail.com>
2020-08-27 18:36:15 +0300

    Fact of the day edit (#1217)
    * Created entity
       FactOfTheDay
       FactOfTheDayTranslation
    Updated Languages
    
    * Added liquibase migration
    
    * Added Repos
    Added Services
    Added DTOs
    Added caching for one day
    Added restController
    Fixed liquibase issues
    
    * Refactor code
    
    * update -> validate
    
    * title -> name
    
    * Clear error issues
    
    * Remove unused object
    
    * Added DTOs. Added method in controller. Realized Pageable in Services. Added some stuff to UI.
    
    * Added view with data and pagination.
    
    * Code refactor
    
    * Fixed issue with test
    
    * Remove unused import
    
    * Resolved conflicts
    
    * Run
    
    * Realized editing, adding, deleting, managing Fact of the day.
    
    * Fixed issue with header and footer
    
    * Update mapping
    
    * Fixed issues

---
+++ core/src/main/java/greencity/dto/genericresponse/GenericResponseDto.java
@@ -0,0 +1,16 @@
+package greencity.dto.genericresponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GenericResponseDto {
+    List<FieldErrorDto> errors = new ArrayList<>();
+}
--- core/src/main/java/greencity/webcontroller/ManagementFactOfTheDayController.java
+++ core/src/main/java/greencity/webcontroller/ManagementFactOfTheDayController.java
@@ -4,6 +4,8 @@ import greencity.annotations.ApiPageable;
 import greencity.dto.PageableDto;
 import greencity.dto.factoftheday.FactOfTheDayDTO;
 import greencity.service.FactOfTheDayService;
+import greencity.service.LanguageService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +21,8 @@ import springfox.documentation.annotations.ApiIgnore;
 public class ManagementFactOfTheDayController {
     @Autowired
     private FactOfTheDayService factOfTheDayService;
+    @Autowired
+    private LanguageService languageService;
 
     /**
      * Returns management page with all facts of the day.
@@ -27,10 +31,12 @@ public class ManagementFactOfTheDayController {
      * @return model
      */
     @ApiPageable
+    @ApiOperation(value = "Get management page with facts of the day.")
     @GetMapping("")
     public String getAllFacts(Model model, @ApiIgnore Pageable pageable) {
         PageableDto<FactOfTheDayDTO> allFactsOfTheDay = factOfTheDayService.getAllFactsOfTheDay(pageable);
         model.addAttribute("pageable", allFactsOfTheDay);
+        model.addAttribute("languages",languageService.getAllLanguages());
         return "core/management_fact_of_the_day";
     }
 }
