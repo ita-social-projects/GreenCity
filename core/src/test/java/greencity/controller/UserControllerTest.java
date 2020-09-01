458e8cc65686982431a6a7537bcb64caae2531aa
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-09-01 12:59:12 +0300

    Revert "added test"
    This reverts commit 9b0a40a81bf3148a2511c52728a33a2e746988ce.

--- core/src/test/java/greencity/controller/UserControllerTest.java
+++ core/src/test/java/greencity/controller/UserControllerTest.java
@@ -28,8 +28,6 @@ import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.security.Principal;
@@ -494,18 +492,10 @@ class UserControllerTest {
             "\t\"showEcoPlace\": true,\n" +
             "\t\"showShoppingList\": false\n" +
             "}";
-
         MockMultipartFile jsonFile = new MockMultipartFile("userProfileDtoRequest", "", "application/json", json.getBytes());
 
-        MockMultipartHttpServletRequestBuilder builder =
-            MockMvcRequestBuilders.multipart(userLink + "/profile");
-        builder.with(request -> {
-            request.setMethod("PUT");
-            return request;
-        });
-
-        this.mockMvc.perform(builder.
-            file(jsonFile)
+        this.mockMvc.perform(multipart(userLink + "/profile")
+            .file(jsonFile)
             .principal(principal)
             .accept(MediaType.APPLICATION_JSON)
             .contentType(MediaType.APPLICATION_JSON))
