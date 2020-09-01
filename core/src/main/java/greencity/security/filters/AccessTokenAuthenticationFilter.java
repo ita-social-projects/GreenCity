5b9fd1decca91a408a2712f95be3f7bf8bdb61cc
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-09-01 15:41:35 +0300

    deleted unused imports

--- core/src/main/java/greencity/security/filters/AccessTokenAuthenticationFilter.java
+++ core/src/main/java/greencity/security/filters/AccessTokenAuthenticationFilter.java
@@ -10,12 +10,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
