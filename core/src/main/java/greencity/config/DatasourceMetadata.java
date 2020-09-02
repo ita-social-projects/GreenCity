2345a2d67ddae79713ff86205b297ceb1a1d8fb3
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-09-02 14:33:14 +0300

    updated functions

--- core/src/main/java/greencity/config/DatasourceMetadata.java
+++ core/src/main/java/greencity/config/DatasourceMetadata.java
@@ -1,11 +1,21 @@
 package greencity.config;
 
+import java.io.LineNumberReader;
+import java.sql.SQLException;
 import java.time.ZoneId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import javax.sql.DataSource;
 
 /**
  * Configuration class that obtains metadata of the database.
@@ -15,15 +25,17 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @Configuration
 public class DatasourceMetadata {
     private final JdbcTemplate jdbcTemplate;
-
+    private final DataSource dataSource;
     /**
      * Constructor.
      *
      * @param jdbcTemplate {@link JdbcTemplate}
      */
+
     @Autowired
-    public DatasourceMetadata(JdbcTemplate jdbcTemplate) {
+    public DatasourceMetadata(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
     }
 
     /**
@@ -43,6 +55,19 @@ public class DatasourceMetadata {
         }
         log.info("Obtained timezone of the database is {}", zoneId);
         return ZoneId.of(zoneId);
+    }
+
+    /**
+     * Creates functions for database.
+     *
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void createFunctions() {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.setSqlScriptEncoding("UTF-8");
+        databasePopulator.addScript(
+            new ClassPathResource("db/functions/fn_textsearcheconews.sql"));
+        databasePopulator.execute(dataSource);
     }
 
     public static class FailedToObtainDatasourceTimezone extends RuntimeException {
