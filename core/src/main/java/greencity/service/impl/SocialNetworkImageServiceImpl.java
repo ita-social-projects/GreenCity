8fc3ed8973fb3dbc2a3097ff0b86693aed7edc31
Mykola Lehkyi <lintey8@gmail.com>
2020-09-03 14:29:16 +0300

    Service update, now gets link.

--- core/src/main/java/greencity/service/impl/SocialNetworkImageServiceImpl.java
+++ core/src/main/java/greencity/service/impl/SocialNetworkImageServiceImpl.java
@@ -4,17 +4,22 @@ import greencity.entity.SocialNetworkImage;
 import greencity.repository.SocialNetworkImageRepo;
 import greencity.service.FileService;
 import greencity.service.SocialNetworkImageService;
-import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.util.Base64;
 import java.util.Optional;
+import javax.imageio.ImageIO;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.awt.image.ToolkitImage;
-import sun.awt.image.URLImageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Service
 public class SocialNetworkImageServiceImpl implements SocialNetworkImageService {
@@ -31,7 +36,8 @@ public class SocialNetworkImageServiceImpl implements SocialNetworkImageService 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        Optional<SocialNetworkImage> optionalSocialNetworkImage = socialNetworkImageRepo.findByBasePath(preparedUrlHost);
+        Optional<SocialNetworkImage> optionalSocialNetworkImage =
+            socialNetworkImageRepo.findByBasePath(preparedUrlHost);
         if (optionalSocialNetworkImage.isPresent()) {
             return optionalSocialNetworkImage.get();
         }
@@ -39,17 +45,49 @@ public class SocialNetworkImageServiceImpl implements SocialNetworkImageService 
             String preparedFaviconUrl = String.format("http://www.google.com/s2/favicons?domain_url=%s", URLEncoder
                 .encode(preparedUrlHost, "UTF-8"));
             URL faviconUrl = new URL(preparedFaviconUrl);
-            URLImageSource content = (URLImageSource) faviconUrl.getContent();
-            ToolkitImage image = (ToolkitImage) Toolkit.getDefaultToolkit().createImage(content);
-            System.out.println(image);
+            BufferedImage bufferedImage = ImageIO.read(faviconUrl);
+            File tempFile = new File("tempImage.png");
+            ImageIO.write(bufferedImage, "png", tempFile);
+            FileItem fileItem = new DiskFileItem("mainFile", Files.probeContentType(tempFile.toPath()),
+                false, tempFile.getName(), (int) tempFile.length(), tempFile.getParentFile());
+            fileItem.getOutputStream();
+            URL uploadCloud = fileService.upload(new CommonsMultipartFile(fileItem));
+            String imagePath = uploadCloud.toString();
+            SocialNetworkImage socialNetworkImage = SocialNetworkImage.builder()
+                .basePath(preparedUrlHost)
+                .imagePath(imagePath)
+                .build();
+            socialNetworkImageRepo.save(socialNetworkImage);
+            return socialNetworkImage;
 
-        } catch (UnsupportedEncodingException | MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+//            encodeToString(bufferedImage, ".png");
+          /*  URLImageSource content = (URLImageSource) faviconUrl.getContent();
+            ToolkitImage image = (ToolkitImage) Toolkit.getDefaultToolkit().createImage(content);
+            System.out.println(image.toString());
+           */
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-//        fileService.upload()
         return null;
+    }
+
+    public static String encodeToString(BufferedImage image, String type) {
+        String imageString = null;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        try {
+            ImageIO.write(image, type, byteArrayOutputStream);
+            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+            Base64.getEncoder().encodeToString(imageBytes);
+            /*
+            BASE64Encoder encoder = new BASE64Encoder();
+            imageString = encoder.encode(imageBytes);*/
+
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageString;
     }
 }
