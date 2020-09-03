030730237687ad4370b0934cc39a620ae1653f60
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-09-03 13:57:58 +0300

    changed doFilterInternal and deleted unused imports

--- core/src/main/java/greencity/security/filters/AccessTokenAuthenticationFilter.java
+++ core/src/main/java/greencity/security/filters/AccessTokenAuthenticationFilter.java
@@ -61,11 +61,8 @@ public class AccessTokenAuthenticationFilter extends OncePerRequestFilter {
                     .authenticate(new UsernamePasswordAuthenticationToken(token, null));
                 User user = userService.findByEmail((String) authentication.getPrincipal());
                 if (user.getUserStatus() != UserStatus.DEACTIVATED) {
-                    log.info("User successfully authenticate - {}", authentication.getPrincipal());
+                    log.debug("User successfully authenticate - {}", authentication.getPrincipal());
                     SecurityContextHolder.getContext().setAuthentication(authentication);
-                } else {
-                    authenticationManager.authenticate(null);
-                    return;
                 }
             } catch (ExpiredJwtException e) {
                 log.info("Token has expired: " + token);
