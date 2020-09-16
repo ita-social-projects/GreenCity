335185fa7dbc5fca8e982e0c557564f7b339578d
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-09-16 16:43:41 +0300

    added dependency and method for generating random password

--- core/src/main/java/greencity/security/service/impl/OwnSecurityServiceImpl.java
+++ core/src/main/java/greencity/security/service/impl/OwnSecurityServiceImpl.java
@@ -28,8 +28,12 @@ import greencity.security.service.OwnSecurityService;
 import greencity.service.UserService;
 import io.jsonwebtoken.ExpiredJwtException;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -239,6 +243,8 @@ public class OwnSecurityServiceImpl implements OwnSecurityService {
     @Override
     public void managementRegisterUser(UserManagementDto dto) {
         User user = managementCreateNewRegisteredUser(dto, jwtTool.generateTokenKey());
+        OwnSecurity ownSecurity = managementCreateOwnSecurity(user);
+        user.setOwnSecurity(ownSecurity);
         savePasswordRestorationTokenForUser(user, jwtTool.generateTokenKey());
     }
 
@@ -257,11 +263,30 @@ public class OwnSecurityServiceImpl implements OwnSecurityService {
             .build();
     }
 
-    private OwnSecurity managementCreateOwnSecurity(String password, User user) {
+    private OwnSecurity managementCreateOwnSecurity(User user) {
         return OwnSecurity.builder()
-            .password(passwordEncoder.encode(password))
+            .password(passwordEncoder.encode(generatePassword()))
             .user(user)
             .build();
+    }
+
+    private String generatePassword() {
+        String upperCaseLetters = RandomStringUtils.random(2, 65, 90, true, true);
+        String lowerCaseLetters = RandomStringUtils.random(2, 97, 122, true, true);
+        String numbers = RandomStringUtils.randomNumeric(2);
+        String specialChar = RandomStringUtils.random(2, 33, 47, false, false);
+        String totalChars = RandomStringUtils.randomAlphanumeric(2);
+        String combinedChars = upperCaseLetters.concat(lowerCaseLetters)
+            .concat(numbers)
+            .concat(specialChar)
+            .concat(totalChars);
+        List<Character> pwdChars = combinedChars.chars()
+            .mapToObj(c -> (char) c)
+            .collect(Collectors.toList());
+        Collections.shuffle(pwdChars);
+        return pwdChars.stream()
+            .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+            .toString();
     }
 
     /**
