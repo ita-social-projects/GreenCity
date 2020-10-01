1d793f2325cfb0ec5392d0848889c5713bcc5f7d
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-10-01 21:23:37 +0300

    added property files, removed unused dependencies

--- core/src/main/resources/db/functions/pg_stat_statements_reset.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.pg_stat_statements_reset(
-	)
-    RETURNS void
-    LANGUAGE 'c'
-    COST 1
-    VOLATILE 
-AS '$libdir/pg_stat_statements', 'pg_stat_statements_reset'
-;
