d1dcfbe80d102c1dd4e99fe735f6275574cb71c8
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-10-01 16:25:11 +0300

    added tests for UserGoalRepo and OpenHoursRepo

---
+++ core/src/test/java/greencity/repository/UserGoalRepoTest.java
@@ -0,0 +1,39 @@
+package greencity.repository;
+
+import greencity.GreenCityApplication;
+import greencity.entity.Goal;
+import greencity.entity.UserGoal;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = GreenCityApplication.class)
+@DataJpaTest
+class UserGoalRepoTest {
+
+    @Autowired
+    UserGoalRepo userGoalRepo;
+
+    @Test
+    @Sql("classpath:sql/user_goal.sql")
+    void findAllByUserIdTest() {
+        List<UserGoal> userGoals = userGoalRepo.findAllByUserId(1L);
+        assertEquals(2, userGoals.size());
+        assertEquals(1, userGoals.get(0).getId());
+    }
+
+    @Test
+    @Sql("classpath:sql/user_goal.sql")
+    void findGoalByUserGoalIdTest() {
+        Goal goal = userGoalRepo.findGoalByUserGoalId(1L).get();
+        assertEquals(1, goal.getId());
+    }
+}
