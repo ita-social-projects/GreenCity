af39dd27f0d0016021c801e386239bdf4ee365b8
Yurii Savchenko <43988243+Yura09@users.noreply.github.com>
2020-10-06 14:08:10 +0300

    created tests for controllers (#1575)

---
+++ core/src/test/java/greencity/controller/AchievementControllerTest.java
@@ -0,0 +1,45 @@
+package greencity.controller;
+
+import greencity.service.AchievementService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class AchievementControllerTest {
+    private static final String achievementLink = "/achievements";
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private AchievementController achievementController;
+
+    @Mock
+    private AchievementService achievementService;
+
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(achievementController)
+            .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+            .build();
+    }
+
+    @Test
+    void findAllTest() throws Exception {
+        mockMvc.perform(get(achievementLink)).andExpect(status().isOk());
+        verify(achievementService).findAll();
+    }
+}
