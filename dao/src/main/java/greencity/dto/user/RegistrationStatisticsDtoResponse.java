7abf38cec235bf0c6923409bdccff878c9366a73
Vasyl Zhovnir <vazhovnir@gmail.com>
2020-10-07 11:31:43 +0300

    deleted unnecessary lombok annotations

--- dao/src/main/java/greencity/dto/user/RegistrationStatisticsDtoResponse.java
+++ dao/src/main/java/greencity/dto/user/RegistrationStatisticsDtoResponse.java
@@ -3,12 +3,9 @@ package greencity.dto.user;
 import lombok.*;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @EqualsAndHashCode
 public class RegistrationStatisticsDtoResponse {
-    private Integer month;
-    private Long count;
+    private final Integer month;
+    private final Long count;
 }
