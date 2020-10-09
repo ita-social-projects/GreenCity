6b0251448f1bcc44fdd05cd564d246f8ae3f42cd
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-10-09 15:07:16 +0300

    code refactoring

--- service-api/src/test/java/greencity/service/impl/AchievementServiceImplTest.java
+++ service-api/src/test/java/greencity/service/impl/AchievementServiceImplTest.java
@@ -5,14 +5,16 @@ import greencity.entity.Achievement;
 import greencity.repository.AchievementRepo;
 import java.util.Collections;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AchievementServiceImplTest {
@@ -25,20 +27,20 @@ class AchievementServiceImplTest {
 
     @Test
     void findAllWithEmptyListTest() {
-        Mockito.when(achievementRepo.findAll()).thenReturn(Collections.emptyList());
+        when(achievementRepo.findAll()).thenReturn(Collections.emptyList());
         List<AchievementDTO> findAllResult = achievementService.findAll();
-        Assertions.assertTrue(findAllResult.isEmpty());
+        assertTrue(findAllResult.isEmpty());
     }
 
     @Test
     void findAllWithOneValueInRepoTest() {
         Achievement achievement = new Achievement(1L, "foo", null, null, null);
-        Mockito.when(achievementRepo.findAll())
+        when(achievementRepo.findAll())
             .thenReturn(Collections.singletonList(achievement));
-        Mockito.when(modelMapper.map(achievement, AchievementDTO.class))
+        when(modelMapper.map(achievement, AchievementDTO.class))
             .thenReturn(new AchievementDTO(achievement.getId(), achievement.getTitle(), null, null));
         List<AchievementDTO> findAllResult = achievementService.findAll();
-        Assertions.assertEquals("foo", findAllResult.get(0).getTitle());
-        Assertions.assertEquals(1L, (long) findAllResult.get(0).getId());
+        assertEquals("foo", findAllResult.get(0).getTitle());
+        assertEquals(1L, (long) findAllResult.get(0).getId());
     }
 }
