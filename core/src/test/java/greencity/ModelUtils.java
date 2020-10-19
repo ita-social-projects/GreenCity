a81ac011b269f8b6cb24891d8676a576a7d89d6d
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-10-19 09:43:00 +0300

    added HabitStatusDtoMapperTest and refactored HabitStatusServiceImplTest

--- core/src/test/java/greencity/ModelUtils.java
+++ core/src/test/java/greencity/ModelUtils.java
@@ -189,6 +189,27 @@ public class ModelUtils {
                 .build();
     }*/
 
+    public static HabitStatus getHabitStatus() {
+        HabitAssign habitAssign = getHabitAssign();
+
+        return HabitStatus.builder()
+                .id(1L)
+                .workingDays(10)
+                .habitStreak(5)
+                .lastEnrollmentDate(LocalDateTime.now())
+                .habitAssign(habitAssign).build();
+
+    }
+
+    public static HabitAssign getHabitAssign() {
+        return HabitAssign.builder()
+                .id(1L)
+                .acquired(true)
+                .suspended(false)
+                .createDate(ZonedDateTime.now())
+                .habit(Habit.builder().id(1L).build()).build();
+    }
+
     public static Category getCategory() {
         return Category.builder()
                 .id(12L)
