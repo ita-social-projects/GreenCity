86f79896172c7f5f36281250d6acc16c94dde7d2
Yurii Savchenko <43988243+Yura09@users.noreply.github.com>
2020-10-20 12:24:29 +0300

    made code refactoring of Category and moved to another layer (#1666)
    * made code refactoring of Category and moved to another layer
    
    * code refactoring
    
    * fix issues

---
+++ service-api/src/main/java/greencity/dto/category/CategoryDtoResponse.java
@@ -0,0 +1,17 @@
+package greencity.dto.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class CategoryDtoResponse {
+    private Long id;
+    private String name;
+}
