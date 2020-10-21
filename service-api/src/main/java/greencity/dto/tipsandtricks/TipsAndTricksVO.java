d7864012de03f1aab173e25b804f91db7f05bca3
olenapetryshak <petryshak94@gmail.com>
2020-10-21 20:39:21 +0300

    move TipsAndTricksTranslationService(updated)

--- service-api/src/main/java/greencity/dto/tipsandtricks/TipsAndTricksVO.java
+++ service-api/src/main/java/greencity/dto/tipsandtricks/TipsAndTricksVO.java
@@ -5,12 +5,14 @@ import greencity.dto.user.UserVO;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class TipsAndTricksVO {
     private Long id;
