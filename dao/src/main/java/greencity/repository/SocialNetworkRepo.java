af18e079e43a2975e519eb92113dd27399ab91bb
forestik <orest199910@gmail.com>
2020-11-24 17:39:17 +0200

    Annotation

--- dao/src/main/java/greencity/repository/SocialNetworkRepo.java
+++ dao/src/main/java/greencity/repository/SocialNetworkRepo.java
@@ -2,6 +2,8 @@ package greencity.repository;
 
 import greencity.entity.SocialNetwork;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SocialNetworkRepo extends JpaRepository<SocialNetwork, Long> {
 }
