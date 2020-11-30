adfb0b7e21f52b8f231e16b7d060d2faf767ea46
airetOK <sergey.bezrukavyy@gmail.com>
2020-11-30 20:01:00 +0200

    Add tests for AchievementServiceImpl

--- service/src/test/java/greencity/service/AchievementServiceImplTest.java
+++ service/src/test/java/greencity/service/AchievementServiceImplTest.java
@@ -1,20 +1,34 @@
 package greencity.service;
 
-import greencity.dto.achievement.AchievementDTO;
+import greencity.constant.ErrorMessage;
+import greencity.dto.PageableAdvancedDto;
+import greencity.dto.achievement.*;
+import greencity.dto.achievementcategory.AchievementCategoryDto;
+import greencity.dto.achievementcategory.AchievementCategoryVO;
+import greencity.dto.language.LanguageVO;
+import greencity.dto.user.UserVO;
 import greencity.entity.Achievement;
+import greencity.entity.AchievementCategory;
+import greencity.entity.Language;
+import greencity.entity.localization.AchievementTranslation;
+import greencity.exception.exceptions.NotDeletedException;
+import greencity.exception.exceptions.NotUpdatedException;
 import greencity.repository.AchievementRepo;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class AchievementServiceImplTest {
@@ -22,6 +36,10 @@ class AchievementServiceImplTest {
     private AchievementRepo achievementRepo;
     @Mock
     private ModelMapper modelMapper;
+    @Mock
+    private AchievementCategoryService achievementCategoryService;
+    @Mock
+    private UserService userService;
     @InjectMocks
     private AchievementServiceImpl achievementService;
 
@@ -41,5 +59,121 @@ class AchievementServiceImplTest {
             .thenReturn(new AchievementDTO(achievement.getId(), null, null, null));
         List<AchievementDTO> findAllResult = achievementService.findAll();
         assertEquals(1L, (long) findAllResult.get(0).getId());
+    }
+
+    @Test
+    void findAllByPageableTest() {
+        Pageable pageable = PageRequest.of(0,2);
+        Achievement achievement = new Achievement();
+        AchievementVO achievementVO = new AchievementVO();
+        Page<Achievement> pages = new PageImpl<>(Collections.singletonList(achievement),pageable,10);
+        when(achievementRepo.findAll(pageable)).thenReturn(pages);
+        when(modelMapper.map(achievement, AchievementVO.class)).thenReturn(achievementVO);
+        PageableAdvancedDto<AchievementVO> pageableAdvancedDto = achievementService.findAll(pageable);
+        assertEquals(10, pageableAdvancedDto.getTotalElements());
+    }
+
+    @Test
+    void searchAchievementByTest() {
+        Pageable pageable = PageRequest.of(0,2);
+        Achievement achievement = new Achievement();
+        Page<Achievement> page = new PageImpl<>(Collections.singletonList(achievement),pageable,10);
+        AchievementVO achievementVO = new AchievementVO();
+        when(achievementRepo.searchAchievementsBy(pageable,"")).thenReturn(page);
+        when(modelMapper.map(achievement, AchievementVO.class)).thenReturn(achievementVO);
+        PageableAdvancedDto<AchievementVO> pageableAdvancedDto = achievementService.searchAchievementBy(pageable,"");
+        assertEquals(10, pageableAdvancedDto.getTotalElements());
+    }
+
+    @Test
+    public void saveTest() {
+        Achievement achievement = new Achievement();
+        achievement.setTranslations(new ArrayList<>());
+        AchievementCategory achievementCategory = new AchievementCategory();
+        AchievementCategoryDto achievementCategoryDto = new AchievementCategoryDto("Test");
+        AchievementPostDto achievementPostDto = new AchievementPostDto();
+        achievementPostDto.setAchievementCategory(achievementCategoryDto);
+        AchievementCategoryVO achievementCategoryVO = new AchievementCategoryVO();
+        AchievementVO achievementVO = new AchievementVO();
+        UserVO userVO = new UserVO();
+        UserAchievementVO userAchievement = new UserAchievementVO();
+        List <UserAchievementVO> userAchievements = new ArrayList<>();
+        userAchievements.add(userAchievement);
+        userVO.setUserAchievements(userAchievements);
+        when(modelMapper.map(achievementPostDto, Achievement.class)).thenReturn(achievement);
+        when(achievementCategoryService.findByName("Test")).thenReturn(achievementCategoryVO);
+        when(modelMapper.map(achievementCategoryVO, AchievementCategory.class)).thenReturn(achievementCategory);
+        when(achievementRepo.save(achievement)).thenReturn(achievement);
+        when(modelMapper.map(achievement, AchievementVO.class)).thenReturn(achievementVO);
+        when(userService.findAll()).thenReturn(Collections.singletonList(userVO));
+        AchievementVO expected = achievementService.save(achievementPostDto);
+        assertEquals(expected, achievementVO);
+    }
+
+    @Test
+    void findByIdTest() {
+        Achievement achievement = new Achievement(1L, null, null, null, 1);
+        AchievementVO achievementVO = new AchievementVO();
+        when(achievementRepo.findById(1L)).thenReturn(Optional.of(achievement));
+        when(modelMapper.map(achievement, AchievementVO.class)).thenReturn(achievementVO);
+        AchievementVO expected = achievementService.findById(1L);
+        assertEquals(expected, achievementVO);
+    }
+
+    @Test
+    void updateTest() {
+        Achievement achievement = new Achievement(1L, null, null, null, 1);
+        AchievementTranslation achievementTranslation = new AchievementTranslation();
+        Language language = new Language();
+        language.setCode("Code");
+        achievementTranslation.setLanguage(language);
+        achievement.setTranslations(Collections.singletonList(achievementTranslation));
+        AchievementPostDto achievementPostDto = new AchievementPostDto();
+        AchievementManagementDto achievementManagementDto = new AchievementManagementDto(1L);
+        AchievementTranslationVO achievementTranslationVO = new AchievementTranslationVO();
+        LanguageVO languageVO = new LanguageVO(1L,"Code");
+        achievementTranslationVO.setLanguage(languageVO);
+        achievementManagementDto.setTranslations(Collections.singletonList(achievementTranslationVO));
+        achievementPostDto.setTranslations(Collections.singletonList(new AchievementTranslationVO()));
+        when(achievementRepo.findById(1L)).thenReturn(Optional.of(achievement));
+        when(achievementRepo.save(achievement)).thenReturn(achievement);
+        when(modelMapper.map(achievement, AchievementPostDto.class)).thenReturn(achievementPostDto);
+        AchievementPostDto expected = achievementService.update(achievementManagementDto);
+        assertEquals(expected, achievementPostDto);
+    }
+
+    @Test
+    void updateWithUnknownId() {
+        AchievementManagementDto achievementManagementDto = new AchievementManagementDto(345L);
+        when(achievementRepo.findById(achievementManagementDto.getId()))
+            .thenThrow(new NotUpdatedException(ErrorMessage.ACHIEVEMENT_NOT_FOUND_BY_ID  + achievementManagementDto.getId()));
+        assertThrows(NotUpdatedException.class, () -> achievementService.update(achievementManagementDto));
+    }
+
+    @Test
+    void deleteTest() {
+        Achievement achievement = new Achievement(1L, null, null, null, 1);
+        doNothing().when(achievementRepo).deleteById(1L);
+        achievementRepo.deleteById(1L);
+        verify(achievementRepo, times(1)).deleteById(1L);
+        long expected = achievementService.delete(1L);
+        Assertions.assertEquals(expected, achievement.getId());
+    }
+
+    @Test
+    void deleteWithNonExistingId() {
+        doThrow(EmptyResultDataAccessException.class).when(achievementRepo).deleteById(345L);
+        assertThrows(NotDeletedException.class, () -> achievementService.delete(345L) );
+    }
+
+    @Test
+    void deleteAll() {
+        List<Long> listId = Arrays.asList(1L, 2L, 3L, 4L);
+        listId.forEach(l -> {
+            doNothing().when(achievementRepo).deleteById(l);
+            achievementRepo.deleteById(l);
+            verify(achievementRepo, times(1)).deleteById(l);
+        });
+        achievementService.deleteAll(listId);
     }
 }
