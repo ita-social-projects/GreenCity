1cbc65fe568be795f5ab04f85a352e9fd4f7c489
Yurii Savchenko <43988243+Yura09@users.noreply.github.com>
2020-12-03 15:56:03 +0200

    updated

--- dao/src/main/java/greencity/repository/TipsAndTricksRepo.java
+++ dao/src/main/java/greencity/repository/TipsAndTricksRepo.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TipsAndTricksRepo extends JpaRepository<TipsAndTricks, Long>,
     JpaSpecificationExecutor<TipsAndTricks> {
-        
     /**
      * Method returns {@link TipsAndTricks} by specific tags.
      *
