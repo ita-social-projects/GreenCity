c1539d8d65b68b7723cf85dbdd4201371c15c9b0
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-12-06 16:04:06 +0200

    add cookies for each AJAX request

--- service/src/main/java/greencity/security/service/TokenServiceImpl.java
+++ service/src/main/java/greencity/security/service/TokenServiceImpl.java
@@ -1,8 +1,11 @@
 package greencity.security.service;
 
+import org.springframework.stereotype.Service;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+@Service
 public class TokenServiceImpl implements TokenService {
     /**
      * {@inheritDoc}
@@ -11,7 +14,6 @@ public class TokenServiceImpl implements TokenService {
     public void passTokenToCookies(String accessToken, HttpServletResponse response) {
         Cookie cookie = new Cookie("accessToken", accessToken);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
         response.addCookie(cookie);
     }
 }
