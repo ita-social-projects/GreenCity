556345816487612ef3166bfdb06981d6239637fc
Terry <taraskavkalo@gmail.com>
2020-12-10 16:47:12 +0200

    add unit tests for AchievementVOMapper

--- dao/src/main/java/greencity/entity/AchievementCategory.java
+++ dao/src/main/java/greencity/entity/AchievementCategory.java
@@ -1,9 +1,6 @@
 package greencity.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "achievement_categories")
+@EqualsAndHashCode
 public class AchievementCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
