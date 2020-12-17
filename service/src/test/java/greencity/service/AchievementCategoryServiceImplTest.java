abad4a3f59dd8cce6679b4e43b4737c19e7ef6c1
airetOK <sergey.bezrukavyy@gmail.com>
2020-12-17 13:30:24 +0100

    Fix code smell

--- service/src/test/java/greencity/service/AchievementCategoryServiceImplTest.java
+++ service/src/test/java/greencity/service/AchievementCategoryServiceImplTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class AchievementCategoryServiceImplTest {
+class AchievementCategoryServiceImplTest {
 
     @Mock
     private AchievementCategoryRepo achievementCategoryRepo;
