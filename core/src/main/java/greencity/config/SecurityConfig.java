175a0ed1a3a4eec7b7c5e14a14080cf2557110aa
Yurii Savchenko <43988243+Yura09@users.noreply.github.com>
2020-12-29 11:16:13 +0200

    Update SecurityConfig.java

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -188,7 +188,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/user/userAndAllFriendsWithOnlineStatus",
                 "/user/{userId}/recommendedFriends/",
                 "/user/{userId}/friends/",
-                "/user/{userId}/friendRequests/")
+                "/user/{userId}/friendRequests/",
                 "/chat")
             .hasAnyRole(USER, ADMIN, MODERATOR)
             .antMatchers(HttpMethod.POST,
