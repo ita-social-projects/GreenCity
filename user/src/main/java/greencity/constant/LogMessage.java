0278cdf4d300950206c7b6e5111f741a2203a2f4
forestik <orest199910@gmail.com>
2020-12-29 16:32:37 +0200

    Controllers for RestTemplate

--- user/src/main/java/greencity/constant/LogMessage.java
+++
@@ -1,8 +0,0 @@
-package greencity.constant;
-
-public final class LogMessage {
-    public static final String IN_FIND_ID_BY_EMAIL = "in findIdByEmail(), email: {}";
-
-    private LogMessage() {
-    }
-}
--- user/src/main/java/greencity/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package greencity.enums;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_MODERATOR
-}
