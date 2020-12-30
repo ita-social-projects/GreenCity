d4b7659314a1d930388dea63a8861c7eb53025cf
Skajl-dev <69860972+Skajl-dev@users.noreply.github.com>
2020-12-30 18:00:34 +0000

    Deploying to gh-pages from  @ 0ff8f8082b4bce810424613a4a3b3991aa640dc0 🚀

--- service/src/main/java/greencity/service/HabitAssignServiceImpl.java
+++ service/src/main/java/greencity/service/HabitAssignServiceImpl.java
@@ -118,7 +118,7 @@ public class HabitAssignServiceImpl implements HabitAssignService {
         return habitAssignRepo.save(
             HabitAssign.builder()
                 .habit(habit)
-                .status(HabitAssignStatus.ACTIVE)
+                .status(HabitAssignStatus.INPROGRESS)
                 .createDate(ZonedDateTime.now())
                 .user(user)
                 .duration(0)
