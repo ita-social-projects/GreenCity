6df5fa8df859a4b53d0710d176b16beed94630bd
ruslana <ruslanka5555gmail.com>
2021-01-05 17:44:19 +0200

    AdviceSpecificationTest

---
+++ service/src/test/java/greencity/filters/RatingStatisticsSpecificationTest.java
@@ -0,0 +1,15 @@
+package greencity.filters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import org.junit.jupiter.api.Test;
+
+class RatingStatisticsSpecificationTest {
+
+    @Test
+    void toPredicate() {
+        CriteriaBuilder criteriaBuilder;
+
+    }
+}
