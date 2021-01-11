4ebe2f700d9b412b44b0ae58534d7d905f45c204
Skajl-dev <bilonigka19999@gmail.com>
2021-01-11 11:59:28 +0200

    fix back

--- email/src/main/java/greencity/service/impl/EmailServiceImpl.java
+++ email/src/main/java/greencity/service/impl/EmailServiceImpl.java
@@ -193,7 +193,7 @@ public class EmailServiceImpl implements EmailService {
         Locale rus = new Locale("ru", "RU");
         Locale ua = new Locale("uk", "UA");
         switch (language) {
-            case "uk":
+            case "ua":
                 Locale.setDefault(ua);
                 break;
             case "ru":
