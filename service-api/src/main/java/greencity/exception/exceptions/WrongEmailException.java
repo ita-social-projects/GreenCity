78e8029edb9e61290456461e3f9a33099775c686
terrytarik <40128049+terrytarik@users.noreply.github.com>
2021-01-14 18:10:32 +0000

    Deploying to gh-pages from  @ 94fa34b2389d96828b2eabce38c8ec1b8c9d6c77 🚀

--- service-api/src/main/java/greencity/exception/exceptions/WrongEmailException.java
+++ service-api/src/main/java/greencity/exception/exceptions/WrongEmailException.java
@@ -1,8 +1,12 @@
 package greencity.exception.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception that we get when user by this email not found.
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class WrongEmailException extends RuntimeException {
     /**
      * Constructor.
