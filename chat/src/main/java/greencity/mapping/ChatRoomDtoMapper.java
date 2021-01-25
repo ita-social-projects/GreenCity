b05396dc41e08a359cca27defa33704e560579fb
airetOK <sergey.bezrukavyy@gmail.com>
2021-01-25 22:58:18 +0100

    Fix code smell

--- chat/src/main/java/greencity/mapping/ChatRoomDtoMapper.java
+++ chat/src/main/java/greencity/mapping/ChatRoomDtoMapper.java
@@ -7,7 +7,6 @@ import greencity.entity.ChatRoom;
 import java.util.stream.Collectors;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
