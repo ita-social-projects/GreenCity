354ba98a4934d0af87a63dd825d9d07a2605fd66
ruslana <ruslanka5555gmail.com>
2021-02-16 12:30:56 +0200

    bug fix, added UserHasNoPermissionToAccessException

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -434,6 +434,22 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
+     * Method intercept exception {@link UserHasNoPermissionToAccessException}.
+     * 
+     * @param ex      Exception witch should be intercepted.
+     * @param request contain detail about occur exception
+     * @return ResponseEntity witch contain http status and body with message of
+     *         exception.
+     */
+    @ExceptionHandler(UserHasNoPermissionToAccessException.class)
+    public final ResponseEntity<Object> handleUserHasNoPermissionToAccessException(
+        UserHasNoPermissionToAccessException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(getErrorAttributes(request));
+        log.trace(ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exceptionResponse);
+    }
+
+    /**
      * Customize the response for HttpMessageNotReadableException.
      *
      * @param ex      the exception
