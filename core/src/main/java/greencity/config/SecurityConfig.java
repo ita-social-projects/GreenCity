9fa6e6d4d688c42f5c950d2a989762e622c5544a
ViktoriyaRyazhska <vryazhskaya@gmail.com>
2021-02-22 09:19:06 +0000

    Deploying to gh-pages from  @ 6515545230d73c74adb33a1e5fc411c5917b6c84 🚀

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -243,6 +243,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 HABIT_ASSIGN_ID,
                 "/shopping-list-items/shoppingList/{userId}",
                 HABIT_ASSIGN_ID,
+                "/habit/assign/cancel/{habitId}",
                 TIPS_AND_TRICKS_COMMENTS,
                 USER_CUSTOM_SHOPPING_LIST_ITEMS,
                 "/user/shopping-list-items/{userShoppingListItemId}",
