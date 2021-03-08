918d55802433a6548c0a94f61993ac8ebc81742f
V-la-d <vladpikhotskyi@gmail.com>
2021-03-08 22:53:14 +0200

    update_user_achievement

--- dao/src/main/java/greencity/entity/UserAchievement.java
+++ dao/src/main/java/greencity/entity/UserAchievement.java
@@ -6,12 +6,14 @@ import javax.persistence.*;
 import lombok.*;
 
 @Entity
-@Data
 @Table(name = "user_achievements")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
+@Getter
+@Setter
+
 public class UserAchievement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
