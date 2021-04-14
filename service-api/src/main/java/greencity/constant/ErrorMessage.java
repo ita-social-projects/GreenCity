f627385008534b2f0aeb281fb0a6388422e42d78
Zakhar <zaharverem@gmail.com>
2021-04-14 16:09:55 +0300

    CheckStyle

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -146,7 +146,6 @@ public final class ErrorMessage {
     public static final String PAGE_INDEX_IS_MORE_THAN_TOTAL_PAGES = "Page index is more than total pages: ";
     public static final String MultiPartFile_Bad_Request = "Bad inputed image string : ";
 
-
     private ErrorMessage() {
     }
 }
