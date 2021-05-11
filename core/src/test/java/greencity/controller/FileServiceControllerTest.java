0b4f890aed7d2bfe89ec260e45bce9cce10d9da0
adamos98 <Zal.pl13579*>
2021-05-11 17:19:01 +0200

    file service controller test commit

--- core/src/test/java/greencity/controller/FileServiceControllerTest.java
+++ core/src/test/java/greencity/controller/FileServiceControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
