cfddd4fbccc8c6a0be5100e3448eee6d0f58f08b
Володимир Лесько <lescko00@gmail.com>
2021-05-11 13:20:47 +0300

    Fix tests, remove some useless imports.

--- service-api/src/test/java/greencity/client/RestClientTest.java
+++ service-api/src/test/java/greencity/client/RestClientTest.java
@@ -19,8 +19,6 @@ import greencity.message.SendChangePlaceStatusEmailMessage;
 import greencity.message.SendHabitNotification;
 import greencity.message.SendReportEmailMessage;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -257,6 +255,7 @@ class RestClientTest {
         String accessToken = "accessToken";
         List<String> test = List.of("test", "test");
         HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set(AUTHORIZATION, accessToken);
         HttpEntity<List<String>> entity = new HttpEntity<>(test, headers);
         when(httpServletRequest.getHeader(AUTHORIZATION)).thenReturn(accessToken);
@@ -428,6 +427,7 @@ class RestClientTest {
         Pageable pageable = PageRequest.of(0, 20, Sort.unsorted());
         String accessToken = "accessToken";
         HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set(AUTHORIZATION, accessToken);
         UserManagementViewDto userViewDto =
             UserManagementViewDto.builder()
