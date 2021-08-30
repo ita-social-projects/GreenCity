b321cd61a95a07f9cebeac69dc2d6f7dbe801072
Vira Maksymets <viramaximets@gmail.com>
2021-08-30 12:07:43 +0300

    Added Javadoc comment for About Us controller

--- core/src/main/java/greencity/webcontroller/AboutUsController.java
+++ core/src/main/java/greencity/webcontroller/AboutUsController.java
@@ -6,12 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
-
 @Controller
 @RequestMapping("/management")
 @RequiredArgsConstructor
 public class AboutUsController {
+    /**
+     * Show About Us page.
+     *
+     * @param model model
+     * @return {@link String} path to the template part.
+     * @author Vira Maksymets
+     */
     @GetMapping("/aboutus")
     public String aboutUs(Model model) {
         return "core/about_us";
