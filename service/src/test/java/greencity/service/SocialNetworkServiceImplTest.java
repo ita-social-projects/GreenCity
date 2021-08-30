45f92f1609a53a1166c3af46e94d400ff45af396
YevhenZzz <Bufan7140911@gmail.com>
2021-08-30 10:26:05 +0000

    Deploying to gh-pages from  @ 71fce782e8f850d72e98e3c0720e937542cd87ac 🚀

--- service/src/test/java/greencity/service/SocialNetworkServiceImplTest.java
+++ service/src/test/java/greencity/service/SocialNetworkServiceImplTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class SocialNetworkServiceImplTest {
+class SocialNetworkServiceImplTest {
 
     @Mock
     private SocialNetworkRepo socialNetworkRepo;
