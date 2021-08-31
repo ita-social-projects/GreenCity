baf201e310b8c4bbb4772e72bde2d16081540117
YevhenZzz <Bufan7140911@gmail.com>
2021-08-31 12:45:38 +0000

    Deploying to gh-pages from  @ b6d5d40e8384f3baa12b9a6623d930913642de44 🚀

--- service/src/test/java/greencity/service/TipsAndTricksTranslationServiceImplTest.java
+++ service/src/test/java/greencity/service/TipsAndTricksTranslationServiceImplTest.java
@@ -8,6 +8,7 @@ import greencity.entity.TextTranslation;
 import greencity.entity.TitleTranslation;
 import greencity.repository.TextTranslationRepo;
 import greencity.repository.TitleTranslationRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,6 +17,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,8 +38,7 @@ class TipsAndTricksTranslationServiceImplTest {
     @Test
     void saveTitleTranslationsTest() {
         TitleTranslationVO titleTranslationVO = ModelUtils.getTitleTranslationVO();
-        List<TitleTranslationVO> titleTranslationVOList = Collections.singletonList(titleTranslationVO);
-
+        List<TitleTranslationVO> titleTranslationVOs = Collections.singletonList(titleTranslationVO);
         TitleTranslation titleTranslation = TitleTranslation.builder()
             .id(titleTranslationVO.getId())
             .content(titleTranslationVO.getContent())
@@ -46,18 +47,17 @@ class TipsAndTricksTranslationServiceImplTest {
                 .code(titleTranslationVO.getLanguage().getCode())
                 .build())
             .build();
-        List<TitleTranslation> titleTranslationList = Collections.singletonList(titleTranslation);
+        List<TitleTranslation> titleTranslations = Collections.singletonList(titleTranslation);
 
-        when(modelMapper.map(titleTranslationVOList, new TypeToken<List<TitleTranslation>>() {
-        }.getType())).thenReturn(titleTranslationList);
+        when(modelMapper.map(titleTranslationVOs, new TypeToken<List<TitleTranslation>>() {
+        }.getType())).thenReturn(titleTranslations);
+        when(titleTranslationRepo.saveAll(titleTranslations)).thenReturn(titleTranslations);
 
-        when(titleTranslationRepo.saveAll(titleTranslationList)).thenReturn(titleTranslationList);
-
-        when(modelMapper.map(titleTranslationList, new TypeToken<List<TitleTranslationVO>>() {
-        }.getType())).thenReturn(titleTranslationVOList);
-
+        when(modelMapper.map(titleTranslations, new TypeToken<List<TitleTranslationVO>>() {
+        }.getType())).thenReturn(titleTranslationVOs);
         titleTranslationRepo.saveAll(Collections.singletonList(titleTranslation));
-        verify(titleTranslationRepo).saveAll(Collections.singletonList(titleTranslation));
+        List<TitleTranslationVO> expected = Collections.singletonList(titleTranslationVO);
+        Assertions.assertEquals(expected, tipsAndTricksTranslationService.saveTitleTranslations(titleTranslationVOs));
     }
 
     @Test
@@ -81,6 +81,7 @@ class TipsAndTricksTranslationServiceImplTest {
         }.getType())).thenReturn(textTranslationVOList);
 
         textTranslationRepo.saveAll(Collections.singletonList(textTranslation));
-        verify(textTranslationRepo).saveAll(Collections.singletonList(textTranslation));
+        List<TextTranslationVO> expected = Collections.singletonList(textTranslationVO);
+        Assertions.assertEquals(expected, tipsAndTricksTranslationService.saveTextTranslations(textTranslationVOList));
     }
 }
