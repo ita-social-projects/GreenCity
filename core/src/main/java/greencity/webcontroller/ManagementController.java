a7d0e9f9de2fe4bf527666a3511f75e0c2b7ee62
volianskyi <62424161+volianskyi@users.noreply.github.com>
2021-09-02 12:00:46 +0000

    Deploying to gh-pages from  @ eb08d1c022f033bbf182cd53cf5b4fa0324c748e 🚀

--- core/src/main/java/greencity/webcontroller/ManagementController.java
+++ core/src/main/java/greencity/webcontroller/ManagementController.java
@@ -1,14 +1,19 @@
 package greencity.webcontroller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/management")
+@RequestMapping
 public class ManagementController {
+    @Value("${greencityuser.server.address}")
+    private String greenCityUserServerAddress;
+
     /**
      * Returns index page.
      *
@@ -16,8 +21,37 @@ public class ManagementController {
      * @author Dovganyuk Taras
      */
 
-    @GetMapping
+    @GetMapping("/management")
     public String goToIndex() {
         return "core/index";
+    }
+
+    /**
+     * Redirect to logIn page.
+     *
+     * @author Ihor Volianskyi
+     */
+
+    @GetMapping("/")
+    public String redirectLogin() {
+        return "redirect:/management/login";
+    }
+
+    /**
+     * Redirect to logIn page on GreenCityUser or redirect to management page if you
+     * already sign in.
+     *
+     * @author Ihor Volianskyi
+     */
+
+    @GetMapping("/management/login")
+    public String login() {
+        boolean anonymousUser =
+            SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser");
+        if (anonymousUser) {
+            return "redirect:" + greenCityUserServerAddress + "/management/login";
+        } else {
+            return "redirect:/management";
+        }
     }
 }
