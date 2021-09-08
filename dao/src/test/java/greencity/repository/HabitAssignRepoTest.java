3dc31143f55473d9ad75a543155d0994e60bc496
Vira Maksymets <viramaximets@gmail.com>
2021-09-08 17:47:11 +0300

    test fixed

--- dao/src/test/java/greencity/repository/HabitAssignRepoTest.java
+++ dao/src/test/java/greencity/repository/HabitAssignRepoTest.java
@@ -26,22 +26,22 @@ class HabitAssignRepoTest {
 
     @Test
     void findByHabitIdAndUserIdAndStatusIsCancelledTest() {
-        Long expected = 5L;
-        Long actual = habitAssignRepo.findByHabitIdAndUserIdAndStatusIsCancelled(1L, 2L).getId();
+        Long expected = 2L;
+        Long actual = habitAssignRepo.findByHabitIdAndUserIdAndStatusIsCancelled(2L, 2L).getId();
         assertEquals(expected, actual);
     }
 
     @Test
     void findAllByUserIdAndStatusAcquiredTest() {
-        List<HabitAssign> habitAssignList = habitAssignRepo.findAllByUserIdAndStatusAcquired(2L);
+        List<HabitAssign> habitAssignList = habitAssignRepo.findAllByUserIdAndStatusAcquired(3L);
         assertEquals(1L, habitAssignList.size());
-        assertEquals(2L, habitAssignList.get(0).getId());
+        assertEquals(4L, habitAssignList.get(0).getId());
     }
 
     @Test
     void findAllHabitAssignsByStatusTest() {
         List<HabitAssign> habitAssignList =
-            habitAssignRepo.findAllHabitAssignsByStatusAndHabitId(HabitAssignStatus.ACQUIRED, 1L);
-        assertEquals(2L, habitAssignList.size());
+            habitAssignRepo.findAllHabitAssignsByStatusAndHabitId(HabitAssignStatus.INPROGRESS, 1L);
+        assertEquals(1L, habitAssignList.size());
     }
 }
