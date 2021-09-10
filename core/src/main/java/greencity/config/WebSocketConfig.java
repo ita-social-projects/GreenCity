67cf78f5871d472ab4e31a15579f8137e4a1d1b6
YevhenZzz <bufan7140911@gmail.com>
2021-09-10 13:37:39 +0300

    formatter and checkstyle

--- core/src/main/java/greencity/config/WebSocketConfig.java
+++ core/src/main/java/greencity/config/WebSocketConfig.java
@@ -28,15 +28,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             .setAllowedOrigins("*")
             .withSockJS();
     }
-
-//    @Override
-//    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-//        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
-//        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
-//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//        converter.setObjectMapper(new ObjectMapper());
-//        converter.setContentTypeResolver(resolver);
-//        messageConverters.add(converter);
-//        return false;
-//    }
 }
