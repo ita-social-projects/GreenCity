b38de5ca125b7251d8ce7dde841f892a7bcbdc20
Vadym Makitra <hmarax3@gmail.com>
2021-09-14 10:58:43 +0300

    Fixed trouble when controller return 500 response if user entered not supported criteria

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -531,6 +531,14 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
     }
 
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public final ResponseEntity<Object> handleUnsuportedSortException(
+            UnsupportedOperationException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(getErrorAttributes(request));
+        log.trace(ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
+    }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
         MethodArgumentNotValidException ex,
--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -151,6 +151,7 @@ public final class ErrorMessage {
     public static final String INCORRECT_INPUT_ITEM_STATUS = "Incorrect input status to update item.";
     public static final String AZURE_NOT_CONNECTED = "Invalid connection string for Azure";
     public static final String HABIT_ASSIGN_STATUS_IS_NOT_INPROGRESS = "Habit assign status is not INPROGRESS.";
+    public static final String INVALID_SORTING_VALUE = "Supported sort is: asc|desc";
 
     private ErrorMessage() {
     }
