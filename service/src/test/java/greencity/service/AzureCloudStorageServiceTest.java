5bbbe9e7845d57111c0b7d95c69c083bd12c93b4
YevhenZzz <Bufan7140911@gmail.com>
2021-09-30 17:09:33 +0000

    Deploying to gh-pages from  @ f28bb89b6c4527529ce7fdbf29a4d8fd71de737b 🚀

--- service/src/test/java/greencity/service/AzureCloudStorageServiceTest.java
+++ service/src/test/java/greencity/service/AzureCloudStorageServiceTest.java
@@ -7,9 +7,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.core.env.PropertyResolver;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -29,5 +33,12 @@ class AzureCloudStorageServiceTest {
     void convertToMultipartImageThrowsBadRequestException() {
         when(modelMapper.map("Image", MultipartFile.class)).thenThrow(new BadRequestException("S"));
         assertThrows(BadRequestException.class, () -> azureCloudStorageService.convertToMultipartImage("Image"));
+    }
+
+    @Test
+    void convertToMultipartImage() {
+        MultipartFile multipartFile = new MockMultipartFile("Image", "Image".getBytes(StandardCharsets.UTF_8));
+        when(modelMapper.map("Image", MultipartFile.class)).thenReturn(multipartFile);
+        assertEquals(multipartFile, azureCloudStorageService.convertToMultipartImage("Image"));
     }
 }
