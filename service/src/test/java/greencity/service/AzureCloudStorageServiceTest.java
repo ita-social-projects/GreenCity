8f44a1b1e19b185a62b3d848349054301bba21e4
YevhenZzz <bufan7140911@gmail.com>
2021-09-30 16:20:12 +0300

    removed code smell

--- service/src/test/java/greencity/service/AzureCloudStorageServiceTest.java
+++ service/src/test/java/greencity/service/AzureCloudStorageServiceTest.java
@@ -1,14 +1,19 @@
 package greencity.service;
 
+import greencity.ModelUtils;
 import greencity.exception.exceptions.BadRequestException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.core.env.PropertyResolver;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -24,8 +29,11 @@ class AzureCloudStorageServiceTest {
 
     @Test
     void convertToMultipartImageThrowsBadRequestException() {
+        MultipartFile expected = ModelUtils.getFile();
+        MultipartFile multipartFile = new MockMultipartFile("Image", "sada".getBytes(StandardCharsets.UTF_8));
         AzureCloudStorageService azureCloudStorageService = new AzureCloudStorageService(propertyResolver, modelMapper);
-        when(modelMapper.map("Image", MultipartFile.class)).thenReturn((null));
+        when(modelMapper.map("Image", MultipartFile.class)).thenReturn((multipartFile));
+        assertEquals(expected.isEmpty(), azureCloudStorageService.convertToMultipartImage("Image").isEmpty());
         assertThrows(BadRequestException.class, () -> azureCloudStorageService.convertToMultipartImage(any()));
     }
 }
