71a809ee5088877f9a9324209bfc7ae3d56e80c7
Max Bohonko <44169737+Max877866@users.noreply.github.com>
2022-01-18 00:19:05 +0200

    Set shopping list items to HabitAssignDto (#3758)
    * Set shopping list items to HabitAssignDto

--- service/src/test/java/greencity/ModelUtils.java
+++ service/src/test/java/greencity/ModelUtils.java
@@ -1380,6 +1380,7 @@ public class ModelUtils {
     public static ShoppingListItem getShoppingListItem() {
         return ShoppingListItem.builder()
             .id(1L)
+            .translations(getShoppingListItemTranslations())
             .build();
     }
 
