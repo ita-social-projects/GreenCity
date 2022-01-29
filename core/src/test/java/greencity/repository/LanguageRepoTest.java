97a1c3376e1654d4180bf5e08757c23a1ff0ba29
Natalia <nkozak4818@gmail.com>
2022-01-30 00:32:16 +0200

    added docker container

---
+++ core/src/test/java/greencity/repository/LanguageRepoTest.java
@@ -0,0 +1,25 @@
+package greencity.repository;
+
+import greencity.GreenCityApplication;
+import greencity.IntegrationTestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = GreenCityApplication.class)
+public class LanguageRepoTest extends IntegrationTestBase {
+    @Autowired
+    private LanguageRepo languageRepo;
+    @Test
+    void findById() {
+        List<Object> expected = new ArrayList<>();
+        Assertions.assertEquals(expected, languageRepo.findAllLanguageCodes());
+    }
+}
