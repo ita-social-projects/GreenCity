175a4a60e229615d1dc5f07a01ccf7429f3c383a
Danylo Hlynskyi <danylohlynsky@gmail.com>
2022-05-17 13:11:39 +0300

    remove redundant import

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
