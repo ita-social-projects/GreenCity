eaf3ddcec9e06447c6a2b4c882af7901791f59c2
Danylo Hlynskyi <danylohlynsky@gmail.com>
2022-07-06 05:42:26 +0300

    event attenders functionality fixed

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -157,6 +157,9 @@ public final class ErrorMessage {
     public static final String NO_EVENT_LINK_OR_COORDINATES = "Invalid online-link or coordinates";
     public static final String EVENT_START_DATE_AFTER_FINISH_DATE_OR_IN_PAST =
         "Start date must be in future and before finish date";
+    public static final String HAVE_ALREADY_SUBSCRIBED_ON_EVENT = "You have already subscribed on this event";
+    public static final String EVENT_NOT_FOUND = "Event hasn't been found";
+
 
     private ErrorMessage() {
     }
