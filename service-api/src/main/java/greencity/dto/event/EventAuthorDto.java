e73f87a8419aaf1a8a5c0daacdfe72916216883d
NewArtMask <34275837+NewArtMask@users.noreply.github.com>
2022-07-27 15:48:23 +0000

    Deploying to gh-pages from  @ 3413863b4e4185e422c4bf1f9f5242f2f8d8dc5d 🚀

--- service-api/src/main/java/greencity/dto/event/EventAuthorDto.java
+++ service-api/src/main/java/greencity/dto/event/EventAuthorDto.java
@@ -10,4 +10,5 @@ import lombok.*;
 public class EventAuthorDto {
     private Long id;
     private String name;
+    private Double organizerRating;
 }
--- service/src/main/java/greencity/mapping/events/EventDtoMapper.java
+++ service/src/main/java/greencity/mapping/events/EventDtoMapper.java
@@ -40,7 +40,8 @@ public class EventDtoMapper extends AbstractConverter<Event, EventDto> {
         eventDto.setTitleImage(event.getTitleImage());
         eventDto.setOpen(event.isOpen());
         User organizer = event.getOrganizer();
-        eventDto.setOrganizer(EventAuthorDto.builder().id(organizer.getId()).name(organizer.getName()).build());
+        eventDto.setOrganizer(EventAuthorDto.builder().id(organizer.getId()).name(organizer.getName())
+            .organizerRating(organizer.getEventOrganizerRating()).build());
         eventDto.setDates(event.getDates().stream().map(this::convertEventDateLocation).collect(Collectors.toList()));
 
         List<TagUaEnDto> tagUaEnDtos = new ArrayList<>();
