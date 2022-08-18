faf8cee0646cc395da96babae2922df705292d61
OlegV <olegvatul@gmail.com>
2022-08-18 12:27:16 +0300

    removed redundant status codes (#4542)

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -175,8 +175,6 @@ public class EcoNewsController {
     @ApiOperation(value = "Get eco news by id.")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = HttpStatuses.OK),
-        @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
-        @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN),
         @ApiResponse(code = 404, message = HttpStatuses.NOT_FOUND)
     })
     @ApiLocale
