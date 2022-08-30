5951347fa65c3d0e5f48cbebbbbe36eba5f2755e
innayashna <inna.miasoid@gmail.com>
2022-08-30 16:02:06 +0300

    Code smells removed

--- dao/src/main/java/greencity/entity/event/EventComment.java
+++ dao/src/main/java/greencity/entity/event/EventComment.java
@@ -4,10 +4,7 @@ import greencity.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
