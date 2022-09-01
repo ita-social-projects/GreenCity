dd20185f251a2365ff75ca2ddc049ccb81b5a916
OlegVat <73220885+OlegVat@users.noreply.github.com>
2022-09-01 12:28:49 +0000

    Deploying to gh-pages from  @ 3e56f9865be4eb9c0a28d9443dab7ea3c0a805e7 🚀

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -53,7 +53,6 @@ public class EcoNewsController {
     @ApiOperation(value = "Get three last eco news.")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = HttpStatuses.OK),
-        @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
         @ApiResponse(code = 404, message = HttpStatuses.NOT_FOUND)
     })
     @GetMapping("/newest")
