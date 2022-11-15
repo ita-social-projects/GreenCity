837f32969f9b94e72b68094181ec62c01dceba10
NKorzhik <nikitakorzh10@gmail.com>
2022-11-15 09:15:45 +0000

    Deploying to gh-pages from  @ 86e2a6cdd40fc57e03815b9878a9bc28d742137d 🚀

---
+++ core/src/test/java/greencity/controller/EventsControllerTest.java
@@ -0,0 +1,70 @@
+package greencity.controller;
+
+import greencity.converters.UserArgumentResolver;
+import greencity.service.EventService;
+import greencity.service.UserService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.security.Principal;
+
+import static greencity.ModelUtils.getPrincipal;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class EventsControllerTest {
+
+    private static final String EVENTS_CONTROLLER_LINK = "/events";
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private EventsController eventsController;
+
+    @Mock
+    private EventService eventService;
+
+    @Mock
+    private UserService userService;
+    @Mock
+    private ModelMapper modelMapper;
+    private final Principal principal = getPrincipal();
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(eventsController)
+            .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver(),
+                new UserArgumentResolver(userService, modelMapper))
+            .build();
+    }
+
+    @Test
+    @SneakyThrows
+    void getEventsCreatedByUser() {
+        int pageNumber = 0;
+        int pageSize = 20;
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        mockMvc.perform(get(EVENTS_CONTROLLER_LINK + "/myEvents/createdEvents").principal(principal))
+            .andExpect(status().isOk());
+        verify(eventService).getEventsCreatedByUser(pageable, "test@gmail.com");
+
+    }
+
+}
