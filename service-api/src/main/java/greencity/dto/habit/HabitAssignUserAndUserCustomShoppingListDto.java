8ff83b7173773576446ce87eafff16223a4b6af0
NikitaKorzh <nik.korzh.ita@gmail.com>
2022-12-05 10:04:36 +0200

    change naming

--- service-api/src/main/java/greencity/dto/habit/HabitAssignUserAndUserCustomShoppingListDto.java
+++ service-api/src/main/java/greencity/dto/habit/HabitAssignUserAndUserCustomShoppingListDto.java
@@ -14,6 +14,6 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class HabitAssignUserAndUserCustomShoppingListDto {
-    List<UserShoppingListItemResponseDto> userShoppingListItemsDto;
+    List<UserShoppingListItemResponseDto> userShoppingListItemDto;
     List<CustomShoppingListItemResponseDto> customShoppingListItemDto;
 }
