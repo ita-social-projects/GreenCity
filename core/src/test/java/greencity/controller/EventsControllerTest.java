8eecd3321546bd3b26d5e4910905567b925dd1f5
Maksym Lenets <maximlenets@gmail.com>
2022-12-14 00:34:54 +0200

    update tests

--- core/src/test/java/greencity/controller/EventsControllerTest.java
+++ core/src/test/java/greencity/controller/EventsControllerTest.java
@@ -1,9 +1,20 @@
 package greencity.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import greencity.converters.UserArgumentResolver;
+import greencity.dto.PageableAdvancedDto;
+import greencity.dto.event.CoordinatesDto;
+import greencity.dto.event.EventAuthorDto;
+import greencity.dto.event.EventDateLocationDto;
+import greencity.dto.event.EventDto;
+import greencity.dto.tag.TagUaEnDto;
+import greencity.exception.exceptions.BadRequestException;
+import greencity.exception.exceptions.NotFoundException;
 import greencity.service.EventService;
 import greencity.service.UserService;
 import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,16 +27,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.security.Principal;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 import static greencity.ModelUtils.getPrincipal;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -74,12 +92,25 @@ class EventsControllerTest {
     void getUserEventsTest() {
         int pageNumber = 0;
         int pageSize = 20;
-
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
-        mockMvc.perform(get(EVENTS_CONTROLLER_LINK + "/myEvents").principal(principal))
-            .andExpect(status().isOk());
 
-        verify(eventService).getAllUserEvents(pageable, "test@gmail.com");
+        PageableAdvancedDto<EventDto> pageableAdvancedDto = getPageableAdvancedDtoEventDto();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        ObjectWriter ow = objectMapper.writer();
+        String expectedJson = ow.writeValueAsString(pageableAdvancedDto);
+
+        when(eventService.getAllUserEvents(pageable, principal.getName())).thenReturn(pageableAdvancedDto);
+
+        mockMvc.perform(get(EVENTS_CONTROLLER_LINK + "/myEvents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .principal(principal))
+            .andExpect(status().isOk())
+            .andExpect(content().json(expectedJson));
+
+        verify(eventService).getAllUserEvents(pageable, principal.getName());
     }
 
     @Test
@@ -89,7 +120,45 @@ class EventsControllerTest {
 
         mockMvc.perform(post(EVENTS_CONTROLLER_LINK + "/addAttender/{eventId}", eventId).principal(principal));
 
-        verify(eventService).addAttender(eventId, "test@gmail.com");
+        verify(eventService).addAttender(eventId, principal.getName());
+    }
+
+    @Test
+    @SneakyThrows
+    void addAttenderWithNotValidIdBadRequestTest() {
+        String notValidId = "id";
+        mockMvc.perform(post(EVENTS_CONTROLLER_LINK + "/addAttender/{eventId}", notValidId).principal(principal))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void addAttenderNotFoundTest() {
+        Long eventId = 1L;
+
+        doThrow(new NotFoundException("ErrorMessage"))
+            .when(eventService)
+            .addAttender(eventId, principal.getName());
+
+        Assertions.assertThatThrownBy(
+            () -> mockMvc.perform(post(EVENTS_CONTROLLER_LINK + "/addAttender/{eventId}", eventId).principal(principal))
+                .andExpect(status().isNotFound()))
+            .hasCause(new NotFoundException("ErrorMessage"));
+    }
+
+    @Test
+    @SneakyThrows
+    void addAttenderBadRequestTest() {
+        Long eventId = 1L;
+
+        doThrow(new BadRequestException("ErrorMessage"))
+            .when(eventService)
+            .addAttender(eventId, principal.getName());
+
+        Assertions.assertThatThrownBy(
+            () -> mockMvc.perform(post(EVENTS_CONTROLLER_LINK + "/addAttender/{eventId}", eventId).principal(principal))
+                .andExpect(status().isBadRequest()))
+            .hasCause(new BadRequestException("ErrorMessage"));
     }
 
     @Test
@@ -100,7 +169,28 @@ class EventsControllerTest {
         mockMvc.perform(delete(EVENTS_CONTROLLER_LINK + "/removeAttender/{eventId}", eventId).principal(principal))
             .andExpect(status().isOk());
 
-        verify(eventService).removeAttender(eventId, "test@gmail.com");
+        verify(eventService).removeAttender(eventId, principal.getName());
+    }
+
+    @Test
+    @SneakyThrows
+    void removeAttenderBadRequestTest() {
+        String notValidId = "id";
+        mockMvc.perform(delete(EVENTS_CONTROLLER_LINK + "/removeAttender/{eventId}", notValidId).principal(principal))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void removeAttenderNotFoundTest() {
+        Long eventId = 1L;
+        doThrow(new NotFoundException("ErrorMessage"))
+            .when(eventService)
+            .removeAttender(eventId, principal.getName());
+
+        Assertions.assertThatThrownBy(() -> mockMvc
+            .perform(delete(EVENTS_CONTROLLER_LINK + "/removeAttender/{eventId}", eventId).principal(principal))
+            .andExpect(status().isNotFound())).hasCause(new NotFoundException("ErrorMessage"));
     }
 
     @Test
@@ -113,17 +203,135 @@ class EventsControllerTest {
             .perform(post(EVENTS_CONTROLLER_LINK + "/rateEvent/{eventId}/{grade}", eventId, grade).principal(principal))
             .andExpect(status().isOk());
 
-        verify(eventService).rateEvent(eventId, "test@gmail.com", grade);
+        verify(eventService).rateEvent(eventId, principal.getName(), grade);
+    }
+
+    @Test
+    @SneakyThrows
+    void rateEventWithNotValidIdBadRequestTest() {
+        String notValidId = "id";
+        int grade = 2;
+
+        mockMvc
+            .perform(
+                post(EVENTS_CONTROLLER_LINK + "/rateEvent/{eventId}/{grade}", notValidId, grade).principal(principal))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void rateEventWithNotValidGradeBadRequestTest() {
+        Long eventId = 1L;
+        String notValidGrade = "grade";
+
+        mockMvc
+            .perform(post(EVENTS_CONTROLLER_LINK + "/rateEvent/{eventId}/{grade}", eventId, notValidGrade)
+                .principal(principal))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void rateEventNotFoundTest() {
+        Long eventId = 1L;
+        int grade = 2;
+
+        doThrow(new NotFoundException("ErrorMessage"))
+            .when(eventService)
+            .rateEvent(eventId, principal.getName(), grade);
+
+        Assertions.assertThatThrownBy(() -> mockMvc
+            .perform(post(EVENTS_CONTROLLER_LINK + "/rateEvent/{eventId}/{grade}", eventId, grade).principal(principal))
+            .andExpect(status().isNotFound())).hasCause(new NotFoundException("ErrorMessage"));
+    }
+
+    @Test
+    @SneakyThrows
+    void rateEventBadRequestTest() {
+        Long eventId = 1L;
+        int grade = 2;
+
+        doThrow(new BadRequestException("ErrorMessage"))
+            .when(eventService)
+            .rateEvent(eventId, principal.getName(), grade);
+
+        Assertions.assertThatThrownBy(() -> mockMvc
+            .perform(post(EVENTS_CONTROLLER_LINK + "/rateEvent/{eventId}/{grade}", eventId, grade).principal(principal))
+            .andExpect(status().isBadRequest())).hasCause(new BadRequestException("ErrorMessage"));
     }
 
     @Test
     @SneakyThrows
     void getAllEventSubscribersTest() {
         Long eventId = 1L;
-
         mockMvc.perform(get(EVENTS_CONTROLLER_LINK + "/getAllSubscribers/{eventId}", eventId))
             .andExpect(status().isOk());
 
         verify(eventService).getAllEventAttenders(eventId);
+    }
+
+    @Test
+    @SneakyThrows
+    void getAllEventSubscribersWithNotValidIdBadRequestTest() {
+        String notValidId = "id";
+        mockMvc.perform(get(EVENTS_CONTROLLER_LINK + "/getAllSubscribers/{eventId}", notValidId))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void getAllEventSubscribersNotFoundTest() {
+        Long eventId = 1L;
+
+        doThrow(new NotFoundException("ErrorMessage"))
+            .when(eventService)
+            .getAllEventAttenders(eventId);
+
+        Assertions.assertThatThrownBy(
+            () -> mockMvc.perform(get(EVENTS_CONTROLLER_LINK + "/getAllSubscribers/{eventId}", eventId))
+                .andExpect(status().isNotFound()))
+            .hasCause(new NotFoundException("ErrorMessage"));
+    }
+
+    private PageableAdvancedDto<EventDto> getPageableAdvancedDtoEventDto() {
+        EventDto eventDto = EventDto.builder()
+            .id(11L)
+            .title("Test-Title")
+            .organizer(EventAuthorDto.builder()
+                .id(12L)
+                .name("Organizer-Name")
+                .organizerRating(1.1D)
+                .build())
+            .description("Event Description")
+            .dates(List.of(EventDateLocationDto.builder()
+                .id(13L)
+                .startDate(ZonedDateTime.of(2023, 6, 1, 1, 2, 3, 4, ZoneId.of("Europe/Kiev")))
+                .finishDate(ZonedDateTime.of(2023, 6, 1, 3, 2, 3, 4, ZoneId.of("Europe/Kiev")))
+                .onlineLink("some-link.com")
+                .coordinates(CoordinatesDto.builder()
+                    .addressEn("address-1")
+                    .addressUa("адреса-1")
+                    .latitude(50.45594503475653d)
+                    .longitude(30.5058935778292d).build())
+                .build()))
+            .tags(List.of(TagUaEnDto.builder()
+                .id(20)
+                .nameEn("Name")
+                .nameUa("Назва").build()))
+            .titleImage("https://csb10032000a548f571.blob.core.windows.net/all/8f09887c.png")
+            .additionalImages(List.of("https://csb10032000a548f571.blob.core.windows.net/all/10005000.png"))
+            .isOpen(true)
+            .isSubscribed(true).build();
+
+        return new PageableAdvancedDto<>(
+            List.of(eventDto),
+            1,
+            0,
+            1,
+            0,
+            false,
+            false,
+            true,
+            true);
     }
 }
