51098935c46be4307dae71cf2340b99b304c40aa
Maksym Golik <maksym.golik@gmail.com>
2023-03-19 19:30:36 +0200

    code formatted

--- service/src/main/java/greencity/mapping/AddressLatLngResponseMapper.java
+++ service/src/main/java/greencity/mapping/AddressLatLngResponseMapper.java
@@ -8,18 +8,18 @@ public class AddressLatLngResponseMapper extends AbstractConverter<AddressLatLng
     @Override
     protected CoordinatesDto convert(AddressLatLngResponse addressLatLngResponse) {
         CoordinatesDto coordinatesDto = CoordinatesDto
-                .builder()
-                .latitude(addressLatLngResponse.getLatitude())
-                .longitude(addressLatLngResponse.getLongitude())
-                .build();
-        if(addressLatLngResponse.getAddressUa()!=null){
+            .builder()
+            .latitude(addressLatLngResponse.getLatitude())
+            .longitude(addressLatLngResponse.getLongitude())
+            .build();
+        if (addressLatLngResponse.getAddressUa() != null) {
             coordinatesDto.setStreetUa(addressLatLngResponse.getAddressUa().getStreet());
             coordinatesDto.setHouseNumber(addressLatLngResponse.getAddressUa().getHouseNumber());
             coordinatesDto.setCityUa(addressLatLngResponse.getAddressUa().getCity());
             coordinatesDto.setRegionUa(addressLatLngResponse.getAddressUa().getRegion());
             coordinatesDto.setCountryUa(addressLatLngResponse.getAddressUa().getCountry());
         }
-        if(addressLatLngResponse.getAddressEn()!=null){
+        if (addressLatLngResponse.getAddressEn() != null) {
             coordinatesDto.setStreetEn(addressLatLngResponse.getAddressEn().getStreet());
             coordinatesDto.setCityEn(addressLatLngResponse.getAddressEn().getCity());
             coordinatesDto.setRegionEn(addressLatLngResponse.getAddressEn().getRegion());
--- service/src/test/java/greencity/mapping/AddressLatLngResponseMapperTest.java
+++ service/src/test/java/greencity/mapping/AddressLatLngResponseMapperTest.java
@@ -16,29 +16,29 @@ class AddressLatLngResponseMapperTest {
     private AddressLatLngResponseMapper mapper;
 
     @Test
-    void convert(){
+    void convert() {
         CoordinatesDto expected = ModelUtils.getCoordinatesDto();
         AddressLatLngResponse response = AddressLatLngResponse
+            .builder()
+            .latitude(expected.getLatitude())
+            .longitude(expected.getLongitude())
+            .addressUa(AddressResponse
                 .builder()
-                .latitude(expected.getLatitude())
-                .longitude(expected.getLongitude())
-                .addressUa(AddressResponse
-                        .builder()
-                        .street(expected.getStreetUa())
-                        .houseNumber(expected.getHouseNumber())
-                        .city(expected.getCityUa())
-                        .region(expected.getRegionUa())
-                        .country(expected.getCountryUa())
-                        .build())
-                .addressEn(AddressResponse
-                        .builder()
-                        .street(expected.getStreetEn())
-                        .houseNumber(expected.getHouseNumber())
-                        .city(expected.getCityEn())
-                        .region(expected.getRegionEn())
-                        .country(expected.getCountryEn())
-                        .build())
-                .build();
+                .street(expected.getStreetUa())
+                .houseNumber(expected.getHouseNumber())
+                .city(expected.getCityUa())
+                .region(expected.getRegionUa())
+                .country(expected.getCountryUa())
+                .build())
+            .addressEn(AddressResponse
+                .builder()
+                .street(expected.getStreetEn())
+                .houseNumber(expected.getHouseNumber())
+                .city(expected.getCityEn())
+                .region(expected.getRegionEn())
+                .country(expected.getCountryEn())
+                .build())
+            .build();
         Assertions.assertEquals(expected, mapper.convert(response));
     }
 }
