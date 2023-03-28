dbbbaf2bf26e266ea1053415e523e43d4c234c33
Anton <bondar.local@gmail.com>
2023-03-28 11:43:49 +0300

    fixed import

--- core/src/main/java/greencity/controller/HabitAssignController.java
+++ core/src/main/java/greencity/controller/HabitAssignController.java
@@ -42,6 +42,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
