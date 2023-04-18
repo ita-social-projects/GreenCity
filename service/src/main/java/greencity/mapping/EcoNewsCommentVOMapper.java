8b141f740d1e77f1f00e74fbb47efeabd8d1064f
MarkAndzh <andzheychak@gmail.com>
2023-04-18 15:37:01 +0200

    Changed EcoNewsCommentVOMapper to add parentComment to comment if parentComment is not null

--- service/src/main/java/greencity/mapping/EcoNewsCommentVOMapper.java
+++ service/src/main/java/greencity/mapping/EcoNewsCommentVOMapper.java
@@ -16,7 +16,7 @@ public class EcoNewsCommentVOMapper extends AbstractConverter<EcoNewsComment, Ec
         EcoNewsCommentVO commentVO = convertWithNullParentComment(ecoNewsComment);
         EcoNewsComment parentComment = ecoNewsComment.getParentComment();
 
-        if (parentComment != null){
+        if (parentComment != null) {
             EcoNewsCommentVO parentCommentVO = convertWithNullParentComment(parentComment);
             commentVO.setParentComment(parentCommentVO);
         }
@@ -26,25 +26,25 @@ public class EcoNewsCommentVOMapper extends AbstractConverter<EcoNewsComment, Ec
 
     private EcoNewsCommentVO convertWithNullParentComment(EcoNewsComment ecoNewsComment) {
         return EcoNewsCommentVO.builder()
-                .id(ecoNewsComment.getId())
-                .user(UserVO.builder()
-                        .id(ecoNewsComment.getUser().getId())
-                        .role(ecoNewsComment.getUser().getRole())
-                        .name(ecoNewsComment.getUser().getName())
-                        .build())
-                .modifiedDate(ecoNewsComment.getModifiedDate())
-                .parentComment(null)
-                .text(ecoNewsComment.getText())
-                .deleted(ecoNewsComment.isDeleted())
-                .currentUserLiked(ecoNewsComment.isCurrentUserLiked())
-                .createdDate(ecoNewsComment.getCreatedDate())
-                .usersLiked(ecoNewsComment.getUsersLiked().stream().map(user -> UserVO.builder()
-                                .id(user.getId())
-                                .build())
-                        .collect(Collectors.toSet()))
-                .ecoNews(EcoNewsVO.builder()
-                        .id(ecoNewsComment.getEcoNews().getId())
-                        .build())
-                .build();
+            .id(ecoNewsComment.getId())
+            .user(UserVO.builder()
+                .id(ecoNewsComment.getUser().getId())
+                .role(ecoNewsComment.getUser().getRole())
+                .name(ecoNewsComment.getUser().getName())
+                .build())
+            .modifiedDate(ecoNewsComment.getModifiedDate())
+            .parentComment(null)
+            .text(ecoNewsComment.getText())
+            .deleted(ecoNewsComment.isDeleted())
+            .currentUserLiked(ecoNewsComment.isCurrentUserLiked())
+            .createdDate(ecoNewsComment.getCreatedDate())
+            .usersLiked(ecoNewsComment.getUsersLiked().stream().map(user -> UserVO.builder()
+                .id(user.getId())
+                .build())
+                .collect(Collectors.toSet()))
+            .ecoNews(EcoNewsVO.builder()
+                .id(ecoNewsComment.getEcoNews().getId())
+                .build())
+            .build();
     }
 }
