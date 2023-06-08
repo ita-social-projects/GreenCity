bd25a674ea21d349fe0b449db7a43650381c5ad0
sergoliarnik <74297669+sergoliarnik@users.noreply.github.com>
2023-06-08 07:56:33 +0000

    Deploying to gh-pages from  @ 5c37d2255de54ec091122c60c3b16c47af07bb37 🚀

--- service-api/src/main/java/greencity/service/FriendService.java
+++ service-api/src/main/java/greencity/service/FriendService.java
@@ -9,4 +9,13 @@ public interface FriendService {
      * @author Marian Datsko
      */
     void deleteUserFriendById(Long userId, Long friendId);
+
+    /**
+     * Add new friend by friendId.
+     *
+     * @param userId   {@link Long}
+     * @param friendId {@link Long}
+     * @author Marian Datsko
+     */
+    void addNewFriend(Long userId, Long friendId);
 }
