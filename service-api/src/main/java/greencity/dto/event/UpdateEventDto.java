f73b21f1ff7f822d31b746c0a5e24bbeb7300797
Oleh Kulbaba <elatedexorcist@gmail.com>
2023-06-22 14:16:14 +0300

    changed swagger documentation

--- service-api/src/main/java/greencity/dto/event/UpdateEventDto.java
+++ service-api/src/main/java/greencity/dto/event/UpdateEventDto.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 public class UpdateEventDto {
-    @NotEmpty
+    @NotNull
     private Long id;
 
     @Size(min = 1, max = 70)
