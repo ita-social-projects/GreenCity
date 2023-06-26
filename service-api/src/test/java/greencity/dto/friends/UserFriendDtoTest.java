3cab6e5a2e0f8a3f9f5b0602de7a9da0ab540a1a
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2023-06-26 18:16:46 +0300

    add: add test for UserFriendDto constructor

--- service-api/src/test/java/greencity/dto/friends/UserFriendDtoTest.java
+++ service-api/src/test/java/greencity/dto/friends/UserFriendDtoTest.java
@@ -2,9 +2,27 @@ package greencity.dto.friends;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class UserFriendDtoTest {
     @Test
     void userFriendDtoConstructorTest() {
-
+        Long id = 1L;
+        String name = "name";
+        String city = "city";
+        Double rating = 1.0;
+        Long mutualFriends = 2L;
+        String profilePicture = "profilePicture";
+        Long chatId = 4L;
+        UserFriendDto userFriendDto = new UserFriendDto(id, name, city, rating, mutualFriends,
+                profilePicture, chatId);
+        assertEquals(id, userFriendDto.getId());
+        assertEquals(name, userFriendDto.getName());
+        assertEquals(city, userFriendDto.getCity());
+        assertEquals(rating, userFriendDto.getRating());
+        assertEquals(mutualFriends, userFriendDto.getMutualFriends());
+        assertEquals(profilePicture, userFriendDto.getProfilePicturePath());
+        assertEquals(chatId, userFriendDto.getChatId());
     }
 }
