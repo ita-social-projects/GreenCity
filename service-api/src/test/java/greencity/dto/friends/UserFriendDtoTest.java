c694bebc8bd59b391375d849d5bfe52d1b02abd3
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2023-06-28 15:49:03 +0300

    fix: fix code smell

--- service-api/src/test/java/greencity/dto/friends/UserFriendDtoTest.java
+++ service-api/src/test/java/greencity/dto/friends/UserFriendDtoTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserFriendDtoTest {
+class UserFriendDtoTest {
     @Test
     void userFriendDtoConstructorTest() {
         Long id = 1L;
