9e8f06c2dfbee9c11f27f1d2865a74b2b35f32f1
ospodaryk <ospodaryk11@gmail.com>
2023-08-18 15:46:53 +0300

    Changed script

--- dao/src/main/resources/db/functions/insert-values-employee_authorities.sql
+++ dao/src/main/resources/db/functions/insert-values-employee_authorities.sql
@@ -1,29 +1,25 @@
--- Insert for 'CREATE_NEW_COURIER' authority
+-- Insert for 'DELETE_DEACTIVATE_COURIER' authority
 INSERT INTO employee_authorities_mapping (authority_id, user_id)
-SELECT ea.id, eam.user_id
-FROM employee_authorities AS ea
-         JOIN employee_authorities_mapping AS eam ON eam.authority_id IN (
-    SELECT ea2.id
-    FROM employee_authorities AS ea2
-    WHERE ea2.name = 'CREATE_NEW_COURIER'
-)
-WHERE ea.id IN (
-    SELECT ea3.id
-    FROM employee_authorities AS ea3
-    WHERE ea3.name = 'DELETE_DEACTIVATE_COURIER'
+SELECT ea.id, pam.user_id
+FROM employee_authorities_mapping AS pam
+         JOIN employee_authorities AS ea
+              ON ea.name LIKE 'DELETE_DEACTIVATE_COURIER'
+WHERE pam.authority_id IN (
+    SELECT id
+    FROM employee_authorities
+    WHERE name LIKE 'CREATE_NEW_COURIER'
 );
+
 
 -- Insert for 'DELETE_DEACTIVATE_STATION' authority
 INSERT INTO employee_authorities_mapping (authority_id, user_id)
-SELECT ea.id, eam.user_id
-FROM employee_authorities AS ea
-         JOIN employee_authorities_mapping AS eam ON eam.authority_id IN (
-    SELECT ea2.id
-    FROM employee_authorities AS ea2
-    WHERE ea2.name = 'CREATE_NEW_STATION'
-)
-WHERE ea.id IN (
-    SELECT ea3.id
-    FROM employee_authorities AS ea3
-    WHERE ea3.name = 'DELETE_DEACTIVATE_STATION'
+SELECT ea.id, pam.user_id
+FROM employee_authorities_mapping AS pam
+         JOIN employee_authorities AS ea
+              ON ea.name LIKE 'DELETE_DEACTIVATE_STATION'
+WHERE pam.authority_id IN (
+    SELECT id
+    FROM employee_authorities
+    WHERE name LIKE 'CREATE_NEW_STATION'
 );
+
