bec89bb1b5863ea1308ff17edee38b9cf0d07527
ospodaryk <ospodaryk11@gmail.com>
2023-08-23 11:41:37 +0300

    fixed import *

--- dao/src/main/java/greencity/entity/event/Event.java
+++ dao/src/main/java/greencity/entity/event/Event.java
@@ -1,6 +1,5 @@
 package greencity.entity.event;
 
-import greencity.entity.EcoNewsComment;
 import greencity.entity.Tag;
 import greencity.entity.User;
 import greencity.enums.EventType;
@@ -11,7 +10,21 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.FetchType;
+import javax.persistence.OrderBy;
+
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.List;
