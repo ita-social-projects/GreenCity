699846e5716e8e810e26a931845ce21e4c5fec87
ospodaryk <ospodaryk11@gmail.com>
2023-08-23 10:55:23 +0300

    Added columns to event

--- dao/src/main/java/greencity/entity/event/Event.java
+++ dao/src/main/java/greencity/entity/event/Event.java
@@ -1,5 +1,6 @@
 package greencity.entity.event;
 
+import greencity.entity.EcoNewsComment;
 import greencity.entity.Tag;
 import greencity.entity.User;
 import greencity.enums.EventType;
@@ -10,19 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.OrderBy;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -91,6 +80,16 @@ public class Event {
 
     @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
     private List<EventGrade> eventGrades = new ArrayList<>();
+
+    @OneToMany(mappedBy = "event", fetch = FetchType.LAZY)
+    private List<EventComment> eventsComments = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "events_users_likes",
+        joinColumns = @JoinColumn(name = "event_id"),
+        inverseJoinColumns = @JoinColumn(name = "users_id"))
+    private Set<User> usersLikedNews = new HashSet<>();
 
     /**
      * Method for getting an event type.
