a67ae16d27c0f40cde068d590294b1dde68cbb8f
2or5 <125138706+2or5@users.noreply.github.com>
2023-09-05 08:17:14 +0000

    Deploying to gh-pages from  @ c701a69d69b661baf0919c3c34e203319320a21b 🚀

--- service-api/src/main/java/greencity/constant/AppConstant.java
+++ service-api/src/main/java/greencity/constant/AppConstant.java
@@ -23,7 +23,7 @@ public final class AppConstant {
     public static final int MAX_DAYS_DURATION = 56;
     public static final Integer MAX_PASSED_DAYS_OF_ABILITY_TO_ENROLL = 8;
     public static final String DEFAULT_HABIT_IMAGE = "img/habit-default.png";
-    public static final String DEFAULT_EVENT_IMAGES = "assets/img/habits/default-habit-image.png";
+    public static final String DEFAULT_EVENT_IMAGES = "img/habits/default-habit-image.png";
 
     private AppConstant() {
     }
