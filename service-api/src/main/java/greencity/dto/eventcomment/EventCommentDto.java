60e7f2cb9090a517b1b17a5adae5df51b45a46f5
ospodaryk <ospodaryk11@gmail.com>
2023-09-06 12:12:31 +0300

    Fixed bug codesmell

--- service-api/src/main/java/greencity/dto/eventcomment/EventCommentDto.java
+++ service-api/src/main/java/greencity/dto/eventcomment/EventCommentDto.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
