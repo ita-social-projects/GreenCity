6960caa916f2621905946490f50391c1f608049c
Maksym Lenets <maximlenets@gmail.com>
2023-09-08 13:22:43 +0300

    test_4

--- core/src/test/java/greencity/repository/LanguageRepoTest.java
+++ core/src/test/java/greencity/repository/LanguageRepoTest.java
@@ -1,19 +1,18 @@
 package greencity.repository;
 
+import greencity.DaoApplication;
+
 import greencity.IntegrationTestBase;
 import greencity.entity.Language;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest(classes = DaoApplication.class)
 class LanguageRepoTest extends IntegrationTestBase {
     @Autowired
     private LanguageRepo languageRepo;
