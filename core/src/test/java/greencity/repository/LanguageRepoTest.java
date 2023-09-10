19d38ebbfc54864f96c199bc175e988f8458d9c3
Maksym Lenets <maximlenets@gmail.com>
2023-09-10 12:54:10 +0300

    rollback

--- core/src/test/java/greencity/repository/LanguageRepoTest.java
+++ core/src/test/java/greencity/repository/LanguageRepoTest.java
@@ -1,18 +1,20 @@
 package greencity.repository;
 
-import greencity.DaoApplication;
-
+import greencity.GreenCityApplication;
 import greencity.IntegrationTestBase;
 import greencity.entity.Language;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest(classes = DaoApplication.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = GreenCityApplication.class)
 class LanguageRepoTest extends IntegrationTestBase {
     @Autowired
     private LanguageRepo languageRepo;
