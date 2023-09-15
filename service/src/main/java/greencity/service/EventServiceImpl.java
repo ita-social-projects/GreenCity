8428ddc7e9327808c9fb0ad89ad332f6c68d0926
Lena Sotnik <lenasotnik@Lenas-MacBook-Air.local>
2023-09-15 04:27:44 -0700

    Commit with adding filter operation to stream in method getAllAddresses()

--- service/src/main/java/greencity/service/EventServiceImpl.java
+++ service/src/main/java/greencity/service/EventServiceImpl.java
@@ -178,7 +178,9 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Set<AddressDto> getAllEventsAddresses() {
-        return eventRepo.findAll().stream().map(event -> modelMapper
+        return eventRepo.findAll().stream()
+                .filter(event -> Objects.nonNull(event.getDates().get(event.getDates().size() - 1).getAddress()))
+                .map(event -> modelMapper
             .map((event.getDates().get(event.getDates().size() - 1).getAddress()), AddressDto.class))
             .collect(Collectors.toSet());
     }
