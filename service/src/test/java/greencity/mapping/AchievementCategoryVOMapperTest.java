6dd1c4808f043c9cb311a1ae1ace425e793e3310
ospodaryk <ospodaryk11@gmail.com>
2023-09-21 12:56:00 +0300

    Fixed code

--- service/src/test/java/greencity/mapping/AchievementCategoryVOMapperTest.java
+++ service/src/test/java/greencity/mapping/AchievementCategoryVOMapperTest.java
@@ -1,9 +1,6 @@
 package greencity.mapping;
 
-import greencity.ModelUtils;
-import greencity.dto.achievement.AchievementVO;
 import greencity.dto.achievementcategory.AchievementCategoryVO;
-import greencity.dto.useraction.UserActionVO;
 import greencity.entity.AchievementCategory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,7 +18,7 @@ class AchievementCategoryVOMapperTest {
 
     @Test
     void convert() {
-        AchievementCategory achievementCategory = new AchievementCategory(1L, "Name");
+        AchievementCategory achievementCategory = new AchievementCategory(1L, "Name",Collections.emptyList());
         AchievementCategoryVO achievementCategoryVO = AchievementCategoryVO.builder()
             .id(achievementCategory.getId())
             .name(achievementCategory.getName())
