21b8e603852a7dfd0bde486643505304d6a9f9a2
ospodaryk <ospodaryk11@gmail.com>
2023-10-03 22:45:20 +0300

    Removed unused method restClient.save

--- service-api/src/main/java/greencity/client/RestClient.java
+++ service-api/src/main/java/greencity/client/RestClient.java
@@ -506,19 +506,6 @@ public class RestClient {
      * @param userVO for save User.
      * @author Orest Mamchuk
      */
-    public void save(UserVO userVO) {
-        HttpEntity<UserVO> entity = new HttpEntity<>(userVO, setHeader());
-        restTemplate.exchange(greenCityUserServerAddress
-            + RestTemplateLinks.USER, HttpMethod.POST, entity, Object.class)
-            .getBody();
-    }
-
-    /**
-     * Method that allow you to save new {@link UserVO}.
-     *
-     * @param userVO for save User.
-     * @author Orest Mamchuk
-     */
     public void save(UserVO userVO, String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.set(AUTHORIZATION, accessToken);
