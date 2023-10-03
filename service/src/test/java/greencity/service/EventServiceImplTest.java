b55e0881acae0cf1ff0b4879e328361c673ad6e7
Lena Sotnik <lenasotnik@Lenas-MacBook-Air.local>
2023-10-03 00:30:31 -0700

    Removed asterisk from imports

--- service/src/test/java/greencity/service/EventServiceImplTest.java
+++ service/src/test/java/greencity/service/EventServiceImplTest.java
@@ -62,7 +62,15 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class EventServiceImplTest {
