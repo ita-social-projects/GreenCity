1fb4d908f50c0d4368db39c6d0cc5568cfdb7e00
Lilia Mokhnatska <liliamokhnatska@gmail.com>
2023-10-05 18:54:57 +0300

    code optimisation

--- dao/src/main/java/greencity/repository/EcoNewsCommentRepo.java
+++ dao/src/main/java/greencity/repository/EcoNewsCommentRepo.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface EcoNewsCommentRepo extends JpaRepository<EcoNewsComment, Long> {
