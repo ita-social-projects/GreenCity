00b2a3376d828f8321c234ddf36461efd9994a2d
2or5 <nazarbokalo0@gmail.com>
2023-10-12 18:41:42 +0300

    fix code smells

--- core/src/main/java/greencity/controller/FriendController.java
+++ core/src/main/java/greencity/controller/FriendController.java
@@ -29,8 +29,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Validated
