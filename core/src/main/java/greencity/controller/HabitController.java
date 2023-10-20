a9c11de8ab5fedb7b07cd27b6d1caddaefdc497f
VinuVicho <vovkamlad@gmail.com>
2023-10-20 21:09:41 +0300

    Added ExeptionHandler for HabitController

--- core/src/main/java/greencity/controller/HabitController.java
+++ core/src/main/java/greencity/controller/HabitController.java
@@ -5,6 +5,7 @@ import greencity.annotations.ApiPageableWithLocale;
 import greencity.annotations.CurrentUser;
 import greencity.annotations.ImageValidation;
 import greencity.annotations.ValidLanguage;
+import greencity.constant.ErrorMessage;
 import greencity.constant.HttpStatuses;
 import greencity.dto.PageableDto;
 import greencity.dto.habit.CustomHabitDtoRequest;
@@ -34,15 +35,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -284,5 +277,15 @@ public class HabitController {
         @ApiParam(value = "Image of habit") @ImageValidation @RequestPart(required = false) MultipartFile image) {
         return ResponseEntity.status(HttpStatus.OK)
             .body(habitService.updateCustomHabit(request, habitId, principal.getName(), image));
+    }
+
+    /**
+     * Handles IllegalArgumentException.
+     *
+     * @return Error message "Bad Request".
+     */
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<Object> handleException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatuses.BAD_REQUEST);
     }
 }
