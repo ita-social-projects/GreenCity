6d430d6aecfc7d38f4f2be7d002b827f56f06765
VinuVicho <vovkamlad@gmail.com>
2023-10-24 22:01:06 +0300

    Fix empty shopping list bug

--- service/src/main/java/greencity/service/HabitAssignServiceImpl.java
+++ service/src/main/java/greencity/service/HabitAssignServiceImpl.java
@@ -145,7 +145,8 @@ public class HabitAssignServiceImpl implements HabitAssignService {
             habitAssign.setStatus(HabitAssignStatus.INPROGRESS);
             habitAssign.setCreateDate(ZonedDateTime.now());
         } else {
-            List<Long> allShoppingListItemId = shoppingListItemRepo.getAllShoppingListItemIdByHabitIdISContained(habitId);
+            List<Long> allShoppingListItemId =
+                shoppingListItemRepo.getAllShoppingListItemIdByHabitIdISContained(habitId);
             habitAssign = buildHabitAssign(habit, user, HabitAssignStatus.INPROGRESS);
             if (!allShoppingListItemId.isEmpty()) {
                 List<ShoppingListItem> shoppingList =
