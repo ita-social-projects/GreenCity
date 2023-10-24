97d4a3a8f790f4c9f2b201f6ff756349440cf3de
VinuVicho <vovkamlad@gmail.com>
2023-10-24 22:44:29 +0300

    Optimize imports

--- core/src/main/java/greencity/controller/HabitController.java
+++ core/src/main/java/greencity/controller/HabitController.java
@@ -34,7 +34,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
