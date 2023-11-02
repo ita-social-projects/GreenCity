c863b13f985c950517b6004f3bb2468e50dd8cd5
ospodaryk <82940222+ospodaryk@users.noreply.github.com>
2023-11-02 14:17:01 +0000

    Deploying to gh-pages from  @ ed6c3de02c07dfdf4353d73a4178dfd2f2b99af2 🚀

--- dao/src/main/java/greencity/repository/AchievementRepo.java
+++ dao/src/main/java/greencity/repository/AchievementRepo.java
@@ -25,10 +25,18 @@ public interface AchievementRepo extends JpaRepository<Achievement, Long> {
      * @return A list of Achievement objects that the user hasn't achieved within
      *         the specified category.
      */
-    @Query(value = "Select ach.* from achievements as ach"
-        + "            join user_achievements uach on ach.id = uach.achievement_id"
-        + "            join user_actions ua on ua.count < ach.condition"
-        + "            where uach.user_id=:userId and ach.achievement_category_id=:achievementCategoryId",
+    @Query(value = "Select ach.*\n"
+        + "from achievements as ach\n"
+        + "where\n"
+        + "    ach.id in\n"
+        + "      (SELECT achievement_id from user_achievements uach where uach.user_id = :userId)\n"
+        + "  and\n"
+        + "    ach.condition > (SELECT ua.count\n"
+        + "                                      from user_actions ua\n"
+        + "                                      where ua.user_id = :userId and\n"
+        + "                                            ua.achievement_category_id=:achievementCategoryId)\n"
+        + "and\n"
+        + "    ach.achievement_category_id=:achievementCategoryId",
         nativeQuery = true)
     List<Achievement> findUnAchieved(Long userId, Long achievementCategoryId);
 
