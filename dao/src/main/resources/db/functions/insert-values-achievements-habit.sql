f5217be1dfdbd7389c8017765d453572c83d07f3
ospodaryk <ospodaryk11@gmail.com>
2023-11-09 13:54:27 +0200

    Renamed from enroll to commit

--- dao/src/main/resources/db/functions/insert-values-achievements-habit.sql
+++ dao/src/main/resources/db/functions/insert-values-achievements-habit.sql
@@ -8,11 +8,12 @@ SELECT id INTO habit_id FROM achievement_categories WHERE name = 'HABIT';
 
 INSERT INTO achievements(title, name, name_eng, score, achievement_category_id, condition)
 VALUES
-    ('ENROLLED_HABIT_1_TIMES', 'Почато 1 звичку', 'Started 1 habit', 5, habit_id, 1),
-    ('ENROLLED_HABIT_5_TIMES', 'Почато 5 звичок', 'Started 5 habits', 10, habit_id, 5),
-    ('ENROLLED_HABIT_10_TIMES', 'Почато 10 звичок', 'Started 10 habits', 15, habit_id, 10),
-    ('ENROLLED_HABIT_25_TIMES', 'Почато 25 звичок', 'Started 25 habits', 25, habit_id, 25),
-    ('ENROLLED_HABIT_50_TIMES', 'Почато 50 звичок', 'Started 50 habits', 50, habit_id, 50),
-    ('ENROLLED_HABIT_100_TIMES', 'Почато 100 звичок', 'Started 100 habits', 100, habit_id, 100);
+    ('COMPLETED_HABIT_1_TIME', 'Завершено 1 звичку', 'Completed 1 habit', 5, habit_id, 1),
+    ('COMPLETED_HABIT_5_TIMES', 'Завершено 5 звичок', 'Completed 5 habits', 10, habit_id, 5),
+    ('COMPLETED_HABIT_10_TIMES', 'Завершено 10 звичок', 'Completed 10 habits', 15, habit_id, 10),
+    ('COMPLETED_HABIT_25_TIMES', 'Завершено 25 звичок', 'Completed 25 habits', 25, habit_id, 25),
+    ('COMPLETED_HABIT_50_TIMES', 'Завершено 50 звичок', 'Completed 50 habits', 50, habit_id, 50),
+    ('COMPLETED_HABIT_100_TIMES', 'Завершено 100 звичок', 'Completed 100 habits', 100, habit_id, 100);
 
 END $$;
+
