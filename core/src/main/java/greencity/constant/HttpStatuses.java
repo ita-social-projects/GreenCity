ecb81cc1cc444a419543e003c60fd0344d59402c
Yurii Midianyi <loopfordev@gmail.com>
2023-11-29 19:41:16 +0200

    start the pipline

--- core/src/main/java/greencity/constant/HttpStatuses.java
+++ core/src/main/java/greencity/constant/HttpStatuses.java
@@ -3,7 +3,6 @@ package greencity.constant;
 public final class HttpStatuses {
     public static final String OK = "OK";
     public static final String CREATED = "Created";
-
     public static final String BAD_REQUEST = "Bad Request";
     public static final String UNAUTHORIZED = "Unauthorized";
     public static final String FORBIDDEN = "Forbidden";
