e591e0fa4a3f0831df246b74b3955d630412b2ca
VinuVicho <vovkamlad@gmail.com>
2024-02-12 12:08:56 +0200

    Removed empty line

--- service/src/main/java/greencity/service/EventCommentServiceImpl.java
+++ service/src/main/java/greencity/service/EventCommentServiceImpl.java
@@ -34,7 +34,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
