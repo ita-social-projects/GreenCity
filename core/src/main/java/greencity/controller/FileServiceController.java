7b86c2f3939dc608fda3f9d0b31c653990647533
Lena Sotnik <lenasotnik@Lenas-MacBook-Air.local>
2024-02-15 13:26:37 +0200

    Commit with File upload method change and exception handling

--- core/src/main/java/greencity/controller/FileServiceController.java
+++ core/src/main/java/greencity/controller/FileServiceController.java
@@ -3,16 +3,19 @@ package greencity.controller;
 import greencity.constant.HttpStatuses;
 import greencity.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -36,11 +39,12 @@ public class FileServiceController {
      */
     @Operation(summary = "Upload an image.")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+        @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
+            content = @Content(schema = @Schema(implementation = String.class))),
         @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED)
     })
-    @PostMapping("/image")
-    public ResponseEntity<String> uploadImage(@RequestPart @NotNull MultipartFile image) {
+    @PostMapping(path = "/image", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<String> uploadImage(@RequestPart(required = false) @Nullable MultipartFile image) {
         return ResponseEntity.status(HttpStatus.OK).body(fileService.upload(image));
     }
 }
