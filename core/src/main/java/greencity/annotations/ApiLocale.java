33988da7a59235db5bf6e1d4dd516914ea52bf3c
Lena Sotnik <lenasotnik@Lenas-MacBook-Air.local>
2024-02-22 16:17:15 +0200

    Commit update for locale

--- core/src/main/java/greencity/annotations/ApiLocale.java
+++ core/src/main/java/greencity/annotations/ApiLocale.java
@@ -16,7 +16,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-@Parameter(name = "lang", schema = @Schema(type = "string"), in = ParameterIn.DEFAULT,
+@Parameter(name = "lang", schema = @Schema(type = "string"), in = ParameterIn.QUERY,
     description = "Code of the needed language.")
 public @interface ApiLocale {
 }
