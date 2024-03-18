feb386f82ae65d40fde784b4ce2c8a19f5554d9d
Anton Bondar <bondar.local@gmail.com>
2024-03-18 17:43:13 +0200

    cors configuration V22

--- core/src/main/java/greencity/config/WebSocketConfig.java
+++ core/src/main/java/greencity/config/WebSocketConfig.java
@@ -24,7 +24,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-            .setAllowedOrigins("*")
             .withSockJS();
     }
 
