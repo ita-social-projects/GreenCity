61b1d93a709f060454c11c6d94e5a6653dbcd9f2
nazariusPr <protsnazar2004@gmail.com>
2024-08-28 13:13:23 +0300

    Create HabitTranslationFilterDto.java

---
+++ dao/src/main/java/greencity/dto/filter/HabitTranslationFilterDto.java
@@ -0,0 +1,24 @@
+package greencity.dto.filter;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HabitTranslationFilterDto {
+    @NotEmpty
+    private String languageCode;
+    private List<String> tags;
+    private List<Integer> complexities;
+    private boolean isCustom;
+    @NotNull
+    private Long userId;
+}
