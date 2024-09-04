3d5988f49ea5ac915d6f3b150a770e57c9cfb727
ViktoriyaRyazhska <vryazhskaya@gmail.com>
2024-09-04 14:50:47 +0000

    Deploying to gh-pages from  @ 3fda2b809d1ff54c57289bea46b3f732e6ec4ed1 🚀

--- service/src/main/java/greencity/service/EventServiceImpl.java
+++ service/src/main/java/greencity/service/EventServiceImpl.java
@@ -144,10 +144,6 @@ public class EventServiceImpl implements EventService {
     @Override
     public EventDto save(AddEventDtoRequest addEventDtoRequest, String email,
         MultipartFile[] images) {
-        if (addEventDtoRequest.getTitle() == null || addEventDtoRequest.getDescription() == null
-            || addEventDtoRequest.getTitle().trim().isEmpty() || addEventDtoRequest.getDescription().trim().isEmpty()) {
-            throw new BadRequestException(ErrorMessage.TITLE_OR_DESCRIPTION_IS_EMPTY);
-        }
         checkingEqualityDateTimeInEventDateLocationDto(addEventDtoRequest.getDatesLocations());
         addAddressToLocation(addEventDtoRequest.getDatesLocations());
         Event toSave = modelMapper.map(addEventDtoRequest, Event.class);
@@ -526,10 +522,6 @@ public class EventServiceImpl implements EventService {
     @Transactional
     public EventDto update(UpdateEventRequestDto eventDtoRequest, String email, MultipartFile[] images) {
         UpdateEventDto eventDto = modelMapper.map(eventDtoRequest, UpdateEventDto.class);
-        if (eventDto.getTitle() == null || eventDto.getDescription() == null || eventDto.getTitle().trim().isEmpty()
-            || eventDto.getDescription().trim().isEmpty()) {
-            throw new BadRequestException(ErrorMessage.TITLE_OR_DESCRIPTION_IS_EMPTY);
-        }
         checkingEqualityDateTimeInEventDateLocationDto(eventDto.getDatesLocations());
 
         Event toUpdate = eventRepo.findById(eventDto.getId())
