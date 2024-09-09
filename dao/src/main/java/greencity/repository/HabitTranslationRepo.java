5ad988146066319eb3c1450d6f041f5412d4c3d9
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-09-09 13:44:28 +0000

    Deploying to gh-pages from  @ 75a4116edbafadf710dfbb7c122118f995c6ea55 🚀

--- dao/src/main/java/greencity/repository/HabitTranslationRepo.java
+++ dao/src/main/java/greencity/repository/HabitTranslationRepo.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 /**
@@ -14,7 +15,8 @@ import org.springframework.data.jpa.repository.Query;
  *
  * @author Volodymyr Turko
  */
-public interface HabitTranslationRepo extends JpaRepository<HabitTranslation, Long> {
+public interface HabitTranslationRepo
+    extends JpaRepository<HabitTranslation, Long>, JpaSpecificationExecutor<HabitTranslation> {
     /**
      * Method return {@link Optional} of {@link HabitTranslation}.
      *
