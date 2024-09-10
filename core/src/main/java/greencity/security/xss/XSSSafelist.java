3a36618df3c508a05d0b41e21188953d34f9e40e
DmytroDmytruk <97033032+DmytroDmytruk@users.noreply.github.com>
2024-09-11 00:08:30 +0300

    Added html elements to constants

--- core/src/main/java/greencity/security/xss/XSSSafelist.java
+++ core/src/main/java/greencity/security/xss/XSSSafelist.java
@@ -17,13 +17,31 @@ public class XSSSafelist {
     private static final Map<String, XSSAllowedElements> endpointRules = new HashMap<>();
     private static final XSSAllowedElements defaultAllowedElements = XSSAllowedElements.getDefault();
     private static final AntPathMatcher pathMatcher = new AntPathMatcher();
+    //Html tags
+    private static final String P_TAG = "p";
+    private static final String A_TAG = "a";
+    private static final String U_TAG = "u";
+    private static final String EM_TAG = "em";
+    private static final String PRE_TAG = "pre";
+    private static final String SPAN_TAG = "span";
+    private static final String IMG_TAG = "img";
+    private static final String STRONG_TAG = "strong";
+    //Html attributes
     private static final String IFRAME_ATTR = "iframe";
     private static final String CLASS_ATTR = "class";
+    private static final String HREF_ATTR = "href";
+    private static final String SRC_ATTR = "src";
+    private static final String REL_ATTR = "rel";
+    private static final String ALT_ATTR = "rel";
+    private static final String STYLE_ATTR = "style";
+    private static final String TARGET_ATTR = "target";
+    private static final String FRAMEBORDER_ATTR = "frameborder";
+    private static final String ALLOWFULLSCREEN_ATTR = "allowfullscreen";
 
     static {
         Safelist safelistForEvent = new Safelist()
-            .addTags("p", "strong", "em", "u", "span")
-            .addAttributes("span", "class");
+            .addTags(P_TAG, STRONG_TAG, EM_TAG, U_TAG, SPAN_TAG)
+            .addAttributes(SPAN_TAG, CLASS_ATTR);
 
         XSSAllowedElements allowedElementsForEvent = XSSAllowedElements.builder()
             .safelist(safelistForEvent)
@@ -31,16 +49,16 @@ public class XSSSafelist {
             .build();
 
         Safelist safelistForEcoNews = new Safelist()
-            .addTags("pre", "p", "span", "a", IFRAME_ATTR, "img")
-            .addAttributes("pre", CLASS_ATTR)
-            .addAttributes("p", CLASS_ATTR)
-            .addAttributes("span", CLASS_ATTR, "style")
-            .addAttributes("a", "href", "rel", "target")
-            .addAttributes(IFRAME_ATTR, CLASS_ATTR, "src", "frameborder", "allowfullscreen")
-            .addAttributes("img", "src", "alt");
-        safelistForEcoNews.addEnforcedAttribute(IFRAME_ATTR, "frameborder", "0")
-            .addEnforcedAttribute(IFRAME_ATTR, "allowfullscreen", "true")
-            .addProtocols("a", "href", "http", "https");
+            .addTags(PRE_TAG, P_TAG, SPAN_TAG, A_TAG, IFRAME_ATTR, IMG_TAG)
+            .addAttributes(PRE_TAG, CLASS_ATTR)
+            .addAttributes(P_TAG, CLASS_ATTR)
+            .addAttributes(SPAN_TAG, CLASS_ATTR, STYLE_ATTR)
+            .addAttributes(A_TAG, HREF_ATTR, REL_ATTR, TARGET_ATTR)
+            .addAttributes(IFRAME_ATTR, CLASS_ATTR, SRC_ATTR, FRAMEBORDER_ATTR, ALLOWFULLSCREEN_ATTR)
+            .addAttributes(IMG_TAG, SRC_ATTR, ALT_ATTR);
+        safelistForEcoNews.addEnforcedAttribute(IFRAME_ATTR, FRAMEBORDER_ATTR, "0")
+            .addEnforcedAttribute(IFRAME_ATTR, ALLOWFULLSCREEN_ATTR, "true")
+            .addProtocols(A_TAG, HREF_ATTR, "http", "https");
 
         XSSAllowedElements allowedElementsForEcoNews = XSSAllowedElements.builder()
             .safelist(safelistForEcoNews)
@@ -48,8 +66,8 @@ public class XSSSafelist {
             .build();
 
         Safelist safelistForHabit = new Safelist()
-            .addTags("p", "strong", "em", "u", "span")
-            .addAttributes("span", CLASS_ATTR);
+            .addTags(P_TAG, STRONG_TAG, EM_TAG, U_TAG, SPAN_TAG)
+            .addAttributes(SPAN_TAG, CLASS_ATTR);
 
         XSSAllowedElements allowedElementsForHabit = XSSAllowedElements.builder()
             .safelist(safelistForHabit)
