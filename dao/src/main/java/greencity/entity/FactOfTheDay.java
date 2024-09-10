ce99407b99b417b91a79f320cb356c05af06a61c
Maryna-511750 <109355692+Maryna-511750@users.noreply.github.com>
2024-09-10 12:56:14 +0000

    Deploying to gh-pages from  @ d01b26424cb13ccffe12d7725b104d9e44fa9184 🚀

--- dao/src/main/java/greencity/entity/FactOfTheDay.java
+++ dao/src/main/java/greencity/entity/FactOfTheDay.java
@@ -2,7 +2,19 @@ package greencity.entity;
 
 import java.time.ZonedDateTime;
 import java.util.List;
-import jakarta.persistence.*;
+import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -33,4 +45,10 @@ public class FactOfTheDay {
     @CreationTimestamp
     @Column(name = "create_date", nullable = false)
     private ZonedDateTime createDate;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "fact_of_the_day_tags",
+        joinColumns = @JoinColumn(name = "fact_of_the_day_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags;
 }
