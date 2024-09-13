6d18f05118ee9238fe273647814ceded4a35ac02
Lena Sotnik <lenasotnik@Lenas-MacBook-Air.local>
2024-09-13 12:44:04 +0300

    Commit with removed import

--- service/src/main/java/greencity/service/PlaceServiceImpl.java
+++ service/src/main/java/greencity/service/PlaceServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import greencity.entity.*;
 import jakarta.validation.Valid;
 import org.apache.commons.lang3.ArrayUtils;
 import org.modelmapper.ModelMapper;
