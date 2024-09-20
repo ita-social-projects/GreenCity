9a5996fdab368344adb4fa09b50a8751ff71118a
IliaWithHat <146325650+IliaWithHat@users.noreply.github.com>
2024-09-20 11:09:31 +0000

    Deploying to gh-pages from  @ 73ea77ca510211afc9fdb9a0e7c4a1b336fc1b60 🚀

--- service-api/src/main/java/greencity/exception/exceptions/BadRequestException.java
+++ service-api/src/main/java/greencity/exception/exceptions/BadRequestException.java
@@ -4,9 +4,6 @@ import lombok.experimental.StandardException;
 
 /**
  * Exception that we get when user trying to pass bad request.
- *
- * @author Nazar Vladyka
- * @version 1.0
  */
 @StandardException
 public class BadRequestException extends RuntimeException {
