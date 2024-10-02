258be8807df0554ed7a22f89ac0f00d6df54c78b
K4sik <kasiks597@gmail.com>
2024-10-02 13:21:19 +0300

    Added addition logic for comment dislike

--- service/src/main/java/greencity/service/CommentServiceImpl.java
+++ service/src/main/java/greencity/service/CommentServiceImpl.java
@@ -553,6 +553,8 @@ public class CommentServiceImpl implements CommentService {
             ratingCalculation.ratingCalculation(RatingCalculationEnum.UNDO_LIKE_COMMENT_OR_REPLY, userVO);
             achievementCalculation.calculateAchievement(userVO,
                 AchievementCategoryType.LIKE_COMMENT_OR_REPLY, AchievementAction.DELETE);
+            userNotificationService.removeActionUserFromNotification(modelMapper.map(comment.getUser(), UserVO.class),
+                    userVO, commentId, getNotificationType(comment.getArticleType(), CommentActionType.COMMENT_LIKE));
         } else {
             comment.getUsersLiked().add(modelMapper.map(userVO, User.class));
             achievementCalculation.calculateAchievement(userVO,
