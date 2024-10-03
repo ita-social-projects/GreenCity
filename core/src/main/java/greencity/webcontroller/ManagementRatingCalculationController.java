2e1fb1b3b0b39b8b1ba1b9d8872af22e184d3ebd
Maryna <m51571750@gmail.com>
2024-10-03 12:46:54 +0300

    Fixed remark

--- core/src/main/java/greencity/webcontroller/ManagementRatingCalculationController.java
+++ core/src/main/java/greencity/webcontroller/ManagementRatingCalculationController.java
@@ -14,7 +14,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/management/rating/calculation")
