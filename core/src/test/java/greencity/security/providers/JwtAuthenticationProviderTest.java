3116c357a0248c6550e001e3527fa002c8b5fd43
Ivan Holotsvan <iholotsvan@gmail.com>
2024-10-04 11:33:08 +0300

    remove unused import from JwtAuthenticationProviderTest

--- core/src/test/java/greencity/security/providers/JwtAuthenticationProviderTest.java
+++ core/src/test/java/greencity/security/providers/JwtAuthenticationProviderTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
