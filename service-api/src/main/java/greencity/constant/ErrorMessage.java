39ed6ee78a72e4c3093c07609000377e6e2bd913
vnglnk <128087718+holotsvan@users.noreply.github.com>
2024-10-16 13:13:27 +0300

    fix 500 (#7623)

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -200,6 +200,7 @@ public class ErrorMessage {
         "The rating points does not exist by this name: ";
     public static final String RATING_POINTS_NOT_FOUND_BY_ID =
         "The rating points does not exist by this id: ";
+    public static final String NOTIFICATION_NOT_FOUND_BY_ID = "Notification doesn't exist by this id: ";
     public static final String DELETING_RATING_POINTS_NOT_ALLOWED =
         "Deleting this RatingPoints is not possible because an Achievement with such name still exists.";
 }
