4fd9a2d37c456c15ef13ab561112ed6595947d7a
Yehor <egorov2001egorr@gmail.com>
2024-10-23 23:54:54 +0300

    Refactor and enhance exception handling and data classes
    Added parameterized constructor to InvalidURLException for better exception details. Implemented getEmail method in SendHabitNotification and ScheduledEmailMessage to fulfill interface contract. Refactored user DTO classes to use Lombok @Data annotation for cleaner and more concise code.

--- service-api/src/main/java/greencity/dto/user/UserManagementDto.java
+++ service-api/src/main/java/greencity/dto/user/UserManagementDto.java
@@ -3,23 +3,16 @@ package greencity.dto.user;
 import greencity.constant.ServiceValidationConstants;
 import greencity.enums.Role;
 import greencity.enums.UserStatus;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@Builder
-@EqualsAndHashCode
 public class UserManagementDto {
     @NotNull
     private Long id;
--- service-api/src/main/java/greencity/dto/user/UserManagementUpdateDto.java
+++ service-api/src/main/java/greencity/dto/user/UserManagementUpdateDto.java
@@ -7,19 +7,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class UserManagementUpdateDto {
     @NotBlank
     @Size(
