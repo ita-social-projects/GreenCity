cb06088f2e4fb1cc43accc32835717d0e7ef7ded
dxrknesss <imp0rt4ntduck@gmail.com>
2025-02-03 16:17:42 +0200

    test: tests for eventsearchrepo. WIP

--- dao/src/test/java/greencity/repository/impl/EventSearchRepoImplTest.java
+++ dao/src/test/java/greencity/repository/impl/EventSearchRepoImplTest.java
@@ -1,16 +1,47 @@
 package greencity.repository.impl;
 
 import greencity.dto.filter.FilterEventDto;
+import greencity.repository.util.PostgresInitializer;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-import java.time.ZonedDateTime;
+import java.util.List;
 
-@SpringBootTest
-public class EventSearchRepoImplTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+public class EventSearchRepoImplTest extends PostgresInitializer {
+    private final EventSearchRepoImpl eventSearchRepo;
+
+    @Autowired
+    public EventSearchRepoImplTest(EventSearchRepoImpl eventSearchRepo) {
+        this.eventSearchRepo = eventSearchRepo;
+    }
+
+    @BeforeAll
+    static void startContainer() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterAll
+    static void stopContainer() {
+        postgreSQLContainer.stop();
+    }
+
     @Test
     public void findEventIdsTest() {
-        var zonedNow = ZonedDateTime.now();
-        var filterDto = FilterEventDto.builder()
-            .from(zonedNow.plusDays(1)).to(zonedNow.plusDays(10)).build();
+        Pageable page = PageRequest.of(0, 2);
+        var filter = FilterEventDto.builder().cities(List.of("Kyiv")).build();
+        var res = eventSearchRepo.findEventsIds(page, filter, 2L);
+        assertFalse(res.isEmpty());
     }
 }
