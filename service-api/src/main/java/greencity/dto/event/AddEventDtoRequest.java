2f3b86461fabd25fe83cb9a8122d8ad5a06029d1
Urio999 <urio999@gmail.com>
2025-02-14 18:52:19 +0200

    fixed regexp ver3

--- service-api/src/main/java/greencity/dto/event/AddEventDtoRequest.java
+++ service-api/src/main/java/greencity/dto/event/AddEventDtoRequest.java
@@ -29,8 +29,8 @@ public class AddEventDtoRequest {
     @NotBlank
     @Size(min = 10, max = 63206)
     @Pattern(
-        regexp = "^(?!.*\\s{2,})\\S.{8,}\\S$",
-        message = "Description must be at least 10 characters long (excluding leading/trailing spaces)"
+        regexp = "^(?!\\S*\\s{2,})\\S.{8,}\\S$",
+        message = "Description must be at least 10 characters long (excluding leading / trailing spaces) "
             + "and must not contain consecutive spaces.")
     private String description;
 
