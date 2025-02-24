8059ff027f1e5840b3a57b378d193f28369e1a9f
Warded120 <121890324+Warded120@users.noreply.github.com>
2025-02-24 13:41:27 +0000

    Deploying to gh-pages from  @ cb1361f8650abf5ce9b1baa923623ff8e3c9f435 🚀

---
+++ service/src/main/java/greencity/config/DotenvConfig.java
@@ -0,0 +1,25 @@
+package greencity.config;
+
+import greencity.constant.AppConstant;
+import greencity.constant.ErrorMessage;
+import greencity.exception.exceptions.FunctionalityNotAvailableException;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+@Lazy
+public class DotenvConfig {
+    @Bean
+    Dotenv dotenv() {
+        try {
+            return Dotenv.configure()
+                .filename(AppConstant.DOTENV_FILENAME)
+                .load();
+        } catch (DotenvException ignored) {
+            throw new FunctionalityNotAvailableException(ErrorMessage.FUNCTIONALITY_NOT_AVAILABLE);
+        }
+    }
+}
---
+++ service/src/test/java/greencity/service/LogFileServiceImplSanitizeFilenameTest.java
@@ -0,0 +1,38 @@
+package greencity.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class LogFileServiceImplSanitizeFilenameTest {
+
+    @InjectMocks
+    private LogFileServiceImpl logFileService;
+
+    @Mock
+    private DotenvService dotenvService;
+
+    private static Stream<Arguments> filenameProvider() {
+        return Stream.of(
+            Arguments.of("valid_filename123.txt", "valid_filename123.txt"),
+            Arguments.of("invalid@filename#.txt", "invalid_filename_.txt"),
+            Arguments.of("", ""),
+            Arguments.of("@#$.txt", "___.txt"),
+            Arguments.of("file name with spaces.txt", "file_name_with_spaces.txt"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("filenameProvider")
+    void sanitizeFilenameShouldReturnSanitizedFilenameTest(String input, String expected) {
+        String result = logFileService.sanitizeFilename(input);
+        assertEquals(expected, result);
+    }
+}
