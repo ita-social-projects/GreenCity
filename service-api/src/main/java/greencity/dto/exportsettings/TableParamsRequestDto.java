7599c29527a5e337420d7794556b81f4b7e968b9
Chernenko Vitaliy <chernenko_vitaliy@ukr.net>
2025-03-05 16:16:46 +0200

    Second attempt to refactor regex sonar issue;

--- service-api/src/main/java/greencity/dto/exportsettings/TableParamsRequestDto.java
+++ service-api/src/main/java/greencity/dto/exportsettings/TableParamsRequestDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Pattern;
 
 public record TableParamsRequestDto(
-    @Pattern(regexp = "^(?!_)[a-z]+(_[a-z]+)*(?<!_)$", message = ErrorMessage.INVALID_TABLE_NAME) String tableName,
+    @Pattern(regexp = "^(?!_)[a-z]+(?:_[a-z]+){0,10}(?<!_)$", message = ErrorMessage.INVALID_TABLE_NAME) String tableName,
 
     @Min(value = 0, message = ErrorMessage.NEGATIVE_LIMIT) @Max(value = AppConstant.SQL_ROW_LIMIT,
         message = ErrorMessage.EXCEED_LIMIT) int limit,
