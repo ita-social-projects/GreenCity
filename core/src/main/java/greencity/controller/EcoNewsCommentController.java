8f68e4e69d611351f559ffc9acac982fb10a4454
oleksandra-bulhakova <109627866+oleksandra-bulhakova@users.noreply.github.com>
2025-03-25 09:07:43 +0000

    Deploying to gh-pages from  @ e9723769821e8b213f3c6a45e67f7aaf6208303c 🚀

--- core/src/main/java/greencity/controller/EcoNewsCommentController.java
+++ core/src/main/java/greencity/controller/EcoNewsCommentController.java
@@ -202,9 +202,9 @@ public class EcoNewsCommentController {
     }
 
     /**
-     * Method to like/dislike certain {@link CommentDto} specified by id.
+     * Method to like/unlike certain {@link CommentDto} specified by id.
      *
-     * @param commentId of {@link CommentDto} to like/dislike
+     * @param commentId of {@link CommentDto} to like/unlike
      */
     @Operation(summary = "Like/unlike comment.")
     @ApiResponses(value = {
@@ -225,9 +225,9 @@ public class EcoNewsCommentController {
     }
 
     /**
-     * Method to dislike certain {@link CommentDto} specified by id.
+     * Method to dislike/remove dislike certain {@link CommentDto} specified by id.
      *
-     * @param commentId of {@link CommentDto} to like/dislike
+     * @param commentId of {@link CommentDto} to dislike/remove dislike
      */
     @Operation(summary = "Dislike comment.")
     @ApiResponses(value = {
@@ -313,5 +313,52 @@ public class EcoNewsCommentController {
     @MessageMapping("/getUsersToTagInComment")
     public void getUsersToTagInComment(@Payload UserSearchDto searchUsers) {
         commentService.searchUsers(searchUsers);
+    }
+
+    /**
+     * Method to like/unlike certain {@link CommentVO} specified by id.
+     *
+     * @param commentId of {@link CommentVO} to like/unlike
+     * @return an instance of {@link CommentDto} with updated data
+     */
+    @Operation(summary = "Like/unlike comment and get an instance of a comment with updated data.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+        @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST,
+            content = @Content(examples = @ExampleObject(HttpStatuses.BAD_REQUEST))),
+        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED,
+            content = @Content(examples = @ExampleObject(HttpStatuses.UNAUTHORIZED))),
+        @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND,
+            content = @Content(examples = @ExampleObject(HttpStatuses.NOT_FOUND)))
+    })
+    @PostMapping(path = "/comments/likeV2", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommentDto> likeV2(
+        @RequestParam("commentId") Long commentId,
+        @Parameter(hidden = true) @CurrentUser UserVO userVO,
+        @ValidLanguage Locale locale) {
+        return ResponseEntity.ok(commentService.likeV2(commentId, userVO, locale));
+    }
+
+    /**
+     * Method to dislike/remove dislike certain {@link CommentVO} specified by id.
+     *
+     * @param commentId of {@link CommentVO} to dislike/remove dislike
+     * @return an instance of {@link CommentDto} with updated data
+     */
+    @Operation(summary = "Dislike comment and get an instance of a comment with updated data.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+        @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST,
+            content = @Content(examples = @ExampleObject(HttpStatuses.BAD_REQUEST))),
+        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED,
+            content = @Content(examples = @ExampleObject(HttpStatuses.UNAUTHORIZED))),
+        @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND,
+            content = @Content(examples = @ExampleObject(HttpStatuses.NOT_FOUND)))
+    })
+    @PostMapping(path = "/comments/dislikeV2", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommentDto> dislikeV2(
+        @RequestParam("commentId") Long commentId,
+        @Parameter(hidden = true) @CurrentUser UserVO userVO) {
+        return ResponseEntity.ok(commentService.dislikeV2(commentId, userVO));
     }
 }
