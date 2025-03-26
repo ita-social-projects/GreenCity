a2a6061dee7cd7f765b38c9865ea6025cabe58fe
Andrii <Ineedmoreweekends@gmail.com>
2025-03-26 14:54:39 +0200

    This PR should fix swagger 403 error status

--- service/src/main/java/greencity/config/DotenvConfig.java
+++ service/src/main/java/greencity/config/DotenvConfig.java
@@ -5,6 +5,7 @@ import greencity.constant.ErrorMessage;
 import greencity.exception.exceptions.FunctionalityNotAvailableException;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -12,12 +13,19 @@ import org.springframework.context.annotation.Lazy;
 @Configuration
 @Lazy
 public class DotenvConfig {
+
     @Bean
+    public Dotenv fallbackDotenv() {
+        return Dotenv.configure().ignoreIfMissing().load();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(Dotenv.class)
     Dotenv dotenv() {
         try {
             return Dotenv.configure()
-                .filename(AppConstant.DOTENV_FILENAME)
-                .load();
+                    .filename(AppConstant.DOTENV_FILENAME)
+                    .load();
         } catch (DotenvException ignored) {
             throw new FunctionalityNotAvailableException(ErrorMessage.FUNCTIONALITY_NOT_AVAILABLE);
         }
