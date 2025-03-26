484f0d0039b1c6eb3a3cb8500713d83f4d5c5ead
Andrii <Ineedmoreweekends@gmail.com>
2025-03-26 16:56:23 +0200

    Added DotEnvConditionChecker to detect the presence of a dotenv file and automatically create a bean accordingly.

---
+++ service/src/main/java/greencity/config/dotenv/DotEnvConditionChecker.java
@@ -0,0 +1,13 @@
+package greencity.config.dotenv;
+
+import greencity.constant.AppConstant;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DotEnvConditionChecker {
+    public static boolean isEnabled() {
+        return Files.exists(Paths.get(System.getProperty("user.dir") + File.separator + AppConstant.DOTENV_FILENAME));
+    }
+}
--- service/src/main/java/greencity/config/dotenv/DotenvConfig.java
+++ service/src/main/java/greencity/config/dotenv/DotenvConfig.java
@@ -1,25 +1,29 @@
-package greencity.config;
+package greencity.config.dotenv;
 
 import greencity.constant.AppConstant;
 import greencity.constant.ErrorMessage;
 import greencity.exception.exceptions.FunctionalityNotAvailableException;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Lazy
 public class DotenvConfig {
     @Bean
+    @ConditionalOnMissingBean(Dotenv.class)
     public Dotenv fallbackDotenv() {
         return Dotenv.configure().ignoreIfMissing().load();
     }
 
     @Bean
-    @ConditionalOnMissingBean(Dotenv.class)
+    @ConditionalOnExpression("#{T(greencity.config.dotenv.DotEnvConditionChecker).isEnabled()}")
+    @Primary
     Dotenv dotenv() {
         try {
             return Dotenv.configure()
