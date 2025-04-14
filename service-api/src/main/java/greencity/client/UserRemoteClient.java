3a08c142d3617108dbde64651546d6fb65e43484
Maksym Kozak <bersercer70@gmail.com>
2025-04-14 23:25:18 +0300

    add first method to UserRemoteClient

--- service-api/src/main/java/greencity/client/UserRemoteClient.java
+++ service-api/src/main/java/greencity/client/UserRemoteClient.java
@@ -2,7 +2,12 @@ package greencity.client;
 
 import greencity.client.config.UserRemoteClientFallbackFactory;
 import greencity.client.config.UserRemoteClientInterceptor;
+import greencity.dto.user.UserVO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @FeignClient(
         name = "user-remote-client",
@@ -11,4 +16,16 @@ import org.springframework.cloud.openfeign.FeignClient;
         fallbackFactory = UserRemoteClientFallbackFactory.class
 )
 public interface UserRemoteClient {
+
+    String EMAIL = "email";
+
+    /**
+     * Finds {@link UserVO} that is not 'DEACTIVATED' by {@link UserVO}'s Email.
+     *
+     * @param email {@link UserVO}'s Email.
+     * @return {@link Optional} of {@link UserVO}.
+     */
+    @GetMapping("/user/findNotDeactivatedByEmail")
+    Optional<UserVO> findNotDeactivatedByEmail(@RequestParam(EMAIL) String email);
+
 }
--- service-api/src/main/java/greencity/client/config/UserRemoteClientFallbackFactory.java
+++ service-api/src/main/java/greencity/client/config/UserRemoteClientFallbackFactory.java
@@ -2,8 +2,12 @@ package greencity.client.config;
 
 import feign.hystrix.FallbackFactory;
 import greencity.client.UserRemoteClient;
+import greencity.constant.ErrorMessage;
+import greencity.dto.user.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -11,6 +15,11 @@ public class UserRemoteClientFallbackFactory implements FallbackFactory<UserRemo
     @Override
     public UserRemoteClient create(Throwable throwable) {
         return new UserRemoteClient() {
+            @Override
+            public Optional<UserVO> findNotDeactivatedByEmail(String email) {
+                // log.error(ErrorMessage.USER_WITH_THIS_EMAIL_DOES_NOT_EXIST + "{}", email, throwable);
+                return Optional.empty();
+            }
         };
     }
 }
