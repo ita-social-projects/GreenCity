e6dcfc5fff8a4eaef507ce17cf751922bb7955e5
Maksym Kozak <bersercer70@gmail.com>
2025-04-15 01:02:10 +0300

    fix EcoNewsVOMapper

--- service/src/main/java/greencity/mapping/EcoNewsVOMapper.java
+++ service/src/main/java/greencity/mapping/EcoNewsVOMapper.java
@@ -1,27 +1,42 @@
 package greencity.mapping;
 
+import greencity.client.UserRemoteClient;
+import greencity.constant.ErrorMessage;
 import greencity.dto.econews.EcoNewsVO;
 import greencity.dto.language.LanguageVO;
 import greencity.dto.tag.TagTranslationVO;
 import greencity.dto.tag.TagVO;
 import greencity.dto.user.UserVO;
 import greencity.entity.EcoNews;
+import greencity.entity.User;
+import greencity.exception.exceptions.WrongEmailException;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class EcoNewsVOMapper extends AbstractConverter<EcoNews, EcoNewsVO> {
+
+    private final UserRemoteClient userRemoteClient;
+
     @Override
     protected EcoNewsVO convert(EcoNews ecoNews) {
+        User author = ecoNews.getAuthor();
+
+        String authorEmail = author.getEmail();
+        UserVO authorVO = userRemoteClient.findNotDeactivatedByEmail(authorEmail)
+                .orElseThrow(() -> new WrongEmailException(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + authorEmail));
+
         return EcoNewsVO.builder()
             .id(ecoNews.getId())
             .author(UserVO.builder()
-                .id(ecoNews.getAuthor().getId())
-                .name(ecoNews.getAuthor().getName())
-                .email(ecoNews.getAuthor().getEmail())
-                .userStatus(ecoNews.getAuthor().getUserStatus())
-                .role(ecoNews.getAuthor().getRole())
+                .id(author.getId())
+                .name(authorVO.getName())
+                .email(author.getEmail())
+                .userStatus(authorVO.getUserStatus())
+                .role(authorVO.getRole())
                 .build())
             .creationDate(ecoNews.getCreationDate())
             .imagePath(ecoNews.getImagePath())
