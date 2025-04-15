1d0c8afc556fd58b7599cd1f64520e2a12f4732a
Maksym Kozak <bersercer70@gmail.com>
2025-04-15 20:10:01 +0300

    fix EcoNewsAuthorDtoMapper

--- service/src/main/java/greencity/mapping/EcoNewsAuthorDtoMapper.java
+++ service/src/main/java/greencity/mapping/EcoNewsAuthorDtoMapper.java
@@ -1,12 +1,19 @@
 package greencity.mapping;
 
 import greencity.dto.user.EcoNewsAuthorDto;
+import greencity.dto.user.UserVO;
 import greencity.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EcoNewsAuthorDtoMapper extends AbstractConverter<User, EcoNewsAuthorDto> {
+
+    private final ModelMapper modelMapper;
+
     /**
      * Method for converting {@link User} into {@link EcoNewsAuthorDto}.
      *
@@ -15,6 +22,7 @@ public class EcoNewsAuthorDtoMapper extends AbstractConverter<User, EcoNewsAutho
      */
     @Override
     public EcoNewsAuthorDto convert(User author) {
-        return new EcoNewsAuthorDto(author.getId(), author.getName());
+        UserVO authorVO = modelMapper.map(author, UserVO.class);
+        return new EcoNewsAuthorDto(author.getId(), authorVO.getName());
     }
 }
