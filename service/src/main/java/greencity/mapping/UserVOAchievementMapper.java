244ed6b0b5cae3d107aafd23502b19dfdfff2131
Maksym Kozak <bersercer70@gmail.com>
2025-04-15 19:55:23 +0300

    fix UserVOAchievementMapper

--- service/src/main/java/greencity/mapping/UserVOAchievementMapper.java
+++ service/src/main/java/greencity/mapping/UserVOAchievementMapper.java
@@ -5,18 +5,26 @@ import greencity.dto.achievement.UserAchievementVO;
 import greencity.dto.achievement.UserVOAchievement;
 import greencity.dto.user.UserVO;
 import greencity.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class UserVOAchievementMapper extends AbstractConverter<User, UserVOAchievement> {
+
+    private final ModelMapper modelMapper;
+
     @Override
     protected UserVOAchievement convert(User user) {
+        UserVO userVO = modelMapper.map(user, UserVO.class);
+
         return UserVOAchievement.builder()
             .id(user.getId())
-            .name(user.getName())
-            .userAchievements(user.getUserAchievements() != null ? user.getUserAchievements()
+            .name(userVO.getName())
+            .userAchievements(userVO.getUserAchievements() != null ? userVO.getUserAchievements()
                 .stream().map(userAchievement -> UserAchievementVO.builder()
                     .id(userAchievement.getId())
                     .user(UserVO.builder()
