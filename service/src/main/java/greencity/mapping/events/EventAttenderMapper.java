99ed5dd59a4c15f9125553d683140708f00904aa
Maksym Kozak <bersercer70@gmail.com>
2025-04-15 20:04:23 +0300

    fix EventAttenderMapper

--- service/src/main/java/greencity/mapping/events/EventAttenderMapper.java
+++ service/src/main/java/greencity/mapping/events/EventAttenderMapper.java
@@ -1,7 +1,9 @@
 package greencity.mapping.events;
 
 import greencity.dto.event.EventAttenderDto;
+import greencity.dto.user.UserVO;
 import greencity.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -11,10 +13,16 @@ import org.springframework.stereotype.Component;
  * {@link EventAttenderDto}.
  */
 @Component
+@RequiredArgsConstructor
 public class EventAttenderMapper extends AbstractConverter<User, EventAttenderDto> {
+
+    private final ModelMapper modelMapper;
+
     @Override
     protected EventAttenderDto convert(User user) {
-        return EventAttenderDto.builder().id(user.getId()).imagePath(user.getProfilePicturePath())
-            .name(user.getName()).build();
+        UserVO userVO = modelMapper.map(user, UserVO.class);
+
+        return EventAttenderDto.builder().id(user.getId()).imagePath(userVO.getProfilePicturePath())
+            .name(userVO.getName()).build();
     }
 }
