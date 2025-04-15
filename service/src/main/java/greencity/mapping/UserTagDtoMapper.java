93c01e4880d64f2d40b10e9129a7c6997edd9918
Maksym Kozak <bersercer70@gmail.com>
2025-04-15 19:56:35 +0300

    fix UserTagDtoMapper

--- service/src/main/java/greencity/mapping/UserTagDtoMapper.java
+++ service/src/main/java/greencity/mapping/UserTagDtoMapper.java
@@ -1,18 +1,27 @@
 package greencity.mapping;
 
 import greencity.dto.user.UserTagDto;
+import greencity.dto.user.UserVO;
 import greencity.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserTagDtoMapper extends AbstractConverter<User, UserTagDto> {
+
+    private final ModelMapper modelMapper;
+
     @Override
     protected UserTagDto convert(User user) {
+        UserVO userVO = modelMapper.map(user, UserVO.class);
+
         return UserTagDto.builder()
             .userId(user.getId())
-            .userName(user.getName())
-            .profilePicture(user.getProfilePicturePath())
+            .userName(userVO.getName())
+            .profilePicture(userVO.getProfilePicturePath())
             .build();
     }
 }
