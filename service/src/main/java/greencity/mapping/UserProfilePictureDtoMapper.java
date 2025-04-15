889e655319d6afdebca4daebad5e25149c323b03
Maksym Kozak <bersercer70@gmail.com>
2025-04-15 20:12:41 +0300

    fix UserProfilePictureDtoMapper

--- service/src/main/java/greencity/mapping/UserProfilePictureDtoMapper.java
+++ service/src/main/java/greencity/mapping/UserProfilePictureDtoMapper.java
@@ -1,18 +1,27 @@
 package greencity.mapping;
 
 import greencity.dto.user.UserProfilePictureDto;
+import greencity.dto.user.UserVO;
 import greencity.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserProfilePictureDtoMapper extends AbstractConverter<User, UserProfilePictureDto> {
+
+    private final ModelMapper modelMapper;
+
     @Override
     protected UserProfilePictureDto convert(User user) {
+        UserVO userVO = modelMapper.map(user, UserVO.class);
+
         return UserProfilePictureDto.builder()
             .id(user.getId())
-            .name(user.getName())
-            .profilePicturePath(user.getProfilePicturePath())
+            .name(userVO.getName())
+            .profilePicturePath(userVO.getProfilePicturePath())
             .build();
     }
 }
