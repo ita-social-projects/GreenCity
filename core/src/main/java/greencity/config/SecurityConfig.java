b37b23583a4cca390bab0b15618bb1b676ecba5e
ChernenkoVitaliy <Chernenko_vitaliy@ukr.net>
2025-04-16 14:32:29 +0000

    Deploying to gh-pages from  @ cd24e61701c26659ff1c77c39cd9a7177099fa93 🚀

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -87,7 +87,6 @@ public class SecurityConfig {
     private static final String INVITATION_ID = "/{invitationId}";
     private static final String COMMIT_INFO = "/commit-info";
     public static final String LOGS = "/logs/**";
-    public static final String SETTINGS_EXPORT = "/export/settings/**";
     private final JwtTool jwtTool;
     private final UserService userService;
     private final AuthenticationConfiguration authenticationConfiguration;
@@ -295,8 +294,7 @@ public class SecurityConfig {
                     FRIENDS,
                     NOTIFICATIONS,
                     HABIT_ASSIGN_ID + "/friends/habit-duration-info",
-                    "/ai/**",
-                    SETTINGS_EXPORT)
+                    "/ai/**")
                 .hasAnyRole(USER, ADMIN, MODERATOR, UBS_EMPLOYEE)
                 .requestMatchers(HttpMethod.POST,
                     CATEGORIES,
