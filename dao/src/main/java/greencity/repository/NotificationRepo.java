503e17f1d71d8aca7d4708d43d0e48d2ddba8b98
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-04-18 08:25:04 +0300

    formatted

--- dao/src/main/java/greencity/repository/NotificationRepo.java
+++ dao/src/main/java/greencity/repository/NotificationRepo.java
@@ -4,7 +4,6 @@ import greencity.entity.Notification;
 import greencity.enums.NotificationType;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
