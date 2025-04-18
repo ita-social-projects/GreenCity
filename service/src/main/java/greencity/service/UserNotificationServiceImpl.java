c48724b33ba5025069c5d419e14e4c108c787ffc
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-04-18 08:40:19 +0300

    formatted

--- service/src/main/java/greencity/service/UserNotificationServiceImpl.java
+++ service/src/main/java/greencity/service/UserNotificationServiceImpl.java
@@ -679,8 +679,7 @@ public class UserNotificationServiceImpl implements UserNotificationService {
      * that match the provided search request. The results are sorted by date in
      * descending order and returned as a pageable DTO.
      *
-     * <p>
-     * The search is case-insensitive and checks for matches in the notification's
+     * <p>The search is case-insensitive and checks for matches in the notification's
      * title, body, message, second message, and action user text.
      *
      * @param page          the pagination information without sorting
