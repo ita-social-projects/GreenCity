72d1665565b66a09e6761efc78b0781e0468a611
Maksym Kozak <bersercer70@gmail.com>
2025-04-20 16:41:55 +0300

    fix EcoNewsVOMapperTest

--- service/src/test/java/greencity/mapping/EcoNewsVOMapperTest.java
+++ service/src/test/java/greencity/mapping/EcoNewsVOMapperTest.java
@@ -1,27 +1,41 @@
 package greencity.mapping;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import greencity.ModelUtils;
 import greencity.dto.econews.EcoNewsVO;
 import greencity.dto.tag.TagVO;
 import greencity.dto.user.UserVO;
 import greencity.entity.EcoNews;
-import java.util.stream.Collectors;
-
+import greencity.entity.User;
+import greencity.enums.Role;
+import greencity.enums.UserStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EcoNewsVOMapperTest {
+
+    @Mock
+    ModelMapper modelMapper;
+
     @InjectMocks
     EcoNewsVOMapper ecoNewsVOMapper;
 
     @Test
     void convert() {
         EcoNews ecoNews = ModelUtils.getEcoNews();
+        User author = ecoNews.getAuthor();
+        UserVO authorVO = mock(UserVO.class);
+        UserStatus userStatus = ecoNews.getAuthor().getUserStatus();
+        Role role = Role.ROLE_USER;
 
         EcoNewsVO expected = EcoNewsVO.builder()
             .id(ecoNews.getId())
@@ -30,7 +44,7 @@ class EcoNewsVOMapperTest {
                 .name(ecoNews.getAuthor().getName())
                 .email(ecoNews.getAuthor().getEmail())
                 .userStatus(ecoNews.getAuthor().getUserStatus())
-                //.role(ecoNews.getAuthor().getRole())
+                .role(role)
                 .build())
             .creationDate(ecoNews.getCreationDate())
             .imagePath(ecoNews.getImagePath())
@@ -53,6 +67,13 @@ class EcoNewsVOMapperTest {
                     .build())
                 .collect(Collectors.toSet()))
             .build();
+
+        when(modelMapper.map(author, UserVO.class))
+                .thenReturn(authorVO);
+        when(authorVO.getUserStatus())
+                .thenReturn(userStatus);
+        when(authorVO.getRole())
+                .thenReturn(role);
 
         assertEquals(expected, ecoNewsVOMapper.convert(ecoNews));
     }
