91d18d2d32d188689ae6ac3fc5c5fd6250884498
Maksym Kozak <bersercer70@gmail.com>
2025-04-20 15:36:34 +0300

    fix UserProfilePictureDtoMapperTest

--- service/src/test/java/greencity/mapping/UserProfilePictureDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/UserProfilePictureDtoMapperTest.java
@@ -2,28 +2,43 @@ package greencity.mapping;
 
 import greencity.ModelUtils;
 import greencity.dto.user.UserProfilePictureDto;
+import greencity.dto.user.UserVO;
 import greencity.entity.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class UserProfilePictureDtoMapperTest {
+
+    @Mock
+    ModelMapper modelMapper;
+
     @InjectMocks
-    private UserProfilePictureDtoMapper mapper;
+    UserProfilePictureDtoMapper mapper;
 
     @Test
     void convertTest() {
         User user = ModelUtils.getUser();
-
+        UserVO userVO = mock(UserVO.class);
         UserProfilePictureDto expected = UserProfilePictureDto.builder()
-            .id(user.getId())
-            .name(user.getName())
-            .profilePicturePath(user.getProfilePicturePath())
-            .build();
+                .id(user.getId())
+                .name(user.getName())
+                .profilePicturePath(user.getProfilePicturePath())
+                .build();
+
+        when(modelMapper.map(user, UserVO.class))
+                .thenReturn(userVO);
+        when(userVO.getProfilePicturePath())
+                .thenReturn(expected.getProfilePicturePath());
 
         assertEquals(expected, mapper.convert(user));
     }
