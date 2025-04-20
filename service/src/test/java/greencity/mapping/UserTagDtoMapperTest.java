23aa36bc286ce061709ad571e7ce6595bb759a17
Maksym Kozak <bersercer70@gmail.com>
2025-04-20 16:04:35 +0300

    fix UserTagDtoMapperTest

--- service/src/test/java/greencity/mapping/UserTagDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/UserTagDtoMapperTest.java
@@ -1,18 +1,26 @@
 package greencity.mapping;
 
 import greencity.dto.user.UserTagDto;
+import greencity.dto.user.UserVO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 import static greencity.ModelUtils.getTagUser;
-
 import static greencity.ModelUtils.getUserTagDto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserTagDtoMapperTest {
+
+    @Mock
+    ModelMapper modelMapper;
+
     @InjectMocks
     UserTagDtoMapper mapper;
 
@@ -20,6 +28,13 @@ class UserTagDtoMapperTest {
     void convertTest() {
         var user = getTagUser();
         var expected = getUserTagDto();
+        UserVO userVO = mock(UserVO.class);
+
+        when(modelMapper.map(user, UserVO.class))
+                .thenReturn(userVO);
+        when(userVO.getProfilePicturePath())
+                .thenReturn(expected.getProfilePicture());
+
         UserTagDto actual = mapper.convert(user);
         assertEquals(expected, actual);
     }
