5eb942c44d34f77d33d693c28e27c6e7c30fbf25
Maksym Kozak <bersercer70@gmail.com>
2025-04-23 01:22:20 +0300

    fix EventCommentDtoMapperTest

--- service/src/test/java/greencity/mapping/EventCommentDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/EventCommentDtoMapperTest.java
@@ -2,26 +2,18 @@ package greencity.mapping;
 
 import greencity.ModelUtils;
 import greencity.dto.comment.CommentDto;
-import greencity.dto.user.UserVO;
 import greencity.entity.Comment;
 import greencity.entity.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EventCommentDtoMapperTest {
-
-    @Mock
-    ModelMapper modelMapper;
 
     @InjectMocks
     CommentDtoMapper commentDtoMapper;
@@ -30,13 +22,6 @@ class EventCommentDtoMapperTest {
     void convertTest() {
         Comment comment = ModelUtils.getComment().setParentComment(new Comment().setId(2L));
         User commentUser = comment.getUser();
-        UserVO commentUserVO = mock(UserVO.class);
-        String profilePicturePath = "profile picture path";
-
-        when(modelMapper.map(commentUser, UserVO.class))
-            .thenReturn(commentUserVO);
-        when(commentUserVO.getProfilePicturePath())
-            .thenReturn(profilePicturePath);
 
         CommentDto actual = commentDtoMapper.convert(comment);
 
@@ -49,6 +34,6 @@ class EventCommentDtoMapperTest {
         assertEquals(comment.getParentComment().getId(), actual.getParentCommentId(), "Parent comment ID mismatch");
         assertEquals(commentUser.getId(), actual.getAuthor().getId());
         assertEquals(commentUser.getName(), actual.getAuthor().getName());
-        assertEquals(profilePicturePath, actual.getAuthor().getProfilePicturePath());
+        assertEquals(commentUser.getProfilePicturePath(), actual.getAuthor().getProfilePicturePath());
     }
 }
