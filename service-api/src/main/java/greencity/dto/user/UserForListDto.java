c787b80ec2fd17a11f8f9d25035995248a2b78bb
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-04-23 17:58:08 +0300

    formatted

--- service-api/src/main/java/greencity/dto/user/UserForListDto.java
+++ service-api/src/main/java/greencity/dto/user/UserForListDto.java
@@ -13,7 +13,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Builder;
-
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
--- service-api/src/main/java/greencity/dto/user/UserVO.java
+++ service-api/src/main/java/greencity/dto/user/UserVO.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
