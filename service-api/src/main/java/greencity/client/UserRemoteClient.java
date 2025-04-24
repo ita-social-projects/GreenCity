5b5337dccee0738b51bdf59256484ee16b365868
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-04-24 09:05:53 +0300

    mvn clean install

--- service-api/src/main/java/greencity/client/UserRemoteClient.java
+++ service-api/src/main/java/greencity/client/UserRemoteClient.java
@@ -126,8 +126,10 @@ public interface UserRemoteClient {
      * Retrieves the list of IDs of users who have the user status set to
      * {@code ACTIVATED}.
      *
-     * @param ids a list of user IDs to check; may be {@code null} or empty to indicate all users
-     * @return a list of {@code Long} values representing the IDs of all activated users
+     * @param ids a list of user IDs to check; may be {@code null} or empty to
+     *            indicate all users
+     * @return a list of {@code Long} values representing the IDs of all activated
+     *         users
      */
     @GetMapping("/user/activated-ids")
     List<Long> getActivatedUsersIds(@RequestParam(value = "ids", required = false) List<Long> ids);
