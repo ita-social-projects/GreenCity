db7f8263b67edd0f2bcb28dab28d407ab4946c82
Andrii-Danylenko <62876824+Andrii-Danylenko@users.noreply.github.com>
2025-04-25 17:43:15 +0000

    Deploying to gh-pages from  @ 2a9c2198ae35be0bbf6b6e14b420f1fcde39efaf 🚀

--- service-api/src/main/java/greencity/service/EventService.java
+++ service-api/src/main/java/greencity/service/EventService.java
@@ -5,6 +5,7 @@ import greencity.dto.PageableDto;
 import greencity.dto.event.AddEventDtoRequest;
 import greencity.dto.event.AddressDto;
 import greencity.dto.event.EventAttenderDto;
+import greencity.dto.event.EventCityDto;
 import greencity.dto.event.EventDto;
 import greencity.dto.event.EventResponseDto;
 import greencity.dto.event.EventVO;
@@ -393,4 +394,15 @@ public interface EventService {
      *         the user, or an empty pageable if none are found.
      */
     Page<EventResponseDto> getPageableAllEventsAttendedByUser(Pageable pageable, Long userId);
+
+    /**
+     * Returns a list of cities relevant to the user, including the user's city (if
+     * available) and the top three cities with the most events.
+     *
+     * @param userVO {@link UserVO} - represents the current user.
+     * @return {@link List} of {@link EventCityDto} containing cities sorted in
+     *         descending order based on the number of events.
+     * @author Andrii Danylenko
+     */
+    List<EventCityDto> getAllRelevantEventsCityByUser(UserVO userVO);
 }
