10a209ad253b134662b608fc41485fefb176f920
SonnyRose <keeganwunder0@gmail.com>
2025-04-27 14:20:26 +0300

    Formatted GrammarChecker.java using formatter-maven-plugin

--- service/src/main/java/greencity/service/GrammarChecker.java
+++ service/src/main/java/greencity/service/GrammarChecker.java
@@ -21,26 +21,18 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
- * Service class for checking and correcting grammar in text.
- * This class integrates with JLanguageTool for grammar checking and OpenNLP for language detection.
- * It also supports caching of results for efficiency.
- *
- * <p>
- * The main flow is:
- * 1. Check grammar using the `checkGrammar` method.
- * 2. Detect the language of the provided text via the `detectLanguage` method.
- * 3. Correct the grammar using `applyCorrections` based on detected issues.
- * 4. Cache the results to avoid redundant processing.
- * 5. Clear cached data using `clearCache` when necessary.
- *
- * <p>
- * Cache is used to store:
- * - The corrected text (for grammar checking).
- * - The detected language (to avoid re-detecting the language every time).
- *
- * <p>
- * Cache will be evicted either manually (via the `clearCache` method) or
- * automatically according to the configured expiration settings in the cache configuration.
+ * Service class for checking and correcting grammar in text. This class
+ * integrates with JLanguageTool for grammar checking and OpenNLP for language
+ * detection. It also supports caching of results for efficiency. The main flow
+ * is: 1. Check grammar using the `checkGrammar` method. 2. Detect the language
+ * of the provided text via the `detectLanguage` method. 3. Correct the grammar
+ * using `applyCorrections` based on detected issues. 4. Cache the results to
+ * avoid redundant processing. 5. Clear cached data using `clearCache` when
+ * necessary. Cache is used to store: - The corrected text (for grammar
+ * checking). - The detected language (to avoid re-detecting the language every
+ * time). Cache will be evicted either manually (via the `clearCache` method) or
+ * automatically according to the configured expiration settings in the cache
+ * configuration.
  */
 
 @Service
@@ -51,32 +43,38 @@ public class GrammarChecker implements GrammarCheckerService {
     private final CacheManager cacheManager;
 
     public GrammarChecker(@Qualifier("englishTool") JLanguageTool enLangTool,
-                          @Qualifier("ukrainianTool") JLanguageTool uaLangTool,
-                          CacheManager cacheManager) {
+        @Qualifier("ukrainianTool") JLanguageTool uaLangTool,
+        CacheManager cacheManager) {
         this.enLangTool = enLangTool;
         this.uaLangTool = uaLangTool;
         this.cacheManager = cacheManager;
     }
 
     /**
-     * Checks and corrects the grammar of the provided text.
+     * Checks and corrects the grammar of the provided text. This method uses the
+     * configured grammar checking tool (JLanguageTool) to find and apply
+     * corrections to grammar issues in the input text. The result is cached for
+     * future use, so the same text does not need to be checked again if it has
+     * already been processed.
      *
-     * <p>This method uses the configured grammar checking tool (JLanguageTool) to find and apply corrections
-     * to grammar issues in the input text. The result is cached for future use, so the same text does not
-     * need to be checked again if it has already been processed.</p>
-     *
-     * @param text the text to check and correct.
-     *              This is the input text which will be processed for grammar errors.
-     * @return the corrected text with grammar fixes applied.
-     *         If no errors are found, the original text will be returned unchanged.
-     * @throws IOException if an error occurs during grammar checking.
-     *         This could happen if the grammar tool encounters an I/O issue while processing the text.
+     * @param text the text to check and correct. This is the input text which will
+     *             be processed for grammar errors.
+     * @return the corrected text with grammar fixes applied. If no errors are
+     *         found, the original text will be returned unchanged.
+     * @throws IOException           if an error occurs during grammar checking.
+     *                               This could happen if the grammar tool
+     *                               encounters an I/O issue while processing the
+     *                               text.
      * @throws GrammarCheckException if an error occurs during grammar checking.
-     *         This could happen due to unexpected exceptions or issues in processing the grammar.
-     *
-     * <p>This method first checks if a cached version of the result for the provided text is available in
-     * the `languageCache`. If available, it returns the cached value. If not, it proceeds with grammar checking
-     * using the appropriate language tool, applies the necessary corrections, caches the result, and returns it.</p>
+     *                               This could happen due to unexpected exceptions
+     *                               or issues in processing the grammar. This
+     *                               method first checks if a cached version of the
+     *                               result for the provided text is available in
+     *                               the `languageCache`. If available, it returns
+     *                               the cached value. If not, it proceeds with
+     *                               grammar checking using the appropriate language
+     *                               tool, applies the necessary corrections, caches
+     *                               the result, and returns it.
      */
     @Cacheable(value = "languageCache", key = "#text")
     @Override
@@ -95,21 +93,21 @@ public class GrammarChecker implements GrammarCheckerService {
         }
     }
 
-
     /**
-     * Clears the cache for the provided text.
+     * Clears the cache for the provided text. This method removes the cached
+     * grammar results and detected language for the specified text, both in the
+     * `languageCache` and `grammarCache` caches. It can be used when the data for a
+     * specific text needs to be invalidated, for example, after an update or change
+     * in the text that might require re-checking the grammar or re-detecting the
+     * language.
      *
-     * <p>This method removes the cached grammar results and detected language for the specified text,
-     * both in the `languageCache` and `grammarCache` caches. It can be used when the data for a specific
-     * text needs to be invalidated, for example, after an update or change in the text that might require
-     * re-checking the grammar or re-detecting the language.</p>
-     *
-     * @param text the text whose cache needs to be cleared.
-     *             The cache entries associated with this text will be evicted from both `languageCache` and `grammarCache`.
-     *
-     * <p>This method is typically called when a user explicitly requests the removal of cached data
-     * (e.g., after modifying or updating a text). The cache is cleared from both caches to ensure
-     * that subsequent requests for the same text will trigger a fresh evaluation of the grammar and language.</p>
+     * @param text the text whose cache needs to be cleared. The cache entries
+     *             associated with this text will be evicted from both
+     *             `languageCache` and `grammarCache`. This method is typically
+     *             called when a user explicitly requests the removal of cached data
+     *             (e.g., after modifying or updating a text). The cache is cleared
+     *             from both caches to ensure that subsequent requests for the same
+     *             text will trigger a fresh evaluation of the grammar and language.
      */
     @Override
     @CacheEvict(value = {"languageCache", "grammarCache"}, key = "#text")
@@ -119,15 +117,16 @@ public class GrammarChecker implements GrammarCheckerService {
     }
 
     /**
-     * Detects the language of the provided text.
+     * Detects the language of the provided text. This method uses OpenNLP's
+     * language detection tools to determine the language of the provided text. The
+     * detected language is cached to avoid re-detecting the language for the same
+     * text in the future.
      *
-     * <p>This method uses OpenNLP's language detection tools to determine the language of the provided
-     * text. The detected language is cached to avoid re-detecting the language for the same text in the future.</p>
-     *
-     * @param text the text whose language needs to be detected.
-     *             This text will be analyzed to determine its language.
-     * @return the detected language code (e.g., "en" for English, "uk" for Ukrainian).
-     *         This is the language code corresponding to the detected language of the input text.
+     * @param text the text whose language needs to be detected. This text will be
+     *             analyzed to determine its language.
+     * @return the detected language code (e.g., "en" for English, "uk" for
+     *         Ukrainian). This is the language code corresponding to the detected
+     *         language of the input text.
      */
     private String detectLanguage(String text) {
         String cachedLanguageResult = getCacheValue(text);
@@ -152,17 +151,20 @@ public class GrammarChecker implements GrammarCheckerService {
     }
 
     /**
-     * Retrieves the appropriate JLanguageTool instance based on the detected language.
+     * Retrieves the appropriate JLanguageTool instance based on the detected
+     * language. This method selects the correct grammar checking tool
+     * (JLanguageTool) based on the provided language code. For English, it returns
+     * the English language tool; for Ukrainian, it returns the Ukrainian language
+     * tool.
      *
-     * <p>This method selects the correct grammar checking tool (JLanguageTool) based on the provided language code.
-     * For English, it returns the English language tool; for Ukrainian, it returns the Ukrainian language tool.</p>
-     *
-     * @param language the detected language code (e.g., "en" for English, "uk" for Ukrainian).
-     * @return the JLanguageTool instance for the detected language.
-     *         This instance will be used to check the grammar of the text in the corresponding language.
-     *
-     * <p>If the language code provided is "en", the English language tool is returned. If it is not "en", the
-     * Ukrainian language tool is returned by default. You can extend this logic if more languages are supported in the future.</p>
+     * @param language the detected language code (e.g., "en" for English, "uk" for
+     *                 Ukrainian).
+     * @return the JLanguageTool instance for the detected language. This instance
+     *         will be used to check the grammar of the text in the corresponding
+     *         language. If the language code provided is "en", the English language
+     *         tool is returned. If it is not "en", the Ukrainian language tool is
+     *         returned by default. You can extend this logic if more languages are
+     *         supported in the future.
      */
     private JLanguageTool getLanguageTool(String language) {
         return ENGLISH_PREFIX.equals(language) ? enLangTool : uaLangTool;
@@ -170,19 +172,21 @@ public class GrammarChecker implements GrammarCheckerService {
 
     /**
      * Applies grammar corrections to the text based on the provided rule matches.
+     * This method takes the text and a list of `RuleMatch` objects, which represent
+     * the grammar issues identified by the grammar checking tool. It then applies
+     * the suggested corrections to the text.
      *
-     * <p>This method takes the text and a list of `RuleMatch` objects, which represent the grammar issues
-     * identified by the grammar checking tool. It then applies the suggested corrections to the text.</p>
-     *
-     * @param text the original text with potential grammar issues.
-     * @param matches the list of `RuleMatch` objects, each representing a grammar issue found in the text.
-     *                These contain the position of the error and suggested corrections.
-     * @return the corrected text with all the suggested changes applied.
-     *         If there are no grammar issues, the original text is returned unchanged.
-     *
-     * <p>This method iterates over the list of `RuleMatch` objects, replacing the erroneous parts of the text
-     * with the suggested replacements. It takes care to adjust for changes in the text's length as corrections
-     * are applied, ensuring the correct positions are updated. The method returns the fully corrected text.</p>
+     * @param text    the original text with potential grammar issues.
+     * @param matches the list of `RuleMatch` objects, each representing a grammar
+     *                issue found in the text. These contain the position of the
+     *                error and suggested corrections.
+     * @return the corrected text with all the suggested changes applied. If there
+     *         are no grammar issues, the original text is returned unchanged. This
+     *         method iterates over the list of `RuleMatch` objects, replacing the
+     *         erroneous parts of the text with the suggested replacements. It takes
+     *         care to adjust for changes in the text's length as corrections are
+     *         applied, ensuring the correct positions are updated. The method
+     *         returns the fully corrected text.
      */
     private String applyCorrections(String text, List<RuleMatch> matches) {
         StringBuilder correctedText = new StringBuilder(text);
@@ -203,16 +207,16 @@ public class GrammarChecker implements GrammarCheckerService {
     }
 
     /**
-     * Retrieves the cached value for the given key.
-     *
-     * <p>This method checks if a cache exists for the provided key in the `languageCache`. If the cache is
-     * found, it retrieves the cached value; otherwise, it returns null.</p>
+     * Retrieves the cached value for the given key. This method checks if a cache
+     * exists for the provided key in the `languageCache`. If the cache is found, it
+     * retrieves the cached value; otherwise, it returns null.
      *
      * @param key the cache key (text to be used for looking up the cache).
-     * @return the cached value if found, or null if the key does not exist in the cache.
-     *
-     * <p>The method ensures that only valid cache entries are retrieved. If the cache does not exist, it logs
-     * an error message and returns null. This prevents unnecessary errors in the flow when accessing the cache.</p>
+     * @return the cached value if found, or null if the key does not exist in the
+     *         cache. The method ensures that only valid cache entries are
+     *         retrieved. If the cache does not exist, it logs an error message and
+     *         returns null. This prevents unnecessary errors in the flow when
+     *         accessing the cache.
      */
     private String getCacheValue(String key) {
         Cache cache = cacheManager.getCache(LANGUAGE_CACHE_NAME);
@@ -223,16 +227,15 @@ public class GrammarChecker implements GrammarCheckerService {
     }
 
     /**
-     * Puts a value into the cache with the given key.
+     * Puts a value into the cache with the given key. This method stores the
+     * provided value in the `languageCache` using the provided key. It ensures that
+     * the cache is updated with the latest processed result for reuse.
      *
-     * <p>This method stores the provided value in the `languageCache` using the provided key.
-     * It ensures that the cache is updated with the latest processed result for reuse.</p>
-     *
-     * @param key the cache key (text to be used as the cache key).
-     * @param value the value to cache (processed text or detected language).
-     *
-     * <p>If the cache is available, the value is stored under the provided key. If the cache is unavailable,
-     * the method does nothing, but the cache miss is logged for transparency.</p>
+     * @param key   the cache key (text to be used as the cache key).
+     * @param value the value to cache (processed text or detected language). If the
+     *              cache is available, the value is stored under the provided key.
+     *              If the cache is unavailable, the method does nothing, but the
+     *              cache miss is logged for transparency.
      */
     private void putCacheValue(String key, String value) {
         Cache cache = cacheManager.getCache(LANGUAGE_CACHE_NAME);
@@ -242,16 +245,16 @@ public class GrammarChecker implements GrammarCheckerService {
     }
 
     /**
-     * Evicts the cache entry for the given key.
+     * Evicts the cache entry for the given key. This method removes the cache entry
+     * for the provided key from the specified cache. It is typically used when data
+     * needs to be invalidated or refreshed (e.g., after a change in the text).
      *
-     * <p>This method removes the cache entry for the provided key from the specified cache.
-     * It is typically used when data needs to be invalidated or refreshed (e.g., after a change in the text).</p>
-     *
-     * @param cacheName the name of the cache (e.g., "languageCache" or "grammarCache").
-     * @param key the cache key (text for which the cache entry will be evicted).
-     *
-     * <p>The method checks if the cache exists before attempting to evict the entry. If the cache is not found,
-     * it logs an error message for clarity.</p>
+     * @param cacheName the name of the cache (e.g., "languageCache" or
+     *                  "grammarCache").
+     * @param key       the cache key (text for which the cache entry will be
+     *                  evicted). The method checks if the cache exists before
+     *                  attempting to evict the entry. If the cache is not found, it
+     *                  logs an error message for clarity.
      */
     private void evictCache(String cacheName, String key) {
         Cache cache = cacheManager.getCache(cacheName);
