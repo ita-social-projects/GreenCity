3da08c348d36b9710a3c57d5131ac524dc2f1cff
Maksym Kozak <bersercer70@gmail.com>
2025-04-29 11:40:21 +0300

    add first method to UserRemoteWebClient

--- service-api/src/main/java/greencity/client/UserRemoteWebClient.java
+++ service-api/src/main/java/greencity/client/UserRemoteWebClient.java
@@ -1,8 +1,11 @@
 package greencity.client;
 
+import greencity.dto.user.UserVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,28 @@ public class UserRemoteWebClient {
 
     private final WebClient webClient;
 
+    private static final String PAGE_QUERY_PARAM = "page";
+    private static final String PAGE_SIZE_QUERY_PARAM = "size";
+    private static final String USER_EMAIL_QUERY_PARAM = "email";
+
+
+    /**
+     * Finds {@link UserVO} that is not 'DEACTIVATED' by {@link UserVO}'s Email.
+     *
+     * @param email {@link UserVO}'s Email.
+     * @return {@link Optional} of {@link UserVO}.
+     */
+    public Optional<UserVO> findNotDeactivatedByEmail(String email) {
+        String path = "/user/findNotDeactivatedByEmail";
+        UserVO userVO = webClient.get()
+                .uri(uriBuilder ->
+                        uriBuilder.path(path)
+                                .queryParam(USER_EMAIL_QUERY_PARAM, email)
+                                .build()
+                )
+                .retrieve()
+                .bodyToMono(UserVO.class)
+                .block();
+        return Optional.of(userVO);
+    }
 }
