54ba79e08b8575beb7ae6fa88b6442f44100365b
Maksym Kozak <bersercer70@gmail.com>
2025-05-07 20:37:48 +0300

    move mapping logic from service to UserVOMapper and manually set userCredo since it will no longer come from GreenCityUser

--- service/src/main/java/greencity/mapping/UserVOMapper.java
+++ service/src/main/java/greencity/mapping/UserVOMapper.java
@@ -2,22 +2,39 @@ package greencity.mapping;
 
 import greencity.client.UserRemoteClient;
 import greencity.constant.ErrorMessage;
+import greencity.dto.location.UserLocationDto;
 import greencity.dto.user.UserVO;
 import greencity.entity.User;
+import greencity.entity.UserLocation;
 import greencity.exception.exceptions.WrongEmailException;
-import lombok.RequiredArgsConstructor;
 import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class UserVOMapper extends AbstractConverter<User, UserVO> {
+    private final ModelMapper modelMapper;
     private final UserRemoteClient userRemoteClient;
+
+    @Lazy
+    public UserVOMapper(ModelMapper modelMapper, UserRemoteClient userRemoteClient) {
+        this.modelMapper = modelMapper;
+        this.userRemoteClient = userRemoteClient;
+    }
 
     @Override
     protected UserVO convert(User user) {
         String email = user.getEmail();
-        return userRemoteClient.findNotDeactivatedByEmail(email)
+        UserVO userVO = userRemoteClient.findNotDeactivatedByEmail(email)
             .orElseThrow(() -> new WrongEmailException(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + email));
+        UserLocation userLocation = user.getUserLocation();
+        if (userLocation != null) {
+            UserLocationDto userLocationDto = modelMapper.map(userLocation, UserLocationDto.class);
+            userVO.setUserLocation(userLocationDto);
+        }
+        userVO.setUserCredo(user.getUserCredo());
+
+        return userVO;
     }
 }
