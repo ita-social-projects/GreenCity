5449bba33954533b7e11f820690379c3a4f7d9a7
Maksym Kozak <bersercer70@gmail.com>
2025-05-08 19:30:18 +0300

    expose endpoint to get user rating by user id

--- core/src/main/java/greencity/controller/UserController.java
+++ core/src/main/java/greencity/controller/UserController.java
@@ -172,6 +172,27 @@ public class UserController {
     }
 
     /**
+     * Method to get user rating by user id.
+     *
+     * @param userId user id.
+     * @return {@link String} user rating.
+     */
+    @Operation(summary = "Get user rating")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST,
+                    content = @Content(examples = @ExampleObject(HttpStatuses.BAD_REQUEST))),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED,
+                    content = @Content(examples = @ExampleObject(HttpStatuses.UNAUTHORIZED))),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND,
+                    content = @Content(examples = @ExampleObject(HttpStatuses.NOT_FOUND)))
+    })
+    @GetMapping("/{id}/rating")
+    public ResponseEntity<Double> findUserRatingByUserId(@PathVariable("id") Long userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findUserRating(userId));
+    }
+
+    /**
      * Method to update user credo by user id.
      *
      * @param updateUserCredoDto containing update information.
