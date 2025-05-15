8ab8a850e1a132129d3ca6f45a9db0a5fc435554
Maksym Kozak <bersercer70@gmail.com>
2025-05-15 21:01:47 +0300

    remove @CurrentuserId usages near HabitAssignService and HabitAssignController

--- service-api/src/main/java/greencity/service/HabitAssignService.java
+++ service-api/src/main/java/greencity/service/HabitAssignService.java
@@ -349,12 +349,12 @@ public interface HabitAssignService {
     /**
      * Method invite friends to your habit with email notification.
      *
-     * @param userVO     {@link UserVO} user.
+     * @param userId     current user id.
      * @param friendsIds {@link Long} User friends ids.
      * @param habitId    {@link Long} habit id.
      * @param locale     {@link Locale} language.
      */
-    void inviteFriendForYourHabitWithEmailNotification(UserVO userVO, List<Long> friendsIds, Long habitId,
+    void inviteFriendForYourHabitWithEmailNotification(Long userId, List<Long> friendsIds, Long habitId,
         Locale locale);
 
     /**
