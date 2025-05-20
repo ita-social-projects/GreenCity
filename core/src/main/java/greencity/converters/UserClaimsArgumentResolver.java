c70791ffb2854e8ef22381d5269ae51e87d1979e
Maksym Kozak <bersercer70@gmail.com>
2025-05-20 12:07:38 +0300

    do not throw exception when resolving request with no auth header

--- core/src/main/java/greencity/converters/UserClaimsArgumentResolver.java
+++ core/src/main/java/greencity/converters/UserClaimsArgumentResolver.java
@@ -2,6 +2,7 @@ package greencity.converters;
 
 import greencity.annotations.CurrentUserClaims;
 import greencity.dto.user.UserClaims;
+import greencity.exception.exceptions.NoJwtException;
 import greencity.security.jwt.JwtTool;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +29,12 @@ public class UserClaimsArgumentResolver implements HandlerMethodArgumentResolver
         ModelAndViewContainer mavContainer,
         @NonNull NativeWebRequest webRequest,
         WebDataBinderFactory binderFactory) throws Exception {
-        String jwt = jwtTool.extractJwtFromNativeWebRequest(webRequest);
+        String jwt;
+        try {
+            jwt = jwtTool.extractJwtFromNativeWebRequest(webRequest);
+        } catch (NoJwtException e) {
+            return null;
+        }
         return jwtTool.extractUserClaims(jwt);
     }
 }
