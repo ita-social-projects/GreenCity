274eda8e4eaa780761852a2a8652dad70c9302f4
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-05-21 00:32:25 +0300

    Deleted unneded comment

--- dao/src/test/java/greencity/ModelUtils.java
+++ dao/src/test/java/greencity/ModelUtils.java
@@ -17,7 +17,7 @@ public class ModelUtils {
     public static User getUser() {
         return User.builder()
             .id(1L)
-//            .email("danylo@gmail.com")
+            .email("danylo@gmail.com")
             .name("Taras")
             .build();
     }
