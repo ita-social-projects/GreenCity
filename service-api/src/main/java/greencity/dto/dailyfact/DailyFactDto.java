579587b9d24103e36aa8965fbc587d68cf24eca5
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-06-27 13:19:17 +0300

    Add DailyFactDto class

---
+++ service-api/src/main/java/greencity/dto/dailyfact/DailyFactDto.java
@@ -0,0 +1,36 @@
+package greencity.dto.dailyfact;
+
+import greencity.constant.ServiceValidationConstants;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import java.time.ZonedDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class DailyFactDto {
+    @NotNull
+    private Long id;
+
+    @Email(regexp = ServiceValidationConstants.EMAIL_REGEXP)
+    private String email;
+
+    @NotBlank
+    @Length(max = 300)
+    private String factEn;
+
+    @NotBlank
+    @Length(max = 300)
+    private String factUk;
+
+    private ZonedDateTime createdAt;
+}
