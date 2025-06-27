b1a453a9a775dd28f9320f0d1e95d96d477d885c
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-06-27 12:30:14 +0300

    Create DailyFact entity to represent a personalized eco-topic daily fact for each user

---
+++ dao/src/main/java/greencity/entity/DailyFact.java
@@ -0,0 +1,43 @@
+package greencity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.ZonedDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(exclude = {"createdAt"})
+@Table(name = "daily_fact")
+public class DailyFact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "fact_en", nullable = false, length = 300)
+    private String factEn;
+
+    @Column(name = "fact_uk", nullable = false, length = 300)
+    private String factUk;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private ZonedDateTime createdAt;
+}
