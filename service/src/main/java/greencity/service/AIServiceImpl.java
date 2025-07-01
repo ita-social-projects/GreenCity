fa840d2e5989ee3ef9c47051d118faf858f58c73
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-07-01 15:34:00 +0300

    Remove unused imports

--- service/src/main/java/greencity/service/AIServiceImpl.java
+++ service/src/main/java/greencity/service/AIServiceImpl.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
