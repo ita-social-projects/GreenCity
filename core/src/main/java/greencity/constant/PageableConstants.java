4eae7801fa8a888f222bd8ce76af51c96cfbe397
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-07-04 11:29:43 +0300

    Add new constant for sorting parameter into PageableConstants

--- core/src/main/java/greencity/constant/PageableConstants.java
+++ core/src/main/java/greencity/constant/PageableConstants.java
@@ -8,5 +8,6 @@ public class PageableConstants {
     public static final Integer DEFAULT_PAGE_SIZE = 20;
     public static final String PAGE = "page";
     public static final String SIZE = "size";
+    public static final String SORT = "sort";
     public static final Integer MAX_PAGE_SIZE = 100;
 }
