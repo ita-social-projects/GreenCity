ae64374d67c1716ea7b58e200156894721fd3e5d
olehio-p <146092129+olehio-p@users.noreply.github.com>
2025-07-16 12:36:38 +0000

    Deploying to gh-pages from  @ 2301b0eaf506c9900620c065eb97b155421c2929 🚀

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -145,7 +145,7 @@ public class SecurityConfig {
                     "/swagger-resources/**",
                     "/webjars/**")
                 .permitAll()
-                .requestMatchers("/css/**", "/img/**").permitAll()
+                .requestMatchers("/css/**", "/img/**", "/scripts/**").permitAll()
                 .requestMatchers(HttpMethod.GET,
                     FACT_OF_THE_DAY + RANDOM,
                     CATEGORIES,
