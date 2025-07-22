f8b36a4a3ca489fb5559913d73fc16ed037927f0
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-07-22 11:34:38 +0300

    Align email validation across all entities and dtos

--- service-api/src/main/java/greencity/constant/ServiceValidationConstants.java
+++ service-api/src/main/java/greencity/constant/ServiceValidationConstants.java
@@ -49,6 +49,13 @@ public class ServiceValidationConstants {
     public static final String HABIT_COMPLEXITY = "{greenCity.validation.habit.complexity}";
     public static final String TAG_LIST_MIN_LENGTH = "{greenCity.validation.min.tags}";
     public static final String TAG_LIST_MAX_LENGTH = "{greenCity.validation.max.tags}";
-    public static final String EMAIL_REGEXP = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+    public static final String EMAIL_REGEXP =
+        "^(?=.{3,72}$)"
+            + "([a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+"
+            + "(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*)"
+            + "@"
+            + "(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\\.)+"
+            + "[a-zA-Z]{2,63}|"
+            + "\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}\\])$";
     public static final String HABIT_DEFAULT_DURATION = "{greenCity.validation.habit.defaultDuration}";
 }
