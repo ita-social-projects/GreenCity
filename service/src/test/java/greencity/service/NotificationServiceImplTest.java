2648fad3227b45ce13b32fa8170c38f025880d42
markotymofeiuk <146076997+markotymofeiuk@users.noreply.github.com>
2025-07-25 13:24:47 +0300

    Bugfix #8738 Fixed email notifications for likes (#8857)
    * Fix logical order of like notification creation in EcoNewsServiceImpl
    
    * Add missing functionality for event like notifications

--- service/src/test/java/greencity/service/NotificationServiceImplTest.java
+++ service/src/test/java/greencity/service/NotificationServiceImplTest.java
@@ -382,6 +382,9 @@ class NotificationServiceImplTest {
                 .findAllByNotificationByTypeAndViewedIsFalseAndEmailSentIsFalse(NotificationType.EVENT_COMMENT_LIKE))
                 .thenReturn(Collections.singletonList(notification));
             when(notificationRepo
+                .findAllByNotificationByTypeAndViewedIsFalseAndEmailSentIsFalse(NotificationType.EVENT_LIKE))
+                .thenReturn(Collections.singletonList(notification));
+            when(notificationRepo
                 .findAllByNotificationByTypeAndViewedIsFalseAndEmailSentIsFalse(NotificationType.HABIT_LIKE))
                 .thenReturn(Collections.singletonList(notification));
             when(notificationRepo
@@ -399,7 +402,7 @@ class NotificationServiceImplTest {
             notificationService.sendLikeScheduledEmail();
             ArgumentCaptor<ScheduledEmailMessage> captor = ArgumentCaptor.forClass(ScheduledEmailMessage.class);
             await().atMost(5, SECONDS)
-                .untilAsserted(() -> verify(restClient, times(5)).sendScheduledEmailNotification(captor.capture()));
+                .untilAsserted(() -> verify(restClient, times(6)).sendScheduledEmailNotification(captor.capture()));
             List<ScheduledEmailMessage> capturedMessages = captor.getAllValues();
             for (ScheduledEmailMessage capturedMessage : capturedMessages) {
                 assertEquals(notification.getTargetUser().getEmail(), capturedMessage.getEmail());
