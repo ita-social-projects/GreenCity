fe8ed999f08381de4e7b94c068abbfad32cc6f89
dima <dimasowner7201@gmail.com>
2025-08-03 15:28:04 +0300

    deleted unused imports

--- dao/src/main/java/greencity/entity/TagsCoherence.java
+++ dao/src/main/java/greencity/entity/TagsCoherence.java
@@ -11,11 +11,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
