5ae4d20ece241d30e998b96d558fab5a476871a4
Olexandr <Mega.alex.alex2000@gmail.com>
2025-08-11 13:45:17 +0300

    formatting code

--- service-api/src/main/java/greencity/exception/exceptions/ErrorParsingException.java
+++ service-api/src/main/java/greencity/exception/exceptions/ErrorParsingException.java
@@ -3,5 +3,5 @@ package greencity.exception.exceptions;
 import lombok.experimental.StandardException;
 
 @StandardException
-public class ErrorParsingException extends RuntimeException{
+public class ErrorParsingException extends RuntimeException {
 }
