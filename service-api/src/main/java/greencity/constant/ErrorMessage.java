633c3b84a45c494b25afe5dc4b6283a00f00d465
SaenkoVova <48810972+SaenkoVova@users.noreply.github.com>
2025-08-13 22:19:12 +0000

    Deploying to gh-pages from  @ 8cca59916307ca9e293ee03832363b7d104c871e 🚀

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -243,18 +243,4 @@ public class ErrorMessage {
     public static final String INVALID_SORT_FORMAT_EXCEPTION = "Invalid sort format: '%s'";
     public static final String INVALID_SORT_VALUE_EXCEPTION = "Invalid sort field: '%s'";
     public static final String NEGATIVE_VALUE_EXCEPTION = "%s must be a positive number";
-    public static final String LOG_FILES_NOT_FOUND = "No log files found";
-    public static final String LOG_FILE_NOT_FOUND = "No file found with name: %s";
-    public static final String CANNOT_READ_LOG_FILE = "Error reading log file: %s";
-    public static final String BAD_SECRET_KEY = "The given secret key is incorrect";
-    public static final String CANNOT_DELETE_DOTENV = "Failed to delete .env file";
-    public static final String FUNCTIONALITY_NOT_AVAILABLE = "Functionality is not available";
-    public static final String NEGATIVE_LIMIT = "Limit cannot be negative";
-    public static final String NEGATIVE_OFFSET = "Offset cannot be negative";
-    public static final String SQL_METADATA_EXCEPTION_MESSAGE = "Error occurred while retrieving database data for: ";
-    public static final String EXCEED_LIMIT = "Out of max rows limit. Max limit is " + AppConstant.SQL_ROW_LIMIT;
-    public static final String GENERATION_EXCEL_FILE_ERROR = "Error generating Excel file";
-    public static final String EMPTY_TABLE = "Table '%s' doesn't contain any row";
-    public static final String INVALID_TABLE_NAME = "Table name must contain only lowercase letters "
-        + "and single underscores between words. Cannot start or end with an underscore.";
 }
