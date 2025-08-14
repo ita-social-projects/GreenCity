72c7c0c2d8bf5204f3520521285658be17d8c973
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-08-14 18:58:53 +0300

    Add method in CustomExceptionHandler to handle UnsupportedSortException

--- core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
+++ core/src/main/java/greencity/exception/handler/CustomExceptionHandler.java
@@ -35,6 +35,7 @@ import greencity.exception.exceptions.ResourceNotFoundException;
 import greencity.exception.exceptions.TagNotFoundException;
 import greencity.exception.exceptions.ToDoListItemNotFoundException;
 import greencity.exception.exceptions.UnauthorizedException;
+import greencity.exception.exceptions.UnsupportedSortException;
 import greencity.exception.exceptions.UserAlreadyHasEnrolledHabitAssign;
 import greencity.exception.exceptions.UserAlreadyHasHabitAssignedException;
 import greencity.exception.exceptions.UserAlreadyHasMaxNumberOfActiveHabitAssigns;
@@ -240,6 +241,31 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         ExceptionResponse exceptionResponse = new ExceptionResponse(getErrorAttributes(request));
         log.trace(exceptionResponse.getMessage(), exceptionResponse.getTrace());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+    }
+
+    /**
+     * Handles exceptions of type {@link UnsupportedSortException} thrown during request processing.
+     * This method intercepts the {@code UnsupportedSortException}, logs the error, and prepares
+     * a standardized error response containing details about the failed sorting operation.
+     * The client receives a response with HTTP status 400 (Bad Request) and a body that includes
+     * an error message describing the reason for the failure.
+     *
+     * @param ex      the {@link UnsupportedSortException} instance containing details about the invalid sorting
+     *                parameter(s)
+     * @param request the {@link WebRequest} providing context about the web request during which
+     *                the exception occurred
+     * @return a {@link ResponseEntity} containing an {@link ExceptionResponse} object with the error details
+     *         and HTTP status 400 (BAD_REQUEST)
+     */
+    @ExceptionHandler(UnsupportedSortException.class)
+    public final ResponseEntity<Object> handleResourceNotFoundException(UnsupportedSortException ex,
+                                                                        WebRequest request) {
+        log.trace(ex.getMessage(), ex);
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(getErrorAttributes(request));
+        exceptionResponse.setMessage(ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
     }
 
     /**
