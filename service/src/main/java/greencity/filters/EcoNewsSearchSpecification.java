92e43f860b97ee28ba11d75f2253ca183c92c812
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-09-22 10:41:47 +0300

    Replacement logic from repository implementations to services using specifications (#9015)
    * removed EcoNewsSearchRepo and its implementation from dao module; moved logic into service and used existed repo method with specification instead
    
    * removed CustomNotificationRepo and its implementation from dao module; moved logic into service and used existed repo method with specification instead
    
    * removed CustomUserRepo and its implementation from dao module; moved logic into service instead
    
    * removed EventSearchRepo and its implementation from dao module; moved logic into service and used existed repo method with specification instead
    
    * removed PlaceSearchRepo and its implementation from dao module; moved logic into service and used existed repo method with specification instead
    
    * moved author predicate creation method from general MySpecification interface to EcoNewsSpecification where it was only used

---
+++ service/src/main/java/greencity/filters/EcoNewsSearchSpecification.java
@@ -0,0 +1,67 @@
+package greencity.filters;
+
+import greencity.entity.EcoNews;
+import greencity.entity.EcoNews_;
+import greencity.entity.User;
+import greencity.entity.User_;
+import greencity.utils.SpecificationUtils;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.function.TriFunction;
+import org.jetbrains.annotations.NotNull;
+
+@RequiredArgsConstructor
+public class EcoNewsSearchSpecification implements MySpecification<EcoNews> {
+    private final transient List<SearchCriteria> searchCriteriaList;
+    private final transient Long userId;
+
+    private final transient Map<String, TriFunction<Root<EcoNews>, CriteriaBuilder, SearchCriteria, Predicate>> pred =
+        Map.of("text", this::getTextPredicate,
+            "isFavorite", this::getIsFavoritePredicate);
+
+    @Override
+    public Predicate toPredicate(@NotNull Root<EcoNews> root,
+        @NotNull CriteriaQuery<?> criteriaQuery,
+        @NotNull CriteriaBuilder criteriaBuilder) {
+        Predicate allPredicates = toPredicateFromMap(root, criteriaBuilder, searchCriteriaList, pred);
+        criteriaQuery.distinct(true);
+        return allPredicates;
+    }
+
+    private Predicate getTextPredicate(Root<EcoNews> root, CriteriaBuilder criteriaBuilder,
+        SearchCriteria searchCriteria) {
+        String text = searchCriteria.getValue().toString().trim();
+        if (text.isEmpty()) {
+            return criteriaBuilder.conjunction();
+        }
+
+        text = SpecificationUtils.escapeSpecialCharacters(text);
+        Predicate titlePredicate = criteriaBuilder.like(root.get(EcoNews_.TITLE),
+            "%" + text + "%");
+        Predicate textPredicate = criteriaBuilder.like(root.get(EcoNews_.TEXT),
+            "%" + text + "%");
+        Predicate shortInfoPredicate = criteriaBuilder.like(root.get(EcoNews_.SHORT_INFO),
+            "%" + text + "%");
+        return criteriaBuilder.or(titlePredicate, textPredicate, shortInfoPredicate);
+    }
+
+    private Predicate getIsFavoritePredicate(Root<EcoNews> root, CriteriaBuilder criteriaBuilder,
+        SearchCriteria searchCriteria) {
+        Boolean isFavorite = (Boolean) searchCriteria.getValue();
+        if (isFavorite == null) {
+            return criteriaBuilder.conjunction();
+        }
+
+        Join<EcoNews, User> followersJoin = root.join(EcoNews_.FOLLOWERS, JoinType.LEFT);
+        return isFavorite
+            ? criteriaBuilder.equal(followersJoin.get(User_.ID), userId)
+            : criteriaBuilder.notEqual(followersJoin.get(User_.ID), userId);
+    }
+}
