22ad016f850f5dd97ac6c090a0bd347db78dc8c3
dxrknesss <imp0rt4ntduck@gmail.com>
2025-10-08 19:43:40 +0300

    sonarqube

--- core/src/test/java/greencity/exception/handler/CustomExceptionHandlerTest.java
+++ core/src/test/java/greencity/exception/handler/CustomExceptionHandlerTest.java
@@ -26,7 +26,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
