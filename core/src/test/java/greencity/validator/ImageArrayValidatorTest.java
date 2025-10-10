d57a30baa19f0a1ff804631c76bed6dad607c8a2
dxrknesss <imp0rt4ntduck@gmail.com>
2025-10-11 00:35:16 +0300

    refactor test, remove junit4 dependency

--- core/src/test/java/greencity/validator/ImageArrayValidatorTest.java
+++ core/src/test/java/greencity/validator/ImageArrayValidatorTest.java
@@ -1,19 +1,23 @@
 package greencity.validator;
 
 import jakarta.validation.ConstraintValidatorContext;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
+
 import java.util.List;
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -41,40 +45,45 @@ class ImageArrayValidatorTest {
     void validWithValidImageJPEGReturnsTrue() {
         MockMultipartFile[] image = new MockMultipartFile[] {
             new MockMultipartFile("image", "image.jpeg", "image/jpeg", new byte[1024])};
-        assertTrue(imageArrayValidator.isValid(image, constraintValidatorContext));
+
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isTrue();
     }
 
     @Test
     void validWithValidImageJPGReturnsTrue() {
         MockMultipartFile[] image = new MockMultipartFile[] {
             new MockMultipartFile("image", "image.jpg", "image/jpg", new byte[10 * 1024 * 1024])}; // max size
-        assertTrue(imageArrayValidator.isValid(image, constraintValidatorContext));
+
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isTrue();
     }
 
     @Test
     void validWithValidImagePNGReturnsTrue() {
         MockMultipartFile[] image = new MockMultipartFile[] {
             new MockMultipartFile("image", "image.png", "image/png", new byte[1])};
-        assertTrue(imageArrayValidator.isValid(image, constraintValidatorContext));
+
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isTrue();
     }
 
     @Test
     void validWithValidImageGIFReturnsTrue() {
         MockMultipartFile[] image = new MockMultipartFile[] {
             new MockMultipartFile("image", "image.gif", "image/gif", new byte[1024])};
-        assertTrue(imageArrayValidator.isValid(image, constraintValidatorContext));
+
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isTrue();
     }
 
     @Test
     void isValid_WithEmptyImageReturnsTrue() {
         MockMultipartFile[] image = new MockMultipartFile[] {
             new MockMultipartFile("image", "image.jpeg", "image/jpeg", new byte[0])};
-        assertTrue(imageArrayValidator.isValid(image, constraintValidatorContext));
+
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isTrue();
     }
 
     @Test
     void isValid_WithNullImageReturnsTrue() {
-        assertTrue(imageArrayValidator.isValid(null, constraintValidatorContext));
+        then(imageArrayValidator.isValid(null, constraintValidatorContext)).isTrue();
     }
 
     @Test
@@ -82,8 +91,7 @@ class ImageArrayValidatorTest {
         MockMultipartFile[] image = new MockMultipartFile[1];
         image[0] = null;
 
-        boolean result = imageArrayValidator.isValid(image, constraintValidatorContext);
-        assertTrue(result);
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isTrue();
     }
 
     @Test
@@ -93,7 +101,7 @@ class ImageArrayValidatorTest {
         when(constraintValidatorContext.buildConstraintViolationWithTemplate(anyString()))
             .thenReturn(violationBuilder);
 
-        assertFalse(imageArrayValidator.isValid(image, constraintValidatorContext));
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isFalse();
     }
 
     @Test
@@ -105,11 +113,10 @@ class ImageArrayValidatorTest {
         when(constraintValidatorContext.buildConstraintViolationWithTemplate(anyString()))
             .thenReturn(violationBuilder);
 
-        assertFalse(imageArrayValidator.isValid(image, constraintValidatorContext));
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isFalse();
 
         verify(constraintValidatorContext).disableDefaultConstraintViolation();
-        verify(constraintValidatorContext)
-            .buildConstraintViolationWithTemplate(anyString());
+        verify(constraintValidatorContext).buildConstraintViolationWithTemplate(anyString());
     }
 
     @Test
@@ -121,7 +128,7 @@ class ImageArrayValidatorTest {
         when(constraintValidatorContext.buildConstraintViolationWithTemplate(anyString()))
             .thenReturn(violationBuilder);
 
-        assertFalse(imageArrayValidator.isValid(image, constraintValidatorContext));
+        then(imageArrayValidator.isValid(image, constraintValidatorContext)).isFalse();
 
         verify(constraintValidatorContext).disableDefaultConstraintViolation();
         verify(constraintValidatorContext)
@@ -130,30 +137,45 @@ class ImageArrayValidatorTest {
 
     @Test
     void getMaxSizeInBytes_WithInvalidUnit_ThrowsException() {
+        // given
         ReflectionTestUtils.setField(imageArrayValidator, "maxImageSize", "500TB"); // Invalid unit
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            imageArrayValidator.getMaxSizeInBytes();
-        });
-        Assertions.assertEquals("Invalid file size unit: 500TB", exception.getMessage());
+
+        // when
+        // then
+        thenThrownBy(() -> imageArrayValidator.getMaxSizeInBytes())
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Invalid file size unit: 500TB");
+    }
+
+    private static Stream<Arguments> getMaxSizeArgs() {
+        return Stream.of(
+            Arguments.of(1024 * 1024, "1024KB"),
+            Arguments.of(5 * 1024 * 1024, "5MB"),
+            Arguments.of(1024 * 1024 * 1024, "1GB"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getMaxSizeArgs")
+    void getMaxSizeInBytes_WithDifferentUnits_CorrectConversion(long sizeBytes, String sizeWithPrefix) {
+        // given
+        ReflectionTestUtils.setField(imageArrayValidator, "maxImageSize", sizeWithPrefix);
+
+        // when
+        final long size = imageArrayValidator.getMaxSizeInBytes();
+
+        // then
+        then(size).isEqualTo(sizeBytes);
     }
 
     @Test
-    void getMaxSizeInBytes_WithDifferentUnits_CorrectConversion() {
-        ReflectionTestUtils.setField(imageArrayValidator, "maxImageSize", "1024KB");
-        Assertions.assertEquals(1024 * 1024, imageArrayValidator.getMaxSizeInBytes());
-
-        ReflectionTestUtils.setField(imageArrayValidator, "maxImageSize", "5MB");
-        Assertions.assertEquals(5 * 1024 * 1024, imageArrayValidator.getMaxSizeInBytes());
-
-        ReflectionTestUtils.setField(imageArrayValidator, "maxImageSize", "1GB");
-        Assertions.assertEquals(1024 * 1024 * 1024, imageArrayValidator.getMaxSizeInBytes());
-
+    void getMaxSizeIncorrect() {
+        // given
         ReflectionTestUtils.setField(imageArrayValidator, "maxImageSize", "500TB");
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            imageArrayValidator.getMaxSizeInBytes();
-        });
-        Assertions.assertEquals("Invalid file size unit: 500TB", exception.getMessage());
-
+        // when
+        // then
+        thenThrownBy(() -> imageArrayValidator.getMaxSizeInBytes())
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Invalid file size unit: 500TB");
     }
 }
