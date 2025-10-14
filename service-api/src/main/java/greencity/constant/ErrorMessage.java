e8b87b61a010f5920f36d8a4b8c2b2c20974d898
BohdanLys <mrboba04@gmail.com>
2025-10-14 12:22:23 +0300

    Removed unused constant

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -61,8 +61,6 @@ public class ErrorMessage {
         "There is no habit assign for current user and such habit with id: ";
     public static final String HABIT_ASSIGN_NOT_FOUND_WITH_CURRENT_USER_ID_AND_HABIT_ASSIGN_ID =
         "There is no habit assign for current user and such habit assign id: ";
-    public static final String HABIT_ASSIGN_NOT_FOUND_WITH_CURRENT_USER_ID_AND_INPROGRESS_STATUS =
-        "There is no inprogress habit assign for current user: ";
     public static final String HABIT_STATUS_CALENDAR_OUT_OF_ENROLL_RANGE =
         "Can't enroll habit because date input is not in a range from today to it's 7 passed days";
     public static final String HABIT_ASSIGN_ENROLL_RANGE_REACHED =
