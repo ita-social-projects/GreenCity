dc8e93fae90be82f15c3f3e94101a54b2cc68b42
Olexandr Pohranychnyi <78477927+SashaPog@users.noreply.github.com>
2025-12-18 13:36:55 +0200

    hotfix snippet annotations (#9302)
    * hotfix snippet annotations
    
    * formatting code
    
    * fix codeRabbit issues

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -140,8 +140,10 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/error").permitAll()
                 .requestMatchers("/", "/management/", "/management/login").permitAll()
-                .requestMatchers("/management/**").hasAnyRole(ADMIN)
                 .requestMatchers("/actuator/prometheus").permitAll()
+                .requestMatchers("/management/**",
+                    "/actuator/**")
+                .hasAnyRole(ADMIN)
                 .requestMatchers("/v2/api-docs/**",
                     "/v3/api-docs/**",
                     "/swagger.json",
